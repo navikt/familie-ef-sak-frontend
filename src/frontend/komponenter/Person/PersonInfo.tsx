14ad24722b1d0a39a6ef6cab2a331ce87c7c2eea
ma10s <mattis.janitz@nav.no>
2020-03-20 10:26:02 +0100

    Trukket ut adressehistorikk fra json.

--- src/frontend/komponenter/Person/PersonInfo.tsx
+++ src/frontend/komponenter/Person/PersonInfo.tsx
@@ -8,6 +8,7 @@ import { ISaksbehandler } from '../../typer/saksbehandler';
 import { Panel } from 'nav-frontend-paneler';
 import Ekspanderbartpanel from 'nav-frontend-ekspanderbartpanel';
 import { Ressurs } from '../../typer/ressurs';
+// import {Label, Input} from "nav-frontend-skjema";
 
 const PersonInfo = () => {
     const [personinput, settPersoninput] = useState<string>('');
@@ -22,58 +23,85 @@ const PersonInfo = () => {
         };
     }, []);
 
-    const renderPersonhistorikkdata = () => {
+    const renderJason = () => {
         if (persondata) {
             return (
-                <Ekspanderbartpanel tittel="Se json data">
-                    {JSON.stringify(persondata.personhistorikkInfo)}
-                </Ekspanderbartpanel>
+                <>
+                    <Ekspanderbartpanel tittel="Se all json data ">
+                        {JSON.stringify(persondata)}
+                        {JSON.stringify(persondata.personhistorikkInfo)}
+                    </Ekspanderbartpanel>
+                </>
             );
         }
     };
 
     const renderEkspanderbartpanel = () => {
         if (persondata) {
+            const adresseHistorikk = persondata.personhistorikkInfo.adressehistorikk.map(
+                periode => {
+                    return (
+                        <div key={periode.periode.fom}>
+                            <p>
+                                From: {periode.periode.fom}, TO: {periode.periode.tom}
+                            </p>
+                            <p>
+                                Adr: {periode.adresse.adresselinje1}, {periode.adresse.postnummer},{' '}
+                                {periode.adresse.poststed}
+                            </p>
+                        </div>
+                    );
+                }
+            );
+
             return (
-                <Ekspanderbartpanel tittel="Se json data">
-                    {JSON.stringify(persondata)}
-                </Ekspanderbartpanel>
+                <>
+                    <Ekspanderbartpanel tittel="Se person informasjon">
+                        <h2>Persondata </h2>
+                        <p>Navn: {persondata?.personinfo.navn}</p>
+                        <p>Personstatus: {persondata?.personinfo?.personstatus} </p>
+                        <p>
+                            Sivilstatus: {persondata?.personinfo.sivilstand} (* mer data fra
+                            backend/søknad)
+                        </p>
+                        <p>Fødselsdato: {persondata?.personinfo.fødselsdato}</p>
+                        <p>Statsborgerskap: {persondata?.personinfo.statsborgerskap?.kode} </p>
+                        <p>Telefonnummer: (fra søknad?)</p>
+                        <p>Målform: (* mangler fra backend)</p>
+                        <h2>Adressehistorikk</h2>
+                        {adresseHistorikk}
+                    </Ekspanderbartpanel>
+                </>
             );
         }
     };
 
     return (
         <Panel>
-            <InputMedLabelTilVenstre
-                bredde={'S'}
-                label={'Finn person'}
-                value={personinput}
-                type={'string'}
-                onChange={(event: any) => {
-                    settPersoninput(event.target.value);
-                }}
-            />
-            <Knapp
-                onClick={() => {
-                    hentPersoninfo(personinput, saksbehandler!!).then(
-                        (response: Ressurs<IPerson>) => {
-                            settPersondata(response.data);
-                        }
-                    );
-                }}
-            >
-                Finn person
-            </Knapp>
-            <h2>Persondata </h2>
-            <p>Navn: {persondata?.personinfo.navn}</p>
-            <p>Sivilstand: {persondata?.personinfo.sivilstand}</p>
-            <p>Fødselsdato: {persondata?.personinfo.fødselsdato}</p>
-            <p>Statsborgerskap: {persondata?.personinfo.statsborgerskap?.kode} </p>
-
+            <div style={{ display: 'flex' }}>
+                <InputMedLabelTilVenstre
+                    bredde={'S'}
+                    label={'Finn person'}
+                    value={personinput}
+                    type={'string'}
+                    onChange={(event: any) => {
+                        settPersoninput(event.target.value);
+                    }}
+                />
+                <Knapp
+                    onClick={() => {
+                        hentPersoninfo(personinput, saksbehandler!!).then(
+                            (response: Ressurs<IPerson>) => {
+                                settPersondata(response.data);
+                            }
+                        );
+                    }}
+                >
+                    Finn person
+                </Knapp>
+            </div>
             {renderEkspanderbartpanel()}
-
-            <h2>Person historikk</h2>
-            {renderPersonhistorikkdata()}
+            {renderJason()}
         </Panel>
     );
 };
