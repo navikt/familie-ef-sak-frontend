fc8eac6c3be2276e770bc67c3c62c00882c02603
Johan Blomgren <johan.blomgren@nav.no>
2020-08-03 09:55:06 +0200

    Ny ingress, ensligmorellerfar

--- src/backend/konfigurerApp.ts
+++ src/backend/konfigurerApp.ts
@@ -6,7 +6,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const konfigurerAzure = () => {
-    const host = 'alenemedbarn';
+    const host = 'ensligmorellerfar';
     switch (process.env.ENV) {
         case 'local':
             process.env.AAD_LOGOUT_REDIRECT_URL = `https://login.microsoftonline.com/navq.onmicrosoft.com/oauth2/logout?post_logout_redirect_uri=http:\\\\localhost:8000`;
