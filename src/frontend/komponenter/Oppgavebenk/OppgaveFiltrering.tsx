f3bbd3732be7029e180e2b7b2112984699139eb9
Asbjørn Steinskog <asbjorn@steinskog.me>
2020-09-29 15:39:57 +0200

    Fiks 'Tilbakestill filtrering'-knapp

--- src/frontend/komponenter/Oppgavebenk/OppgaveFiltrering.tsx
+++ src/frontend/komponenter/Oppgavebenk/OppgaveFiltrering.tsx
@@ -86,6 +86,7 @@ const OppgaveFiltering: React.FC<IOppgaveFiltrering> = ({ hentOppgaver }) => {
                     />
                 </div>
                 <Select
+                    value={oppgaveRequest.oppgavetype || ''}
                     className="flex-item"
                     label="Oppgavetype"
                     onChange={(event) => {
@@ -104,6 +105,7 @@ const OppgaveFiltering: React.FC<IOppgaveFiltrering> = ({ hentOppgaver }) => {
                     ))}
                 </Select>
                 <Select
+                    value={oppgaveRequest.behandlingstema || ''}
                     className="flex-item"
                     label="Gjelder"
                     onChange={(event) => {
@@ -135,6 +137,7 @@ const OppgaveFiltering: React.FC<IOppgaveFiltrering> = ({ hentOppgaver }) => {
                     />
                 </div>
                 <Select
+                    value={oppgaveRequest.enhet || ''}
                     className="flex-item"
                     label="Enhet"
                     onChange={(event) => {
@@ -152,6 +155,7 @@ const OppgaveFiltering: React.FC<IOppgaveFiltrering> = ({ hentOppgaver }) => {
                 </Select>
 
                 <Select
+                    value={oppgaveRequest.saksbehandler || ''}
                     className="flex-item"
                     label="Saksbehandler"
                     onChange={(event) => {
@@ -196,7 +200,13 @@ const OppgaveFiltering: React.FC<IOppgaveFiltrering> = ({ hentOppgaver }) => {
                 <Hovedknapp className="flex-item" onClick={() => hentOppgaver(oppgaveRequest)}>
                     Hent oppgaver
                 </Hovedknapp>
-                <Knapp className="flex-item" onClick={() => setOppgaveRequest(initOppgaveRequest)}>
+                <Knapp
+                    className="flex-item"
+                    onClick={() => {
+                        setOppgaveRequest(initOppgaveRequest);
+                        hentOppgaver(oppgaveRequest);
+                    }}
+                >
                     Tilbakestill filtrering
                 </Knapp>
             </KnappWrapper>
