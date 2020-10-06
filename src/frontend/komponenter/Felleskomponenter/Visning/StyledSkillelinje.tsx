715300a525f2b8e38c64814c486ec62b7618bb3f
Johan Blomgren <johan.blomgren@nav.no>
2020-10-06 10:08:34 +0200

    pr fiks, bredde på Skillelinje

--- src/frontend/komponenter/Felleskomponenter/Visning/StyledSkillelinje.tsx
+++ src/frontend/komponenter/Felleskomponenter/Visning/StyledSkillelinje.tsx
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import { navLysGra } from '@navikt/familie-header';
 
 export const StyledSkillelinje = styled.div`
-    width: 512px;
     height: 2px;
     border: solid 1px ${navLysGra};
 `;
