22de6d449ec00b454f0a4f912fc301927172c2b2
Johan Blomgren <johan.blomgren@nav.no>
2020-10-07 12:20:20 +0200

    Mulighet til å endre vurderingen

--- src/frontend/komponenter/Behandling/Vurdering/Vurdering.tsx
+++ src/frontend/komponenter/Behandling/Vurdering/Vurdering.tsx
@@ -11,11 +11,19 @@ import {
 import { Radio, RadioGruppe, Select, Textarea } from 'nav-frontend-skjema';
 import { Hovedknapp } from 'nav-frontend-knapper';
 import styled from 'styled-components';
+import { Element, Normaltekst, Undertittel } from 'nav-frontend-typografi';
+import RedigerBlyant from '../../../ikoner/RedigerBlyant';
+import Lenke from 'nav-frontend-lenker';
+
+const StyledEndreVurdering = styled.div`
+    > *:not(:first-child) {
+        margin-top: 10px;
+    }
+`;
 
 const StyledVurdering = styled.div`
-    > div,
-    button {
-        margin-top: 15px;
+    > *:not(:first-child) {
+        margin-top: 5px;
     }
 `;
 
@@ -41,17 +49,42 @@ interface Props {
     className?: string;
     onChangeRadioButton?: (valgtSvar: ChangeEvent<HTMLInputElement>) => void;
 }
+
+/**
+ * Hva skal skje hvis Resultat == Nei? Då trenger man vel ikke fortsette med denne behandlingen?
+ * Styling av StyledVurdering
+ * Valideringer når man klikker på endre-knappen
+ * Propagere opp endringer fra endre-knappen
+ */
 const Vurdering: FC<Props> = ({ vurdering, className }) => {
     const config = VurderingConfig[vurdering.vilkårType];
     const [vurderingState, setVurderingState] = useState<IVurdering>(vurdering);
-    {
-        /* TODO propagerare opp? */
+    const [lagret, setLagret] = useState<boolean>(
+        vurdering.resultat !== VilkårResultat.IKKE_VURDERT
+    );
+    if (lagret) {
+        return (
+            <StyledVurdering className={className}>
+                <Undertittel>Manuelt behandlet</Undertittel>
+                <Lenke href="#" onClick={() => setLagret(false)}>
+                    <RedigerBlyant width={19} heigth={19} withDefaultStroke={false} />
+                    <span>Rediger</span>
+                </Lenke>
+                <Normaltekst>{config.vilkår}</Normaltekst>
+                <Element>Unntak</Element>
+                {vurderingState.unntak && (
+                    <>
+                        <Normaltekst>{vurderingState.unntak}</Normaltekst>
+                        <Element>Begrunnelse</Element>
+                    </>
+                )}
+                <Normaltekst>{vurderingState.begrunnelse}</Normaltekst>
+            </StyledVurdering>
+        );
     }
     return (
-        <StyledVurdering className={className}>
-            {/* TODO lagret/ikke lagret */}
+        <StyledEndreVurdering className={className}>
             <RadioGruppe legend={config.vilkår}>
-                {/* TODO Kanskje? */}
                 {[VilkårResultat.JA, VilkårResultat.NEI].map((vilkårResultat) => (
                     <Radio
                         key={vilkårResultat}
@@ -99,8 +132,14 @@ const Vurdering: FC<Props> = ({ vurdering, className }) => {
                     });
                 }}
             />
-            <Hovedknapp>Lagre</Hovedknapp>
-        </StyledVurdering>
+            <Hovedknapp
+                onClick={() => {
+                    setLagret(true);
+                }}
+            >
+                Lagre
+            </Hovedknapp>
+        </StyledEndreVurdering>
     );
 };
 export default Vurdering;
