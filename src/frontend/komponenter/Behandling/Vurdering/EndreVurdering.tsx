112b0e0f75f5cbe81e5923d227c60fb59a44db42
Johan Blomgren <johan.blomgren@nav.no>
2020-10-13 11:22:52 +0200

    Resetter kommende delvilkår hvis man endrer en tidligere fra nei til ja Renderer unntak hvis den skal vises

--- src/frontend/komponenter/Behandling/Vurdering/EndreVurdering.tsx
+++ src/frontend/komponenter/Behandling/Vurdering/EndreVurdering.tsx
@@ -24,11 +24,23 @@ const StyledEndreVurdering = styled.div`
 `;
 
 const oppdaterDelvilkår = (vurdering: IVurdering, oppdatertDelvilkår: IDelvilkår): IVurdering => {
+    let harPassertSisteDelvilkårSomSkalVises = false;
     return {
         ...vurdering,
-        delvilkårVurderinger: vurdering.delvilkårVurderinger.map((delvilkår) =>
-            delvilkår.type === oppdatertDelvilkår.type ? oppdatertDelvilkår : delvilkår
-        ),
+        delvilkårVurderinger: vurdering.delvilkårVurderinger.map((delvilkår) => {
+            if (delvilkår.type === oppdatertDelvilkår.type) {
+                harPassertSisteDelvilkårSomSkalVises = true;
+                return oppdatertDelvilkår;
+            } else if (
+                harPassertSisteDelvilkårSomSkalVises &&
+                delvilkår.resultat !== VilkårResultat.IKKE_VURDERT
+            ) {
+                return { type: delvilkår.type, resultat: VilkårResultat.IKKE_VURDERT };
+            } else {
+                return delvilkår;
+            }
+        }),
+        unntak: undefined,
     };
 };
 
@@ -47,62 +59,44 @@ const EndreVurdering: FC<Props> = ({ config, data, oppdaterVurdering, settRedige
     );
     const nesteDelvilkår =
         delvilkårSomManglerVurdering.length > 0 ? delvilkårSomManglerVurdering[0] : null;
-    let erPåDelvilkårSomSkalVurderes = false;
+    let harPassertSisteDelvilkårSomSkalVises = false;
+    const sisteDelvilkår: IDelvilkår =
+        vurdering.delvilkårVurderinger[vurdering.delvilkårVurderinger.length - 1];
     return (
         <StyledEndreVurdering>
-            {nesteDelvilkår &&
-                vurdering.delvilkårVurderinger.map((delvilkår) => {
-                    if (erPåDelvilkårSomSkalVurderes) {
-                        return null;
-                    }
-                    if (nesteDelvilkår.type === delvilkår.type) {
-                        erPåDelvilkårSomSkalVurderes = true;
-                    }
-                    return (
-                        <RadioGruppe legend={delvilkårTypeTilTekst[delvilkår.type]}>
-                            {[VilkårResultat.JA, VilkårResultat.NEI].map((vilkårResultat) => (
-                                <Radio
-                                    key={vilkårResultat}
-                                    label={vilkårsResultatTypeTilTekst[vilkårResultat]}
-                                    name={delvilkår.type}
-                                    onChange={() =>
-                                        settVurdering(
-                                            oppdaterDelvilkår(vurdering, {
-                                                type: delvilkår.type,
-                                                resultat: vilkårResultat,
-                                            })
-                                        )
-                                    }
-                                    value={vilkårResultat}
-                                    checked={delvilkår.resultat === vilkårResultat}
-                                />
-                            ))}
-                        </RadioGruppe>
-                    );
-                })}
-            {/*            <RadioGruppe legend={config.vilkår}>
-                {[VilkårResultat.JA, VilkårResultat.NEI].map((vilkårResultat) => (
-                    <Radio
-                        key={vilkårResultat}
-                        label={vilkårsResultatTypeTilTekst[vilkårResultat]}
-                        name={vurdering.vilkårType}
-                        onChange={() => {
-                            const oppdaterUnntak = () =>
-                                vilkårResultat === VilkårResultat.NEI
-                                    ? undefined
-                                    : vurdering.unntak;
-                            settVurdering({
-                                ...vurdering,
-                                resultat: vilkårResultat,
-                                unntak: oppdaterUnntak(),
-                            });
-                        }}
-                        value={vilkårResultat}
-                        checked={vurdering.resultat === vilkårResultat}
-                    />
-                ))}
-            </RadioGruppe>*/}
-            {config.unntak && vurdering.resultat !== VilkårResultat.NEI && (
+            {vurdering.delvilkårVurderinger.map((delvilkår) => {
+                if (harPassertSisteDelvilkårSomSkalVises) {
+                    return null;
+                }
+                if (
+                    nesteDelvilkår?.type === delvilkår.type ||
+                    delvilkår.resultat === VilkårResultat.JA
+                ) {
+                    harPassertSisteDelvilkårSomSkalVises = true;
+                }
+                return (
+                    <RadioGruppe legend={delvilkårTypeTilTekst[delvilkår.type]}>
+                        {[VilkårResultat.JA, VilkårResultat.NEI].map((vilkårResultat) => (
+                            <Radio
+                                key={vilkårResultat}
+                                label={vilkårsResultatTypeTilTekst[vilkårResultat]}
+                                name={delvilkår.type}
+                                onChange={() =>
+                                    settVurdering(
+                                        oppdaterDelvilkår(vurdering, {
+                                            type: delvilkår.type,
+                                            resultat: vilkårResultat,
+                                        })
+                                    )
+                                }
+                                value={vilkårResultat}
+                                checked={delvilkår.resultat === vilkårResultat}
+                            />
+                        ))}
+                    </RadioGruppe>
+                );
+            })}
+            {config.unntak && sisteDelvilkår.resultat === VilkårResultat.NEI && (
                 <Select
                     label="Unntak"
                     value={vurdering.unntak || undefined}
