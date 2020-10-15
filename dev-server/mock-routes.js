a4aed39da9821b0d44f547240f4b4ab423560fb6
Johan Blomgren <johan.blomgren@nav.no>
2020-10-15 15:05:23 +0200

    Slettet mocking av sak-deler

--- dev-server/mock-routes.js
+++ dev-server/mock-routes.js
@@ -34,16 +34,6 @@ app.post('/familie-ef-sak/api/personopplysninger', (req, res) => {
     setTimeout(() => res.send(lesMockFil(filnavn)), delayMs);
 });
 
-app.get('/familie-ef-sak/api/sak/:id', (req, res) => {
-    const filnavn = `sak.json`;
-    setTimeout(() => res.send(lesMockFil(filnavn)), delayMs);
-});
-
-app.post('/familie-ef-sak/api/saksok/ident', (req, res) => {
-    const filnavn = req.body.personIdent === '12345678910' ? `saksøk.json` : `saksøk-feil.json`;
-    setTimeout(() => res.send(lesMockFil(filnavn)), delayMs);
-});
-
 app.get('/familie-ef-sak/api/behandling/2/vedtak-html', (req, res) => {
     setTimeout(
         () =>
