7d9c9658fb22e5ea636cf2dd5adcf05bf0f25285
eirarset <eirik.arseth@nav.no>
2020-10-22 13:34:43 +0200

    Henter og bruker personident fra journalførings-requesten

--- src/frontend/hooks/useHentJournalpost.ts
+++ src/frontend/hooks/useHentJournalpost.ts
@@ -1,13 +1,15 @@
 import { byggHenterRessurs, byggTomRessurs, Ressurs } from '../typer/ressurs';
 import { useApp } from '../context/AppContext';
 import { useCallback, useState } from 'react';
-import { IJournalpost } from '../typer/journalforing';
+import { IJojurnalpostResponse } from '../typer/journalforing';
 import { AxiosRequestConfig } from 'axios';
 import { OrNothing } from './felles/useSorteringState';
 
 export const useHentJournalpost = (journalpostIdParam: OrNothing<string>) => {
     const { axiosRequest, innloggetSaksbehandler } = useApp();
-    const [journalpost, settJournalpost] = useState<Ressurs<IJournalpost>>(byggTomRessurs());
+    const [journalResponse, settJournalResponse] = useState<Ressurs<IJojurnalpostResponse>>(
+        byggTomRessurs()
+    );
 
     const hentJournalpostConfig: AxiosRequestConfig = {
         method: 'GET',
@@ -15,15 +17,15 @@ export const useHentJournalpost = (journalpostIdParam: OrNothing<string>) => {
     };
 
     const hentJournalPost = useCallback(() => {
-        settJournalpost(byggHenterRessurs());
-        axiosRequest<IJournalpost, null>(
+        settJournalResponse(byggHenterRessurs());
+        axiosRequest<IJojurnalpostResponse, null>(
             hentJournalpostConfig,
             innloggetSaksbehandler
-        ).then((res: Ressurs<IJournalpost>) => settJournalpost(res));
+        ).then((res: Ressurs<IJojurnalpostResponse>) => settJournalResponse(res));
     }, [hentJournalpostConfig]);
 
     return {
         hentJournalPost,
-        journalpost,
+        journalResponse,
     };
 };
--- src/frontend/komponenter/Journalforing/Brukerinfo.tsx
+++ src/frontend/komponenter/Journalforing/Brukerinfo.tsx
@@ -1,16 +1,15 @@
 import { Normaltekst, Systemtittel } from 'nav-frontend-typografi';
 import React from 'react';
-import { BrukerInfo } from '../../typer/journalforing';
 
 interface BrukerinfoProps {
-    bruker: BrukerInfo;
+    personIdent: string;
 }
 
 const Brukerinfo: React.FC<BrukerinfoProps> = (props) => {
     return (
         <>
-            <Systemtittel>Bruker</Systemtittel>
-            <Normaltekst>{props.bruker.id}</Normaltekst>
+            <Systemtittel>Fødselsnummer</Systemtittel>
+            <Normaltekst>{props.personIdent}</Normaltekst>
         </>
     );
 };
