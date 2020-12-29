d0a5a4d8070e5cf0c38d30c9ba2ca1aaaae36a40
Johan Blomgren <johan.blomgren@nav.no>
2020-12-29 13:21:36 +0100

    Setter env

--- src/backend/konfigurerApp.ts
+++ src/backend/konfigurerApp.ts
@@ -1,9 +1,12 @@
 import dotenv from 'dotenv';
-if (process.env.NODE_ENV === 'production') {
-    dotenv.config({ path: '/var/run/secrets/nais.io/vault/.env' });
-} else {
-    dotenv.config();
-}
+dotenv.config();
+
+// felles-backend bruker andre variabler enn det som blir satt opp av azureAd
+const settAzureAdProps = () => {
+    process.env.AAD_DISCOVERY_URL = process.env.AZURE_APP_WELL_KNOWN_URL;
+    process.env.CLIENT_ID = process.env.AZURE_APP_CLIENT_ID;
+    process.env.CLIENT_SECRET = process.env.AZURE_APP_CLIENT_SECRET;
+};
 
 const konfigurerAzure = () => {
     const host = 'ensligmorellerfar';
@@ -20,13 +23,13 @@ const konfigurerAzure = () => {
             break;
         case 'preprod':
             process.env.AAD_LOGOUT_REDIRECT_URL = `https://login.microsoftonline.com/navq.onmicrosoft.com/oauth2/logout?post_logout_redirect_uri=https:\\\\${host}.dev.intern.nav.no`;
-            process.env.AAD_REDIRECT_URL = `https://${host}.dev.intern.nav.no/auth/openid/callback`;
-            process.env.AAD_DISCOVERY_URL = process.env.AZURE_APP_WELL_KNOWN_URL;
+            process.env.AAD_REDIRECT_URL = `https://${host}.dev.intern.nav.no/oauth2/callback`;
+            settAzureAdProps();
             break;
         case 'production':
             process.env.AAD_LOGOUT_REDIRECT_URL = `https://login.microsoftonline.com/navno.onmicrosoft.com/oauth2/logout?post_logout_redirect_uri=https:\\\\${host}.intern.nav.no`;
-            process.env.AAD_REDIRECT_URL = `https://${host}.intern.nav.no/auth/openid/callback`;
-            process.env.AAD_DISCOVERY_URL = process.env.AZURE_APP_WELL_KNOWN_URL;
+            process.env.AAD_REDIRECT_URL = `https://${host}.intern.nav.no/oauth2/callback`;
+            settAzureAdProps();
             break;
         default:
             break;
