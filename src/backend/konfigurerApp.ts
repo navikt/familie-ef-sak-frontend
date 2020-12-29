5d117327b80c56f455403301f863b2df5747992a
Johan Blomgren <johan.blomgren@nav.no>
2020-12-29 18:06:48 +0100

    Slettet litt for mye, legger tilbake GRAPH_API

--- src/backend/konfigurerApp.ts
+++ src/backend/konfigurerApp.ts
@@ -16,20 +16,24 @@ const konfigurerAzure = () => {
             process.env.AAD_LOGOUT_REDIRECT_URL = `https://login.microsoftonline.com/navq.onmicrosoft.com/oauth2/logout?post_logout_redirect_uri=http:\\\\localhost:8000`;
             process.env.AAD_REDIRECT_URL = `http://localhost:8000${callbackPath}`;
             process.env.AAD_DISCOVERY_URL = `https://login.microsoftonline.com/navq.onmicrosoft.com/v2.0/.well-known/openid-configuration`;
+            process.env.GRAPH_API = 'https://graph.microsoft.com/v1.0/me';
             break;
         case 'e2e':
             process.env.AAD_LOGOUT_REDIRECT_URL = `http://host.docker.internal:1111/v2.0/logout?post_logout_redirect_uri=http:\\\\localhost:8000`;
             process.env.AAD_REDIRECT_URL = `http://host.docker.internal:8000${callbackPath}`;
             process.env.AAD_DISCOVERY_URL = `http://host.docker.internal:1111/v2.0/.well-known/openid-configuration`;
+            process.env.GRAPH_API = 'http://familie-mock-server:1337/graph/me';
             break;
         case 'preprod':
             process.env.AAD_LOGOUT_REDIRECT_URL = `https://login.microsoftonline.com/navq.onmicrosoft.com/oauth2/logout?post_logout_redirect_uri=https:\\\\${host}.dev.intern.nav.no`;
             process.env.AAD_REDIRECT_URL = `https://${host}.dev.intern.nav.no${callbackPath}`;
+            process.env.GRAPH_API = 'https://graph.microsoft.com/v1.0/me';
             settAzureAdPropsFraEnv();
             break;
         case 'production':
             process.env.AAD_LOGOUT_REDIRECT_URL = `https://login.microsoftonline.com/navno.onmicrosoft.com/oauth2/logout?post_logout_redirect_uri=https:\\\\${host}.intern.nav.no`;
             process.env.AAD_REDIRECT_URL = `https://${host}.intern.nav.no${callbackPath}`;
+            process.env.GRAPH_API = 'https://graph.microsoft.com/v1.0/me';
             settAzureAdPropsFraEnv();
             break;
         default:
