2deab0e77867b832f9a5c2156c3f53b390c6377c
My Thao Nguyen <my.thao.nguyen@nav.no>
2021-01-03 19:17:00 +0100

    Fra ?: til ||

--- src/frontend/ikoner/RedigerBlyant.tsx
+++ src/frontend/ikoner/RedigerBlyant.tsx
@@ -18,8 +18,8 @@ const RedigerBlyant: React.FunctionComponent<ISøknad> = ({
         <svg
             aria-labelledby={'rediger'}
             className={className}
-            height={heigth ? heigth : 19}
-            width={width ? width : 19}
+            height={heigth || 19}
+            width={width || 19}
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
         >
