4c869c20d23bdec9ded9e6114325c4db63e1c073
Johan Blomgren <blommish@users.noreply.github.com>
2021-01-22 10:54:07 +0100

    Bruker steg i Steg.ts

--- src/frontend/typer/fagsak.ts
+++ src/frontend/typer/fagsak.ts
@@ -1,6 +1,7 @@
 import { Stønadstype } from './behandlingstema';
 import { Behandlingstype } from './behandlingstype';
 import { BehandlingStatus } from './behandlingstatus';
+import { Steg } from '../komponenter/Høyremeny/Steg';
 
 export interface Fagsak {
     id: string;
@@ -16,19 +17,4 @@ export interface Behandling {
     steg: Steg;
     status: BehandlingStatus;
     sistEndret: string;
-}
-
-export const enum Steg {
-    REGISTRERE_OPPLYSNINGER = 'REGISTRERE_OPPLYSNINGER',
-    VILKÅRSVURDERE_INNGANGSVILKÅR = 'VILKÅRSVURDERE_INNGANGSVILKÅR',
-    VILKÅRSVURDERE_STØNAD = 'VILKÅRSVURDERE_STØNAD',
-    BEREGNE_YTELSE = 'BEREGNE_YTELSE',
-    SEND_TIL_BESLUTTER = 'SEND_TIL_BESLUTTER',
-    BESLUTTE_VEDTAK = 'BESLUTTE_VEDTAK',
-    IVERKSETT_MOT_OPPDRAG = 'IVERKSETT_MOT_OPPDRAG',
-    VENTE_PÅ_STATUS_FRA_ØKONOMI = 'VENTE_PÅ_STATUS_FRA_ØKONOMI',
-    JOURNALFØR_VEDTAKSBREV = 'JOURNALFØR_VEDTAKSBREV',
-    DISTRIBUER_VEDTAKSBREV = 'DISTRIBUER_VEDTAKSBREV',
-    FERDIGSTILLE_BEHANDLING = 'FERDIGSTILLE_BEHANDLING',
-    BEHANDLING_FERDIGSTILT = 'BEHANDLING_FERDIGSTILT',
 }
