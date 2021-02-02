e63829741d870e6c1db8879a8cb212ab19918690
charliemidtlyng <charlie.midtlyng@bekk.no>
2021-02-02 14:23:36 +0100

    Lagt til felter for visning av aleneomsorg

--- src/frontend/komponenter/Behandling/Inngangsvilkår/Aleneomsorg/AnnenForelder.tsx
+++ src/frontend/komponenter/Behandling/Inngangsvilkår/Aleneomsorg/AnnenForelder.tsx
@@ -1,5 +1,12 @@
 import React, { FC } from 'react';
-import { IAleneomsorgSøknadsgrunnlag, IAnnenForelderAleneomsorg } from './typer';
+import {
+    borAnnenForelderISammeHusTilTekst,
+    harSamværMedBarnTilTekst,
+    harSkriftligSamværsavtaleTilTekst,
+    hvorMyeSammenTilTekst,
+    IAleneomsorgSøknadsgrunnlag,
+    IAnnenForelderAleneomsorg,
+} from './typer';
 import { Søknadsgrunnlag } from '../../../Felleskomponenter/Visning/DataGrunnlagIkoner';
 import { Normaltekst } from 'nav-frontend-typografi';
 import { BooleanTekst } from '../../../Felleskomponenter/Visning/StyledTekst';
@@ -41,11 +48,85 @@ const AnnenForelder: FC<Props> = ({ forelderRegister, søknadsgrunnlag }) => {
                     <BooleanTekst value={søknadsgrunnlag.spørsmålAvtaleOmDeltBosted} />
                 </>
             )}
-            {søknadsgrunnlag.skalAnnenForelderHaSamvær !== '' && (
+            {søknadsgrunnlag.skalAnnenForelderHaSamvær && (
                 <>
                     <Søknadsgrunnlag />
                     <Normaltekst>Annen forelders samvær</Normaltekst>
-                    <Normaltekst>{søknadsgrunnlag.skalAnnenForelderHaSamvær}</Normaltekst>
+                    <Normaltekst>
+                        {harSamværMedBarnTilTekst[søknadsgrunnlag.skalAnnenForelderHaSamvær]}
+                    </Normaltekst>
+                </>
+            )}
+            {søknadsgrunnlag.harDereSkriftligAvtaleOmSamvær && (
+                <>
+                    <Søknadsgrunnlag />
+                    <Normaltekst>Skriftlig samværsavtale</Normaltekst>
+                    <Normaltekst>
+                        {
+                            harSkriftligSamværsavtaleTilTekst[
+                                søknadsgrunnlag.harDereSkriftligAvtaleOmSamvær
+                            ]
+                        }
+                    </Normaltekst>
+                </>
+            )}
+            {søknadsgrunnlag.hvordanPraktiseresSamværet && (
+                <>
+                    <Søknadsgrunnlag />
+                    <Normaltekst>Praktisering av samværet</Normaltekst>
+                    <Normaltekst>{søknadsgrunnlag.hvordanPraktiseresSamværet}</Normaltekst>
+                </>
+            )}
+            {søknadsgrunnlag.borAnnenForelderISammeHus && (
+                <>
+                    <Søknadsgrunnlag />
+                    <Normaltekst>Foreldre har nære boforhold</Normaltekst>
+                    <Normaltekst>
+                        {
+                            borAnnenForelderISammeHusTilTekst[
+                                søknadsgrunnlag.borAnnenForelderISammeHus
+                            ]
+                        }
+                    </Normaltekst>
+                </>
+            )}
+            {søknadsgrunnlag.borAnnenForelderISammeHusBeskrivelse && (
+                <>
+                    <Søknadsgrunnlag />
+                    <Normaltekst>Bo begrunnelse</Normaltekst>
+                    <Normaltekst>
+                        {søknadsgrunnlag.borAnnenForelderISammeHusBeskrivelse}
+                    </Normaltekst>
+                </>
+            )}
+            {søknadsgrunnlag.harDereTidligereBoddSammen && (
+                <>
+                    <Søknadsgrunnlag />
+                    <Normaltekst>Foreldre har bodd sammen</Normaltekst>
+                    <BooleanTekst value={søknadsgrunnlag.harDereTidligereBoddSammen} />
+                </>
+            )}
+            {søknadsgrunnlag.nårFlyttetDereFraHverandre && (
+                <>
+                    <Søknadsgrunnlag />
+                    <Normaltekst>Fraflyttingsdato</Normaltekst>
+                    <Normaltekst>{søknadsgrunnlag.nårFlyttetDereFraHverandre}</Normaltekst>
+                </>
+            )}
+            {søknadsgrunnlag.hvorMyeErDuSammenMedAnnenForelder && (
+                <>
+                    <Søknadsgrunnlag />
+                    <Normaltekst>Foreldres kontakt</Normaltekst>
+                    <Normaltekst>
+                        {hvorMyeSammenTilTekst[søknadsgrunnlag.hvorMyeErDuSammenMedAnnenForelder]}
+                    </Normaltekst>
+                </>
+            )}
+            {søknadsgrunnlag.beskrivSamværUtenBarn && (
+                <>
+                    <Søknadsgrunnlag />
+                    <Normaltekst>Kontakt begrunnelse</Normaltekst>
+                    <Normaltekst>{søknadsgrunnlag.beskrivSamværUtenBarn}</Normaltekst>
                 </>
             )}
         </>
--- src/frontend/komponenter/Behandling/Inngangsvilkår/Aleneomsorg/typer.ts
+++ src/frontend/komponenter/Behandling/Inngangsvilkår/Aleneomsorg/typer.ts
@@ -10,18 +10,18 @@ export interface IAleneomsorgSøknadsgrunnlag {
     fødselTermindato?: string;
     erBarnetFødt: boolean;
     harSammeAdresse?: boolean;
-    skalBoBorHosSøker?: string;
+    skalBoBorHosSøker?: ESkalBarnetBoHosSøker;
     forelder?: IAnnenForelderAleneomsorg;
     ikkeOppgittAnnenForelderBegrunnelse?: string;
     spørsmålAvtaleOmDeltBosted?: boolean;
-    skalAnnenForelderHaSamvær?: string; // TODO: enum?
-    harDereSkriftligAvtaleOmSamvær?: string; // TODO: enum
-    hvordanPraktiseresSamværet?: string; // TODO: enum
-    borAnnenForelderISammeHus?: string; // TODO: enum
+    skalAnnenForelderHaSamvær?: EHarSamværMedBarn;
+    harDereSkriftligAvtaleOmSamvær?: EHarSkriftligSamværsavtale;
+    hvordanPraktiseresSamværet?: string; // Tekst
+    borAnnenForelderISammeHus?: EBorAnnenForelderISammeHus;
     borAnnenForelderISammeHusBeskrivelse?: string;
     harDereTidligereBoddSammen?: boolean;
     nårFlyttetDereFraHverandre?: string;
-    hvorMyeErDuSammenMedAnnenForelder?: string; // TODO: enum
+    hvorMyeErDuSammenMedAnnenForelder?: EHvorMyeSammen;
     beskrivSamværUtenBarn?: string;
 }
 export interface IAnnenForelderAleneomsorg {
@@ -39,10 +39,65 @@ export interface IAleneomsorgRegistergrunnlag {
     forelder?: IAnnenForelderAleneomsorg;
 }
 
-// export enum EÅrsakEnslig {
-//     samlivsbruddForeldre = 'samlivsbruddForeldre',
-//     samlivsbruddAndre = 'samlivsbruddAndre',
-//     aleneFraFødsel = 'aleneFraFødsel',
-//     endringISamværsordning = 'endringISamværsordning',
-//     dødsfall = 'dødsfall',
-// }
+export enum EHarSamværMedBarn {
+    jaIkkeMerEnnVanlig = 'jaIkkeMerEnnVanlig',
+    jaMerEnnVanlig = 'jaMerEnnVanlig',
+    nei = 'nei',
+}
+
+export const harSamværMedBarnTilTekst: Record<EHarSamværMedBarn, string> = {
+    jaIkkeMerEnnVanlig:
+        'Ja, men ikke mer enn én ettermiddag i uken med overnatting og annenhver helg eller tilsvarende',
+    jaMerEnnVanlig:
+        'Ja, mer enn én ettermiddag i uken med overnatting og annenhver helg eller tilsvarende',
+    nei: 'Nei, den andre forelderen har ikke samvær med barnet',
+};
+
+export enum EHarSkriftligSamværsavtale {
+    jaKonkreteTidspunkter = 'jaKonkreteTidspunkter',
+    jaIkkeKonkreteTidspunkter = 'jaIkkeKonkreteTidspunkter',
+    nei = 'nei',
+}
+
+export const harSkriftligSamværsavtaleTilTekst: Record<EHarSkriftligSamværsavtale, string> = {
+    jaKonkreteTidspunkter: 'Ja, den beskriver  når barnet er sammen med hver av foreldrene',
+    jaIkkeKonkreteTidspunkter:
+        'Ja, men den beskriver ikke når barnet er sammen med hver av foreldrene',
+    nei: 'Nei',
+};
+
+export enum EBorAnnenForelderISammeHus {
+    ja = 'ja',
+    nei = 'nei',
+    vetikke = 'vetikke',
+}
+
+export const borAnnenForelderISammeHusTilTekst: Record<EBorAnnenForelderISammeHus, string> = {
+    ja: 'Ja',
+    nei: 'Nei',
+    vetikke: 'Vet ikke hvor den andre forelderen bor',
+};
+
+export enum EHvorMyeSammen {
+    møtesIkke = 'møtesIkke',
+    kunNårLeveres = 'kunNårLeveres',
+    møtesUtenom = 'møtesUtenom',
+}
+
+export const hvorMyeSammenTilTekst: Record<EHvorMyeSammen, string> = {
+    møtesIkke: 'Møtes ikke',
+    kunNårLeveres: 'Møtes kun når barnet skal hentes eller leveres',
+    møtesUtenom: 'Møtes også utenom henting og levering',
+};
+
+export enum ESkalBarnetBoHosSøker {
+    ja = 'ja',
+    jaMenSamarbeiderIkke = 'jaMenSamarbeiderIkke',
+    nei = 'nei',
+}
+
+export const skalBarnetBoHosSøkerTilTekst: Record<ESkalBarnetBoHosSøker, string> = {
+    ja: 'Ja, og vi har eller skal registrere i Folkeregisteret',
+    nei: 'Nei',
+    jaMenSamarbeiderIkke: 'Ja, men den andre forelderen samarbeider ikke om adresseendring',
+};
