d4131b07b6a1a49c6a9f71f6d5a40967b08b1307
alexandraback <alexandra1.back@gmail.com>
2021-02-04 22:06:18 +0100

    Refaktoreringer

---
+++ src/frontend/hooks/useOppgave.ts
@@ -0,0 +1,63 @@
+import { RessursFeilet, RessursStatus, RessursSuksess } from '../typer/ressurs';
+import { useApp } from '../context/AppContext';
+import { useHistory } from 'react-router-dom';
+import { IOppgave } from '../komponenter/Oppgavebenk/oppgave';
+import { useState } from 'react';
+
+interface OppgaveDto {
+    behandlingId: string;
+    gsakId: string;
+}
+
+export const useOppgave = (oppgave: IOppgave) => {
+    const { axiosRequest, innloggetSaksbehandler } = useApp();
+    const history = useHistory();
+    const [feilmelding, settFeilmelding] = useState<string>();
+
+    const settOppgaveTilSaksbehandler = () => {
+        return axiosRequest<string, null>({
+            method: 'POST',
+            url: `/familie-ef-sak/api/oppgave/${oppgave.id}/fordel?saksbehandler=${innloggetSaksbehandler?.navIdent}`,
+        }).then((res: RessursSuksess<string> | RessursFeilet) => {
+            if (res.status === RessursStatus.SUKSESS) {
+                return Promise.resolve();
+            } else {
+                return Promise.reject(new Error(res.frontendFeilmelding));
+            }
+        });
+    };
+
+    const gåTilBehandleSakOppgave = () => {
+        axiosRequest<OppgaveDto, { oppgaveId: string }>({
+            method: 'GET',
+            url: `/familie-ef-sak/api/oppgave/${oppgave.id}`,
+        })
+            .then((res: RessursSuksess<OppgaveDto> | RessursFeilet) => {
+                if (res.status === RessursStatus.SUKSESS) {
+                    return Promise.resolve(res.data.behandlingId);
+                }
+                return Promise.reject(new Error(res.frontendFeilmelding));
+            })
+            .then((behandlingId) => {
+                settOppgaveTilSaksbehandler().then(() =>
+                    history.push(`/behandling/${behandlingId}`)
+                );
+            })
+            .catch((error) => settFeilmelding(error));
+    };
+
+    const gåTilJournalføring = () => {
+        settOppgaveTilSaksbehandler().then(() =>
+            history.push(
+                `/journalfor?journalpostId=${oppgave.journalpostId}&oppgaveId=${oppgave.id}`
+            )
+        );
+    };
+
+    return {
+        feilmelding,
+        settFeilmelding,
+        gåTilBehandleSakOppgave,
+        gåTilJournalføring,
+    };
+};
