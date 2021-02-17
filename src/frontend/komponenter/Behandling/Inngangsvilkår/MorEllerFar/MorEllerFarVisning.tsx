69d7590df4c3fff94a5575f4246f7f732245213a
Neringa Altanaite <neringa.altanaite@nav.no>
2021-02-17 13:15:52 +0100

    Oppdatert med vilkårsvurdering

--- src/frontend/komponenter/Behandling/Inngangsvilkår/MorEllerFar/MorEllerFarVisning.tsx
+++ src/frontend/komponenter/Behandling/Inngangsvilkår/MorEllerFar/MorEllerFarVisning.tsx
@@ -9,7 +9,6 @@ import {
     Søknadsgrunnlag,
 } from '../../../Felleskomponenter/Visning/DataGrunnlagIkoner';
 import { formaterNullableFødsesnummer, formaterNullableIsoDato } from '../../../../utils/formatter';
-
 interface Props {
     barnMedSamvær: IBarnMedSamvær[];
     vilkårStatus: VilkårStatus;
@@ -17,64 +16,78 @@ interface Props {
 
 const MorEllerFarVisning: FC<Props> = ({ barnMedSamvær, vilkårStatus }) => {
     return (
-        <StyledTabell>
-            <VilkårStatusIkon className={'vilkårStatusIkon'} vilkårStatus={vilkårStatus} />
-            <div className="tittel">
-                <Undertittel>Mor eller Far</Undertittel>
-                <EtikettLiten>§15-4</EtikettLiten>
-            </div>
-            {barnMedSamvær.map((barn: IBarnMedSamvær) => {
+        <>
+            {barnMedSamvær.map((barn: IBarnMedSamvær, indeks) => {
                 const { søknadsgrunnlag, registergrunnlag } = barn;
                 const barnUtenNavn: string = søknadsgrunnlag.erBarnetFødt
                     ? 'Ikke fylt ut'
                     : 'Ikke født ennå';
                 return (
                     <>
-                        {registergrunnlag.navn ? (
-                            <>
-                                <LiteBarn />
-                                <Element>{registergrunnlag.navn}</Element>
-                            </>
-                        ) : (
-                            <>
-                                <LiteBarn />
-                                <Element>
-                                    {søknadsgrunnlag.navn ? søknadsgrunnlag.navn : barnUtenNavn}
-                                </Element>
-                            </>
-                        )}
-
-                        {registergrunnlag.fødselsnummer ? (
-                            <>
-                                <Registergrunnlag />
-                                <Normaltekst>Fødsels- eller D-nummer</Normaltekst>
-                                <Normaltekst>
-                                    {formaterNullableFødsesnummer(registergrunnlag.fødselsnummer)}
-                                </Normaltekst>
-                            </>
-                        ) : søknadsgrunnlag.fødselsnummer ? (
-                            <>
-                                <Søknadsgrunnlag />
-                                <Normaltekst>Fødsels- eller D-nummer</Normaltekst>
-                                <Normaltekst>
-                                    {formaterNullableFødsesnummer(søknadsgrunnlag.fødselsnummer)}
-                                </Normaltekst>
-                            </>
-                        ) : (
-                            <>
-                                <Søknadsgrunnlag />
-                                <Normaltekst>
-                                    {søknadsgrunnlag.erBarnetFødt ? 'Fødselsdato' : 'Termindato'}
-                                </Normaltekst>
-                                <Normaltekst>
-                                    {formaterNullableIsoDato(søknadsgrunnlag.fødselTermindato)}
-                                </Normaltekst>
-                            </>
-                        )}
+                        <StyledTabell>
+                            {!indeks && (
+                                <>
+                                    <VilkårStatusIkon
+                                        className={'vilkårStatusIkon'}
+                                        vilkårStatus={vilkårStatus}
+                                    />
+                                    <div className="tittel">
+                                        <Undertittel>Mor eller Far</Undertittel>
+                                        <EtikettLiten>§15-4</EtikettLiten>
+                                    </div>
+                                </>
+                            )}
+                            {registergrunnlag.navn ? (
+                                <>
+                                    <LiteBarn />
+                                    <Element>{registergrunnlag.navn}</Element>
+                                </>
+                            ) : (
+                                <>
+                                    <LiteBarn />
+                                    <Element>
+                                        {søknadsgrunnlag.navn ? søknadsgrunnlag.navn : barnUtenNavn}
+                                    </Element>
+                                </>
+                            )}
+                            {registergrunnlag.fødselsnummer ? (
+                                <>
+                                    <Registergrunnlag />
+                                    <Normaltekst>Fødsels- eller D-nummer</Normaltekst>
+                                    <Normaltekst>
+                                        {formaterNullableFødsesnummer(
+                                            registergrunnlag.fødselsnummer
+                                        )}
+                                    </Normaltekst>
+                                </>
+                            ) : søknadsgrunnlag.fødselsnummer ? (
+                                <>
+                                    <Søknadsgrunnlag />
+                                    <Normaltekst>Fødsels- eller D-nummer</Normaltekst>
+                                    <Normaltekst>
+                                        {formaterNullableFødsesnummer(
+                                            søknadsgrunnlag.fødselsnummer
+                                        )}
+                                    </Normaltekst>
+                                </>
+                            ) : (
+                                <>
+                                    <Søknadsgrunnlag />
+                                    <Normaltekst>
+                                        {søknadsgrunnlag.erBarnetFødt
+                                            ? 'Fødselsdato'
+                                            : 'Termindato'}
+                                    </Normaltekst>
+                                    <Normaltekst>
+                                        {formaterNullableIsoDato(søknadsgrunnlag.fødselTermindato)}
+                                    </Normaltekst>
+                                </>
+                            )}
+                        </StyledTabell>
                     </>
                 );
             })}
-        </StyledTabell>
+        </>
     );
 };
 
