bd372d7badf2575b5a99f148358163eb22ff652e
alexandraback <alexandra1.back@gmail.com>
2021-02-19 20:27:54 +0100

    Man response fra backend til ISøkeresultat

--- src/frontend/komponenter/HeaderMedSøk/PersonSøk.tsx
+++ src/frontend/komponenter/HeaderMedSøk/PersonSøk.tsx
@@ -1,10 +1,29 @@
 import { ISøkeresultat, Søk } from '@navikt/familie-header';
 import React from 'react';
 import { useHistory } from 'react-router';
-import { byggFeiletRessurs, byggHenterRessurs, byggTomRessurs, Ressurs } from '../../typer/ressurs';
-import Søkeresultat from './Søkeresultat';
-import { ISakSøkPersonIdent } from '../../typer/saksøk';
+import {
+    byggHenterRessurs,
+    byggSuksessRessurs,
+    byggTomRessurs,
+    Ressurs,
+    RessursFeilet,
+    RessursStatus,
+    RessursSuksess,
+} from '../../typer/ressurs';
+import { ISaksøk, ISakSøkPersonIdent } from '../../typer/saksøk';
 import { useApp } from '../../context/AppContext';
+import { kjønnType } from '@navikt/familie-typer';
+import { KvinneIkon, MannIkon } from '@navikt/familie-ikoner';
+
+const tilSøkeresultatListe = (resultat: ISaksøk): ISøkeresultat[] => {
+    return resultat.fagsaker.map((fagsakId) => ({
+        harTilgang: true, //Hvis ikke tilgang så RessursFeilet.IkkeTilgang
+        ident: resultat.personIdent,
+        fagsakId,
+        navn: resultat.visningsnavn,
+        ikon: resultat.kjønn === kjønnType.MANN ? <MannIkon /> : <KvinneIkon />,
+    }));
+};
 
 const PersonSøk: React.FC = () => {
     const history = useHistory();
@@ -15,24 +34,24 @@ const PersonSøk: React.FC = () => {
         settResultat(byggTomRessurs());
     };
 
-    /* eslint-disable-next-line @typescript-eslint/no-empty-function */
-    const søkeresultatOnClick = () => {};
+    const søkeresultatOnClick = (søkeresultat: ISøkeresultat) => {
+        history.push(`/fagsak/${søkeresultat.fagsakId}`);
+    };
 
     const søk = (personIdent: string): void => {
         settResultat(byggHenterRessurs());
-        axiosRequest<ISøkeresultat[], ISakSøkPersonIdent>({
+        axiosRequest<ISaksøk, ISakSøkPersonIdent>({
             method: 'POST',
             url: `/familie-ef-sak/api/fagsak/sok/`,
             data: { personIdent: personIdent },
-        })
-            .then((response: Ressurs<ISøkeresultat[]>) => {
+        }).then((response: RessursSuksess<ISaksøk> | RessursFeilet) => {
+            if (response.status === RessursStatus.SUKSESS) {
+                const søkeresultater: ISøkeresultat[] = tilSøkeresultatListe(response.data);
+                settResultat(byggSuksessRessurs(søkeresultater));
+            } else {
                 settResultat(response);
-            })
-            .catch((error) => {
-                settResultat(
-                    byggFeiletRessurs('En ukjent feil oppstod ved henting av person', error)
-                );
-            });
+            }
+        });
     };
 
     return (
@@ -42,18 +61,6 @@ const PersonSøk: React.FC = () => {
             søkeresultater={resultat}
             nullstillSøkeresultater={nullstillResultat}
             søkeresultatOnClick={søkeresultatOnClick}
-            formaterResultat={(søkeresultat) => {
-                return (
-                    <Søkeresultat
-                        alder={20}
-                        navn={søkeresultat.navn}
-                        ident={søkeresultat.ident}
-                        onClick={() => {
-                            history.push(`/fagsak/${søkeresultat.fagsakId}`);
-                        }}
-                    />
-                );
-            }}
         />
     );
 };
--- src/frontend/typer/saksøk.ts
+++ src/frontend/typer/saksøk.ts
@@ -1,19 +1,14 @@
 // Interface
 
 import { kjønnType } from '@navikt/familie-typer';
-import { INavn } from './personopplysninger';
-
-export interface ISakListe {
-    saker: ISaksøk[];
-}
 
 export interface ISakSøkPersonIdent {
     personIdent: string;
 }
 
 export interface ISaksøk {
-    sakId: string;
-    navn: INavn;
+    fagsaker: string[];
+    visningsnavn: string;
     personIdent: string;
     kjønn: kjønnType;
 }
