d9337d459ff9bdf46ef7fa39eebfb082f92036be
ma10s <mattis.janitz@nav.no>
2021-03-05 09:56:00 +0100

    Fjern "lagreknapp" som ikke gjør noe

--- src/frontend/komponenter/Behandling/Blankett/BlankettFooter.tsx
+++ src/frontend/komponenter/Behandling/Blankett/BlankettFooter.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Hovedknapp, Knapp } from 'nav-frontend-knapper';
+import { Hovedknapp } from 'nav-frontend-knapper';
 import styled from 'styled-components';
 import navFarger from 'nav-frontend-core';
 import { useApp } from '../../../context/AppContext';
@@ -50,7 +50,6 @@ const BlankettFooter: React.FC<{ behandlingId: string }> = ({ behandlingId }) =>
     return (
         <Footer>
             <MittstildtInnhold>
-                <Knapp>Lagre</Knapp>
                 <StyledHovedknapp onClick={sendTilBeslutter}>Send til beslutter</StyledHovedknapp>
             </MittstildtInnhold>
         </Footer>
