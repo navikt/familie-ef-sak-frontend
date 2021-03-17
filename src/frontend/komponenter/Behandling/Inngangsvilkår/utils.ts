30ee21b9731d2d5cc5bda25baf915630200d10c9
My Thao Nguyen <my.thao.nguyen@nav.no>
2021-03-17 16:24:52 +0100

    🐛 Bruk riktig varNavn - personIdent i stedet for ident
    - Fiks fødselsnr er undefined selv om man får fnr fra backend

--- src/frontend/komponenter/Behandling/Inngangsvilkår/utils.ts
+++ src/frontend/komponenter/Behandling/Inngangsvilkår/utils.ts
@@ -5,9 +5,10 @@ export const hentBooleanTekst = (value: boolean): string => (value ? 'Ja' : 'Nei
 
 export const hentPersonInfo = (person?: IPersonDetaljer): string => {
     const erNavnUtfylt = person?.navn !== undefined && person?.navn !== null;
+    const hvisFinnesFnrEllerFdato =
+        person?.personIdent || formaterNullableIsoDato(person?.fødselsdato);
+
     return person && !erNavnUtfylt
         ? 'Ikke utfylt'
-        : `${person?.navn || ''} - ${
-              person?.ident || formaterNullableIsoDato(person?.fødselsdato) || 'Ikke utfylt'
-          }`;
+        : `${person?.navn || ''} - ${hvisFinnesFnrEllerFdato || 'Ikke utfylt'}`;
 };
