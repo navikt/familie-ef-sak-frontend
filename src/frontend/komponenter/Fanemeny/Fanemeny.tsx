634f120d70dba4cb1207c0a2127257072455d37d
trondil <56258085+throndi@users.noreply.github.com>
2021-03-17 11:02:09 +0100

    Toggler fane mellom blankett og brev basert på behandlingstype

--- src/frontend/komponenter/Fanemeny/Fanemeny.tsx
+++ src/frontend/komponenter/Fanemeny/Fanemeny.tsx
@@ -3,10 +3,14 @@ import { FC } from 'react';
 import styled from 'styled-components';
 import { useParams } from 'react-router';
 import { IBehandlingParams } from '../../typer/routing';
-import { sider } from './sider';
+import { ISide, SideId, sider } from './sider';
 import { Normaltekst } from 'nav-frontend-typografi';
 import { NavLink } from 'react-router-dom';
 import navFarger from 'nav-frontend-core';
+import { useBehandling } from '../../context/BehandlingContext';
+import { Behandling } from '../../typer/fagsak';
+import DataViewer from '../Felleskomponenter/DataViewer/DataViewer';
+import { Behandlingstype } from '../../typer/behandlingstype';
 
 const StyledFanemeny = styled.div`
     width: 100%;
@@ -44,19 +48,33 @@ const StyledNavLink = styled(NavLink)`
 `;
 const Fanemeny: FC = () => {
     const { behandlingId } = useParams<IBehandlingParams>();
+    const { behandling } = useBehandling();
+
+    const fanerForBlankettbehandling = (sider: ISide[], behandling: Behandling): ISide[] => {
+        if (behandling.type === Behandlingstype.BLANKETT) {
+            return sider.filter((side) => side.id !== SideId.BREV);
+        }
+        return sider.filter((side) => side.id !== SideId.BLANKETT);
+    };
 
     return (
-        <StyledFanemeny>
-            {sider.map((side) => (
-                <StyledNavLink
-                    key={side.navn}
-                    to={`/behandling/${behandlingId}/${side.href}`}
-                    activeClassName="aktiv"
-                >
-                    <Normaltekst>{side.navn}</Normaltekst>
-                </StyledNavLink>
-            ))}
-        </StyledFanemeny>
+        <DataViewer response={{ behandling }}>
+            {({ behandling }) => (
+                <>
+                    <StyledFanemeny>
+                        {fanerForBlankettbehandling(sider, behandling).map((side) => (
+                            <StyledNavLink
+                                key={side.navn}
+                                to={`/behandling/${behandlingId}/${side.href}`}
+                                activeClassName="aktiv"
+                            >
+                                <Normaltekst>{side.navn}</Normaltekst>
+                            </StyledNavLink>
+                        ))}
+                    </StyledFanemeny>
+                </>
+            )}
+        </DataViewer>
     );
 };
 
