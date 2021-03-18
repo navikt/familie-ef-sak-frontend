39149781ad79799a68b3235054c20516de4bf79d
alexandraback <alexandra1.back@gmail.com>
2021-03-18 10:56:57 +0100

    Legg til lagre knapp

--- src/frontend/komponenter/Behandling/Vurdering/EndreVurderingComponent.tsx
+++ src/frontend/komponenter/Behandling/Vurdering/EndreVurderingComponent.tsx
@@ -1,13 +1,15 @@
 import * as React from 'react';
-import { FC, useState } from 'react';
-import { BegrunnelseRegel, RegelId, Regler, RootVilkårsvar, Vilkårsvar } from './typer';
-import { VilkårType } from '../Inngangsvilkår/vilkår';
-import { leggTilRegelIdISvarliste } from './utils';
+import {FC, useState} from 'react';
+import {BegrunnelseRegel, RegelId, Regler, RootVilkårsvar, Vilkårsvar} from './typer';
+import {VilkårType} from '../Inngangsvilkår/vilkår';
+import {erAllaDelvilkårBesvarte, leggTilRegelIdISvarliste} from './utils';
 
-import { Radio, RadioGruppe, Textarea as TextareaNav } from 'nav-frontend-skjema';
+import {Radio, RadioGruppe, Textarea as TextareaNav} from 'nav-frontend-skjema';
 import hiddenIf from '../../Felleskomponenter/HiddenIf/hiddenIf';
+import {Hovedknapp} from "nav-frontend-knapper";
 
 const Textarea = hiddenIf(TextareaNav);
+const Lagreknapp = hiddenIf(Hovedknapp)
 /**
  * Skal resette undervilkår, men ikke rootnivå hvis en tidligere endrer seg
  */
@@ -17,7 +19,7 @@ const EndreVurderingComponent: FC<{
     regler: Regler;
     rotregler: string[];
     oppdaterVurdering: () => void;
-}> = ({ regler, rotregler, oppdaterVurdering }) => {
+}> = ({regler, rotregler, oppdaterVurdering}) => {
     const [vilkårsvar, settVilkårsvar] = useState<RootVilkårsvar>(
         rotregler.reduce((acc, rootregel) => {
             return leggTilRegelIdISvarliste(acc, rootregel);
@@ -25,12 +27,12 @@ const EndreVurderingComponent: FC<{
     );
 
     const oppdateterVilkårsvar = (rootRegelId: string, nySvarArray: Vilkårsvar[]) =>
-        settVilkårsvar((prevSvar) => ({ ...prevSvar, [rootRegelId]: [...nySvarArray] }));
+        settVilkårsvar((prevSvar) => ({...prevSvar, [rootRegelId]: [...nySvarArray]}));
 
     const oppdaterBegrunnelse = (rootRegelId: RegelId) => {
         const svarsliste = vilkårsvar[rootRegelId];
         return (nyttSvar: Vilkårsvar) => {
-            const { svarId, regelId } = nyttSvar;
+            const {svarId, regelId} = nyttSvar;
 
             const nySvarArray = svarsliste.map((v) =>
                 v.svarId === svarId && v.regelId === regelId ? nyttSvar : v
@@ -43,7 +45,7 @@ const EndreVurderingComponent: FC<{
     const oppdaterSvar = (rootRegelId: RegelId) => {
         const svarsliste = vilkårsvar[rootRegelId];
         return (nyttSvar: Vilkårsvar) => {
-            const { regelId, svarId } = nyttSvar;
+            const {regelId, svarId} = nyttSvar;
             const svarIndex = svarsliste.findIndex((s) => s.regelId === regelId);
             const nySvarArray = svarsliste.slice(0, svarIndex);
             const regel = regler[regelId];
@@ -60,11 +62,13 @@ const EndreVurderingComponent: FC<{
                 oppdateterVilkårsvar(rootRegelId, [
                     ...nySvarArray,
                     nyttSvar,
-                    { regelId: nesteStegId },
+                    {regelId: nesteStegId},
                 ]);
             }
         };
     };
+
+    console.log("vilkårsvar", vilkårsvar);
 
     return (
         <form
@@ -75,11 +79,11 @@ const EndreVurderingComponent: FC<{
             }}
         >
             {Object.entries(vilkårsvar).map((svarsNoder) => {
-                const regelId = svarsNoder[0];
-                const regel = regler[regelId];
-                const oppdaterSvarINod = oppdaterSvar(regelId);
-                const oppdaterBegrunnelseINod = oppdaterBegrunnelse(regelId);
+                const hovedRegel = svarsNoder[0];
+                const oppdaterSvarINod = oppdaterSvar(hovedRegel);
+                const oppdaterBegrunnelseINod = oppdaterBegrunnelse(hovedRegel);
                 return svarsNoder[1].map((svar) => {
+                    const regel = regler[svar.regelId];
                     const begrunnelse =
                         (svar.svarId && regel.svarMapping[svar.svarId].begrunnelse) ??
                         BegrunnelseRegel.UTEN;
@@ -95,7 +99,7 @@ const EndreVurderingComponent: FC<{
                                         value={svarId}
                                         checked={svarId === svar.svarId}
                                         onChange={() =>
-                                            oppdaterSvarINod({ ...svar, svarId, regelId })
+                                            oppdaterSvarINod({svarId, regelId: regel.regelId})
                                         }
                                     />
                                 ))}
@@ -117,6 +121,7 @@ const EndreVurderingComponent: FC<{
                     );
                 });
             })}
+            <Lagreknapp htmlType="submit" hidden={!erAllaDelvilkårBesvarte(vilkårsvar, regler)}>Lagre</Lagreknapp>
         </form>
     );
 };
