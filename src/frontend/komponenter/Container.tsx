7fde1c056ce3385dc133734d2c4a75c9c5ad4417
trondil <56258085+throndi@users.noreply.github.com>
2021-03-24 14:39:56 +0100

    Trekker fetching av toggles ut i TogglesContext

--- src/frontend/komponenter/Container.tsx
+++ src/frontend/komponenter/Container.tsx
@@ -1,7 +1,6 @@
 import { HeaderMedSøk } from './HeaderMedSøk/HeaderMedSøk';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import * as React from 'react';
-import { useEffect } from 'react';
 import { ISaksbehandler } from '../typer/saksbehandler';
 import { useApp } from '../context/AppContext';
 import UgyldigSesjon from './Felleskomponenter/Modal/SesjonUtløpt';
@@ -9,8 +8,6 @@ import BehandlingContainer from './Behandling/BehandlingContainer';
 import { OppgaveBenk } from '../sider/Oppgavebenk';
 import { Journalforing } from '../sider/Journalforing';
 import Fagsakoversikt from '../sider/Fagsakoversikt';
-import hentToggles from '../toggles/api';
-import { useToggles } from '../context/TogglesContext';
 
 interface IProps {
     innloggetSaksbehandler?: ISaksbehandler;
@@ -18,17 +15,6 @@ interface IProps {
 
 const Container: React.FC<IProps> = ({ innloggetSaksbehandler }) => {
     const { autentisert } = useApp();
-    const { settToggles } = useToggles();
-
-    useEffect(() => {
-        Promise.all([fetchToggles()]);
-    }, [innloggetSaksbehandler]);
-
-    const fetchToggles = () => {
-        return hentToggles(settToggles).catch((err: Error) => {
-            console.log('Kunne ikke hente toggles, ' + err.message);
-        });
-    };
 
     return (
         <Router>
