4ff517c07fc496ec6bb8cfbd71bd9faa9bbc9e11
Johan Blomgren <blommish@users.noreply.github.com>
2021-03-29 10:03:59 +0200

    Catcher feil for getOnBehalfOfAccessToken (#202)
    * Catcher feil som kastes når en bruker ikke har tilgang til applikasjonen

--- src/backend/proxy.ts
+++ src/backend/proxy.ts
@@ -1,4 +1,10 @@
-import { Client, getOnBehalfOfAccessToken } from '@navikt/familie-backend';
+import {
+    Client,
+    error,
+    getLogTimestamp,
+    getOnBehalfOfAccessToken,
+    info,
+} from '@navikt/familie-backend';
 import { NextFunction, Request, Response } from 'express';
 import { ClientRequest } from 'http';
 import { createProxyMiddleware } from 'http-proxy-middleware';
@@ -31,10 +37,27 @@ export const doProxy: any = () => {
 
 export const attachToken = (authClient: Client) => {
     return async (req: Request, _res: Response, next: NextFunction) => {
-        getOnBehalfOfAccessToken(authClient, req, oboConfig).then((accessToken: string) => {
-            req.headers['Nav-Call-Id'] = uuidv4();
-            req.headers.Authorization = `Bearer ${accessToken}`;
-            return next();
-        });
+        getOnBehalfOfAccessToken(authClient, req, oboConfig)
+            .then((accessToken: string) => {
+                req.headers['Nav-Call-Id'] = uuidv4();
+                req.headers.Authorization = `Bearer ${accessToken}`;
+                return next();
+            })
+            .catch((e) => {
+                if (e.error === 'invalid_grant') {
+                    info(`${getLogTimestamp()}: invalid_grant`);
+                    _res.status(500).json({
+                        status: 'IKKE_TILGANG',
+                        frontendFeilmelding:
+                            'Uventet feil. Det er mulig at du ikke har tilgang til applikasjonen.',
+                    });
+                } else {
+                    error(`${getLogTimestamp()}: Uventet feil - getOnBehalfOfAccessToken  ${e}`);
+                    _res.status(500).json({
+                        status: 'FEILET',
+                        frontendFeilmelding: 'Uventet feil. Vennligst prøv på nytt.',
+                    });
+                }
+            });
     };
 };
