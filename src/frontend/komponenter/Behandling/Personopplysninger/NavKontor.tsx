1d739c312f007ae3b643d90c6e9f3fd856995ead
eirarset <eirik.arseth@nav.no>
2021-04-20 22:59:39 +0200

    Style nav-kontor

--- src/frontend/komponenter/Behandling/Personopplysninger/NavKontor.tsx
+++ src/frontend/komponenter/Behandling/Personopplysninger/NavKontor.tsx
@@ -3,22 +3,38 @@ import { IngenData } from './TabellWrapper';
 import { INavKontor } from '../../../typer/personopplysninger';
 import Bygning from '../../../ikoner/Bygning';
 import styled from 'styled-components';
+import { Undertittel } from 'nav-frontend-typografi';
 
 const StyledNavKontor = styled.div`
     display: grid;
     padding-top: 3rem;
     grid-template-columns: 32px 40px auto 72px;
-    grid-template-areas: "'. ikon tittel' '. . kontor'";
+    grid-template-areas: '. ikon tittel' '. . kontor';
+    padding-bottom: 4rem;
+`;
+
+const StyledBygning = styled(Bygning)`
+    grid-area: ikon;
+`;
+
+const StyledTittel = styled(Undertittel)`
+    grid-area: tittel;
+`;
+
+const StyledKontorNavn = styled.div`
+    grid-area: kontor;
 `;
 
 const NavKontor: React.FC<{ navKontor?: INavKontor }> = ({ navKontor }) => {
     return (
         <StyledNavKontor>
-            <Bygning />
+            <StyledBygning />
             {(navKontor && (
                 <>
-                    NAV-Kontor
-                    {`${navKontor.enhetNr} - ${navKontor.navn}`})
+                    <StyledTittel>NAV-Kontor</StyledTittel>
+                    <StyledKontorNavn>
+                        {`${navKontor.enhetNr} - ${navKontor.navn}`}
+                    </StyledKontorNavn>
                 </>
             )) || <IngenData />}
         </StyledNavKontor>
