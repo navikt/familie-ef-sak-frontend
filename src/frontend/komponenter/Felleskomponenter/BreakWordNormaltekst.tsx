75ba468e05a1c72d2fd92ed944c34bd2910a952d
charliemidtlyng <charlie.midtlyng@bekk.no>
2021-05-18 16:55:57 +0200

    Skal vise begrunnelse i vedtak og totrinnskontroll med tekstbrytning fra saksbehandler

---
+++ src/frontend/komponenter/Felleskomponenter/BreakWordNormaltekst.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import { Normaltekst } from 'nav-frontend-typografi';
+
+export const BreakWordNormaltekst = styled(Normaltekst)`
+    white-space: pre-wrap;
+    word-wrap: break-word;
+`;
