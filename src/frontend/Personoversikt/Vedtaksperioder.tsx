f6fe84d3f6872d88de89d969860dc04a6033465f
Johan Blomgren <blommish@users.noreply.github.com>
2021-07-10 11:32:18 +0200

    Splitter opp komponent

--- src/frontend/Personoversikt/Vedtaksperioder.tsx
+++ src/frontend/Personoversikt/Vedtaksperioder.tsx
@@ -24,6 +24,62 @@ const Rad = styled.tr<{ type?: AndelEndringType }>`
     opacity: ${(props) => (props.type === AndelEndringType.FJERNET ? '50%' : '100%')};
 `;
 
+const VedtaksperioderTabell: React.FC<{ perioder: AndelHistorikk[] }> = ({ perioder }) => {
+    return (
+        <StyledTabell className="tabell">
+            <thead>
+                <tr>
+                    <th>Periode</th>
+                    <th>Inntektsgrunnlag</th>
+                    <th>Stønadsbeløp</th>
+                    <th>Vedtaksdato</th>
+                    <th>Saksbehandler</th>
+                    <th>Endring</th>
+                </tr>
+            </thead>
+            <tbody>
+                {perioder.map((periode) => (
+                    <Rad type={periode.endring?.type}>
+                        <td>
+                            {formaterNullableMånedÅr(periode.andel.stønadFra)}
+                            {' - '}
+                            {formaterNullableMånedÅr(periode.andel.stønadTil)}
+                        </td>
+                        <td>{formaterTallMedTusenSkille(periode.andel.inntekt)}</td>
+                        <td>{formaterTallMedTusenSkille(periode.andel.beløp)}</td>
+                        <td>
+                            {
+                                <Link
+                                    className="lenke"
+                                    to={{
+                                        pathname: `/behandling/${periode.behandlingId}`,
+                                    }}
+                                >
+                                    {formaterIsoDato(periode.vedtaksdato)}
+                                </Link>
+                            }
+                        </td>
+                        <td>{periode.saksbehandler}</td>
+                        <td>
+                            {periode.endring && (
+                                <Link
+                                    className="lenke"
+                                    to={{
+                                        pathname: `/behandling/${periode.endring.behandlingId}`,
+                                    }}
+                                >
+                                    {AndelHistorikkTypeTilTekst[periode.endring.type]} (
+                                    {formaterIsoDato(periode.endring.vedtaksdato)})
+                                </Link>
+                            )}
+                        </td>
+                    </Rad>
+                ))}
+            </tbody>
+        </StyledTabell>
+    );
+};
+
 const Vedtaksperioder: React.FC<{ fagsakId: string }> = ({ fagsakId }) => {
     const periodeHistorikkConfig: AxiosRequestConfig = useMemo(
         () => ({
@@ -36,59 +92,7 @@ const Vedtaksperioder: React.FC<{ fagsakId: string }> = ({ fagsakId }) => {
 
     return (
         <DataViewer response={{ perioder }}>
-            {({ perioder }) => (
-                <StyledTabell className="tabell">
-                    <thead>
-                        <tr>
-                            <th>Periode</th>
-                            <th>Inntektsgrunnlag</th>
-                            <th>Stønadsbeløp</th>
-                            <th>Vedtaksdato</th>
-                            <th>Saksbehandler</th>
-                            <th>Endring</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {perioder.map((periode) => (
-                            <Rad type={periode.endring?.type}>
-                                <td>
-                                    {formaterNullableMånedÅr(periode.andel.stønadFra)}
-                                    {' - '}
-                                    {formaterNullableMånedÅr(periode.andel.stønadTil)}
-                                </td>
-                                <td>{formaterTallMedTusenSkille(periode.andel.inntekt)}</td>
-                                <td>{formaterTallMedTusenSkille(periode.andel.beløp)}</td>
-                                <td>
-                                    {
-                                        <Link
-                                            className="lenke"
-                                            to={{
-                                                pathname: `/behandling/${periode.behandlingId}`,
-                                            }}
-                                        >
-                                            {formaterIsoDato(periode.vedtaksdato)}
-                                        </Link>
-                                    }
-                                </td>
-                                <td>{periode.saksbehandler}</td>
-                                <td>
-                                    {periode.endring && (
-                                        <Link
-                                            className="lenke"
-                                            to={{
-                                                pathname: `/behandling/${periode.endring.behandlingId}`,
-                                            }}
-                                        >
-                                            {AndelHistorikkTypeTilTekst[periode.endring.type]} (
-                                            {formaterIsoDato(periode.endring.vedtaksdato)})
-                                        </Link>
-                                    )}
-                                </td>
-                            </Rad>
-                        ))}
-                    </tbody>
-                </StyledTabell>
-            )}
+            {({ perioder }) => <VedtaksperioderTabell perioder={perioder} />}
         </DataViewer>
     );
 };
