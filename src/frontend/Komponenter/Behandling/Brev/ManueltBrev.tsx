1f5f79af3b075d6a88f3532b92eb183cabc81bdd
Asbjørn Steinskog <asbjorn@steinskog.me>
2021-09-13 21:09:09 +0200

    Legg til komponent for manuelt brev

---
+++ src/frontend/Komponenter/Behandling/Brev/ManueltBrev.tsx
@@ -0,0 +1,103 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Input } from 'nav-frontend-skjema';
+import Panel from 'nav-frontend-paneler';
+import { Textarea } from 'nav-frontend-skjema';
+import { Knapp } from 'nav-frontend-knapper';
+
+const StyledManueltBrev = styled.div`
+    width: 50%;
+    margin-bottom: 10rem;
+`;
+
+const Innholdsrad = styled(Panel)`
+    margin-top: 1rem;
+`;
+
+const StyledKnapp = styled(Knapp)`
+    margin-top: 2rem;
+`;
+
+//eslint-disable-next-line
+const ManueltBrev: React.FC = ({}) => {
+    const førsteRad = [
+        {
+            id: 1,
+            deloverskrift: '',
+            innhold: '',
+        },
+    ];
+
+    const [rader, settRader] = useState(førsteRad);
+
+    const leggTilRad = () => {
+        settRader((s: any) => {
+            return [
+                ...s,
+                {
+                    deloverskrift: '',
+                    innhold: '',
+                },
+            ];
+        });
+    };
+
+    const endreDeloverskrift = (e: any) => {
+        e.preventDefault();
+
+        const index = e.target.id;
+
+        settRader((s) => {
+            const newArr = s.slice();
+            newArr[index].deloverskrift = e.target.value;
+
+            return newArr;
+        });
+    };
+
+    const endreInnhold = (e: any) => {
+        e.preventDefault();
+
+        const index = e.target.id;
+
+        settRader((s) => {
+            const newArr = s.slice();
+            newArr[index].innhold = e.target.value;
+
+            return newArr;
+        });
+    };
+
+    console.log('rad', rader);
+
+    return (
+        <StyledManueltBrev>
+            <h1>Manuelt brev</h1>
+            <Input label="Overskrift" />
+
+            {rader.map((rad, i) => {
+                return (
+                    <Innholdsrad border>
+                        <Input
+                            onChange={endreDeloverskrift}
+                            label="Deloverskrift (valgfri)"
+                            id={i.toString()}
+                            value={rad.deloverskrift}
+                        />
+                        <Textarea
+                            onChange={endreInnhold}
+                            defaultValue=""
+                            label="Innhold"
+                            id={i.toString()}
+                            value={rad.innhold}
+                        />
+                    </Innholdsrad>
+                );
+            })}
+
+            <StyledKnapp onClick={leggTilRad}>Legg til rad</StyledKnapp>
+        </StyledManueltBrev>
+    );
+};
+
+export default ManueltBrev;
