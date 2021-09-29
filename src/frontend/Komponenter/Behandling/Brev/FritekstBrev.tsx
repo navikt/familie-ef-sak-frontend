b2e2ec80d2fc3467e32546d05130d0ecee0c4d73
eirarset <eirik.arseth@nav.no>
2021-09-29 13:44:06 +0200

    Skal automatisk forhåndsvise fritekstbrev og kegger til knapp for fjerneing av avsnitt

--- src/frontend/Komponenter/Behandling/Brev/FritekstBrev.tsx
+++ src/frontend/Komponenter/Behandling/Brev/FritekstBrev.tsx
@@ -1,35 +1,38 @@
-import React, { useMemo, useState, SyntheticEvent, ChangeEvent } from 'react';
+import React, { ChangeEvent, SyntheticEvent, useEffect, useMemo, useState } from 'react';
 import styled from 'styled-components';
-import { Input } from 'nav-frontend-skjema';
+import { Input, Textarea } from 'nav-frontend-skjema';
 import { IPersonopplysninger } from '../../../App/typer/personopplysninger';
 import Panel from 'nav-frontend-paneler';
-import { Textarea } from 'nav-frontend-skjema';
-import { Knapp, Hovedknapp } from 'nav-frontend-knapper';
+import { Knapp } from 'nav-frontend-knapper';
 import { useApp } from '../../../App/context/AppContext';
 import { Ressurs, RessursStatus } from '../../../App/typer/ressurs';
 import { AxiosRequestConfig } from 'axios';
 import { useDataHenter } from '../../../App/hooks/felles/useDataHenter';
-import { IFritekstBrev, IAvsnitt } from '../../../App/typer/brev';
+import { IAvsnitt, IFritekstBrev } from '../../../App/typer/brev';
 import { v4 as uuidv4 } from 'uuid';
+import { useDebouncedCallback } from 'use-debounce';
+import LenkeKnapp from '../../../Felles/Knapper/LenkeKnapp';
+import SlettSøppelkasse from '../../../Felles/Ikoner/SlettSøppelkasse';
 
 const StyledFrittståendeBrev = styled.div`
-    width: 50%;
     margin-bottom: 10rem;
 `;
 
 const Innholdsrad = styled(Panel)`
     margin-top: 1rem;
+    display: grid;
+    grid-template-columns: 90% 10%;
 `;
 
 const Knapper = styled.div`
     margin-top: 2rem;
-
     display: flex;
-    justify-content: space-between;
+    justify-content: space-evenly;
 `;
 
 const BrevKolonner = styled.div`
     display: flex;
+    flex-direction: column;
 `;
 
 type Props = {
@@ -116,6 +119,13 @@ const FritekstBrev: React.FC<Props> = ({ oppdaterBrevressurs, behandlingId, fags
         });
     };
 
+    const fjernRad = (radId: string) => {
+        return () =>
+            settAvsnitt((eksisterendeAvsnitt: IAvsnitt[]) => {
+                return eksisterendeAvsnitt.filter((rad) => radId !== rad.id);
+            });
+    };
+
     const endreInnholdAvsnitt = (radId: string) => {
         return (e: ChangeEvent<HTMLTextAreaElement>) => {
             const oppdaterteAvsnitt = avsnitt.map((rad) => {
@@ -138,48 +148,52 @@ const FritekstBrev: React.FC<Props> = ({ oppdaterBrevressurs, behandlingId, fags
         };
     };
 
+    const utsattGenererBrev = useDebouncedCallback(genererBrev, 1000);
+    useEffect(utsattGenererBrev, [utsattGenererBrev, avsnitt, overskrift]);
+
     return (
         <StyledFrittståendeBrev>
             <h1>Frittstående brev</h1>
             <BrevKolonner>
-                <div>
-                    <Input
-                        label="Overskrift"
-                        value={overskrift}
-                        onChange={(e) => {
-                            settOverskrift(e.target.value);
-                        }}
-                    />
+                <Input
+                    label="Overskrift"
+                    value={overskrift}
+                    onChange={(e) => {
+                        settOverskrift(e.target.value);
+                    }}
+                />
 
-                    {avsnitt.map((rad) => {
-                        const deloverskriftId = `deloverskrift-${rad.id}`;
-                        const innholdId = `innhold-${rad.id}`;
+                {avsnitt.map((rad) => {
+                    const deloverskriftId = `deloverskrift-${rad.id}`;
+                    const innholdId = `innhold-${rad.id}`;
 
-                        return (
-                            <Innholdsrad key={rad.id} border>
-                                <Input
-                                    onChange={endreDeloverskriftAvsnitt(rad.id)}
-                                    label="Deloverskrift (valgfri)"
-                                    id={deloverskriftId}
-                                    value={rad.deloverskrift}
-                                />
-                                <Textarea
-                                    onChange={endreInnholdAvsnitt(rad.id)}
-                                    defaultValue=""
-                                    label="Innhold"
-                                    id={innholdId}
-                                    value={rad.innhold}
-                                    maxLength={0}
-                                />
-                            </Innholdsrad>
-                        );
-                    })}
+                    return (
+                        <Innholdsrad key={rad.id} border>
+                            <Input
+                                onChange={endreDeloverskriftAvsnitt(rad.id)}
+                                label="Deloverskrift (valgfri)"
+                                id={deloverskriftId}
+                                value={rad.deloverskrift}
+                            />
+                            <LenkeKnapp onClick={fjernRad(rad.id)}>
+                                <SlettSøppelkasse withDefaultStroke={false} />
+                                Slett
+                            </LenkeKnapp>
+                            <Textarea
+                                onChange={endreInnholdAvsnitt(rad.id)}
+                                defaultValue=""
+                                label="Innhold"
+                                id={innholdId}
+                                value={rad.innhold}
+                                maxLength={0}
+                            />
+                        </Innholdsrad>
+                    );
+                })}
 
-                    <Knapper>
-                        <Knapp onClick={leggTilRad}>Legg til nytt avsnitt </Knapp>
-                        <Hovedknapp onClick={genererBrev}>Generer brev</Hovedknapp>
-                    </Knapper>
-                </div>
+                <Knapper>
+                    <Knapp onClick={leggTilRad}>Legg til nytt avsnitt</Knapp>
+                </Knapper>
             </BrevKolonner>
         </StyledFrittståendeBrev>
     );
