aedb4574b11bbbedc1d0ffa2f7872076c590a3e8
Asbjørn Steinskog <asbjorn@steinskog.me>
2021-10-08 15:19:15 +0200

    Fjern unødvendige typer

--- src/frontend/App/typer/dokumentliste.ts
+++ src/frontend/App/typer/dokumentliste.ts
@@ -1,22 +1,5 @@
-import * as React from 'react';
 import { Journalposttype } from '@navikt/familie-typer';
-export interface DokumentProps {
-    tittel: string;
-    dato?: string;
-    journalpostId: string;
-    journalposttype: Journalposttype;
-    dokumentinfoId: string;
-    filnavn?: string;
-}
-export interface DokumentElementProps {
-    dokument: DokumentProps;
-    onClick: (dokument: DokumentProps) => void;
-}
-export interface DokumentlisteProps {
-    dokumenter: DokumentProps[];
-    onClick: (dokument: DokumentProps) => void;
-    className?: string;
-}
+
 export interface Dokumentinfo {
     dokumentinfoId: string;
     filnavn?: string;
@@ -25,6 +8,3 @@ export interface Dokumentinfo {
     dato: string;
     journalposttype: Journalposttype;
 }
-export declare const DokumentElement: React.FC<DokumentElementProps>;
-declare const Dokumentliste: React.FC<DokumentlisteProps>;
-export default Dokumentliste;
