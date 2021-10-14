6797e3e0660b7916e97d799c33b221585c9fc2be
Viktor <viktor.grondalen.solberg@nav.no>
2021-10-14 14:52:40 +0200

    årsak til revurdering og behandling skal kun kunne være en av tre årsaker

--- src/frontend/App/typer/Behandlingsårsak.ts
+++ src/frontend/App/typer/Behandlingsårsak.ts
@@ -1,29 +1,17 @@
 export enum Behandlingsårsak {
-    DØDSFALL = 'DØDSFALL',
-    ENDRET_SATS = 'ENDRET_SATS',
-    MIGRERING = 'MIGRERING',
+    KLAGE = 'KLAGE',
     NYE_OPPLYSNINGER = 'NYE_OPPLYSNINGER',
-    PERIODISK_KONTROLL = 'PERIODISK_KONTROLL',
     SØKNAD = 'SØKNAD',
-    TEKNISK_FEIL = 'TEKNISK_FEIL',
 }
 
 export const behandlingsårsakTilTekst: Record<Behandlingsårsak, string> = {
-    DØDSFALL: 'Dødsfall',
-    ENDRET_SATS: 'Endret sats',
-    MIGRERING: 'Migrering',
+    KLAGE: 'Klage',
     NYE_OPPLYSNINGER: 'Nye opplysninger',
-    PERIODISK_KONTROLL: 'Periodisk kontroll',
     SØKNAD: 'Søknad',
-    TEKNISK_FEIL: 'Teknisk feil',
 };
 
 export const behandlingsårsaker: Behandlingsårsak[] = [
-    Behandlingsårsak.DØDSFALL,
-    Behandlingsårsak.ENDRET_SATS,
-    Behandlingsårsak.MIGRERING,
+    Behandlingsårsak.KLAGE,
     Behandlingsårsak.NYE_OPPLYSNINGER,
-    Behandlingsårsak.PERIODISK_KONTROLL,
     Behandlingsårsak.SØKNAD,
-    Behandlingsårsak.TEKNISK_FEIL,
 ];
