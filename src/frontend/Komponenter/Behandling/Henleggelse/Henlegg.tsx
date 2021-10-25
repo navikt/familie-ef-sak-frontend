2ea90ce089abdf52742469a5ff8b27b75e340006
ma10s <mattis.janitz@nav.no>
2021-10-25 10:22:15 +0200

    Ønsker å vise feilmelding hvis henlegging ikke går greit, eller hvis saksbehandler ikke har valgt årsak. Refaktorert visning av henleggelses funksjonalitet -> flytt ut i komponenter. Litt styling: vil legge til større avstand til henlegg-funksjonalitet.
    Co-authored-by: Eirik Årseth <eirik.arseth@nav.no>

--- src/frontend/Komponenter/Behandling/Henleggelse/Henlegg.tsx
+++ src/frontend/Komponenter/Behandling/Henleggelse/Henlegg.tsx
@@ -8,12 +8,18 @@ import { Ressurs, RessursStatus } from '../../../App/typer/ressurs';
 import { useApp } from '../../../App/context/AppContext';
 import { useHistory } from 'react-router-dom';
 import { Behandlingstype } from '../../../App/typer/behandlingstype';
+import styled from 'styled-components';
+import { AlertStripeFeil } from 'nav-frontend-alertstriper';
 
-enum EHenlagtårsak {
+export enum EHenlagtårsak {
     TRUKKET_TILBAKE = 'TRUKKET_TILBAKE',
     FEILREGISTRERT = 'FEILREGISTRERT',
     BEHANDLES_I_GOSYS = 'BEHANDLES_I_GOSYS',
 }
+
+const PaddingTop = styled.div`
+    padding-top: 1rem;
+`;
 
 export const Henlegg: FC<{ behandling: Behandling; fagsakId: string }> = ({
     behandling,
@@ -25,21 +31,26 @@ export const Henlegg: FC<{ behandling: Behandling; fagsakId: string }> = ({
     const history = useHistory();
     const [henlagtårsak, settHenlagtårsak] = useState<EHenlagtårsak>();
     const [låsKnapp, settLåsKnapp] = useState<boolean>(false);
+    const [feilmelding, settFeilmelding] = useState<string>();
 
     const lagreHenleggelse = () => {
-        if (låsKnapp) {
+        if (!henlagtårsak) {
+            settFeilmelding('Du må velge en henleggelsesårsak');
+        }
+
+        if (låsKnapp || !henlagtårsak) {
             return;
         }
         settLåsKnapp(true);
-        axiosRequest<string, any>({
+        axiosRequest<string, { årsak: EHenlagtårsak }>({
             method: 'POST',
             url: `/familie-ef-sak/api/behandling/${behandling.id}/henlegg`,
             data: {
                 årsak: henlagtårsak,
             },
         })
-            .then((response: Ressurs<string>) => {
-                switch (response.status) {
+            .then((respons: Ressurs<string>) => {
+                switch (respons.status) {
                     case RessursStatus.SUKSESS:
                         history.push(`/fagsak/${fagsakId}`);
                         break;
@@ -47,52 +58,89 @@ export const Henlegg: FC<{ behandling: Behandling; fagsakId: string }> = ({
                     case RessursStatus.IKKE_HENTET:
                         break;
                     default:
+                        settFeilmelding(respons.frontendFeilmelding);
                 }
             })
             .finally(() => settLåsKnapp(false));
     };
 
-    if (behandlingRedigerbar && !erBlankett) {
+    if (behandlingRedigerbar) {
         return (
-            <>
-                <hr />
-                <FamilieRadioGruppe erLesevisning={false} legend={<>Henlegg</>}>
-                    <Radio
-                        checked={henlagtårsak === EHenlagtårsak.TRUKKET_TILBAKE}
-                        label="Trukket tilbake"
-                        name="henleggRadio"
-                        onChange={() => {
-                            settHenlagtårsak(EHenlagtårsak.TRUKKET_TILBAKE);
-                        }}
+            <PaddingTop>
+                {erBlankett ? (
+                    <BlankettHenlegging
+                        lagreHenleggelse={lagreHenleggelse}
+                        settHenlagtårsak={settHenlagtårsak}
+                        låsKnapp={låsKnapp}
                     />
-                    <Radio
-                        checked={henlagtårsak === EHenlagtårsak.FEILREGISTRERT}
-                        label="Feilregistrert"
-                        name="henleggRadio"
-                        onChange={() => {
-                            settHenlagtårsak(EHenlagtårsak.FEILREGISTRERT);
-                        }}
+                ) : (
+                    <Henlegging
+                        lagreHenleggelse={lagreHenleggelse}
+                        henlagtårsak={henlagtårsak}
+                        settHenlagtårsak={settHenlagtårsak}
+                        låsKnapp={låsKnapp}
                     />
-                    <Hovedknapp htmlType={'submit'} onClick={lagreHenleggelse} disabled={låsKnapp}>
-                        Henlegg
-                    </Hovedknapp>
-                </FamilieRadioGruppe>
-            </>
-        );
-    } else if (behandlingRedigerbar && erBlankett) {
-        return (
-            <Hovedknapp
-                htmlType={'submit'}
-                onClick={() => {
-                    settHenlagtårsak(EHenlagtårsak.BEHANDLES_I_GOSYS);
-                    lagreHenleggelse();
-                }}
-                disabled={låsKnapp}
-            >
-                Henlegg
-            </Hovedknapp>
+                )}
+                {feilmelding && <AlertStripeFeil>{feilmelding}</AlertStripeFeil>}
+            </PaddingTop>
         );
     } else {
         return <></>;
     }
 };
+
+interface IHenlegg {
+    settHenlagtårsak: (årsak: EHenlagtårsak) => void;
+    lagreHenleggelse: () => void;
+    låsKnapp: boolean;
+    henlagtårsak?: EHenlagtårsak;
+}
+
+const BlankettHenlegging: React.FC<IHenlegg> = ({
+    settHenlagtårsak,
+    lagreHenleggelse,
+    låsKnapp,
+}) => (
+    <Hovedknapp
+        htmlType={'submit'}
+        onClick={() => {
+            settHenlagtårsak(EHenlagtårsak.BEHANDLES_I_GOSYS);
+            lagreHenleggelse();
+        }}
+        disabled={låsKnapp}
+    >
+        Henlegg
+    </Hovedknapp>
+);
+
+const Henlegging: React.FC<IHenlegg> = ({
+    settHenlagtårsak,
+    lagreHenleggelse,
+    låsKnapp,
+    henlagtårsak,
+}) => (
+    <>
+        <h4>Henlegg</h4>
+        <FamilieRadioGruppe erLesevisning={false}>
+            <Radio
+                checked={henlagtårsak === EHenlagtårsak.TRUKKET_TILBAKE}
+                label="Trukket tilbake"
+                name="henleggRadio"
+                onChange={() => {
+                    settHenlagtårsak(EHenlagtårsak.TRUKKET_TILBAKE);
+                }}
+            />
+            <Radio
+                checked={henlagtårsak === EHenlagtårsak.FEILREGISTRERT}
+                label="Feilregistrert"
+                name="henleggRadio"
+                onChange={() => {
+                    settHenlagtårsak(EHenlagtårsak.FEILREGISTRERT);
+                }}
+            />
+            <Hovedknapp htmlType={'submit'} onClick={lagreHenleggelse} disabled={låsKnapp}>
+                Henlegg
+            </Hovedknapp>
+        </FamilieRadioGruppe>
+    </>
+);
