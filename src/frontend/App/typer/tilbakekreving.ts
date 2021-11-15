f190b634de9cb84f0e315340f034bd4b98128337
charliemidtlyng <charlie.midtlyng@bekk.no>
2021-11-15 15:45:30 +0100

    Skal lenke videre til tilbakekrevinsløsningen for behandlinger som listes opp fra tilbakekrevingssystemet

--- src/frontend/App/typer/tilbakekreving.ts
+++ src/frontend/App/typer/tilbakekreving.ts
@@ -9,22 +9,50 @@ export interface TilbakekrevingBehandling {
 }
 
 export enum TilbakekrevingBehandlingsstatus {
-    AVSLUTTET,
-    FATTER_VEDTAK,
-    IVERKSETTER_VEDTAK,
-    OPPRETTET,
-    UTREDES,
+    AVSLUTTET = 'AVSLUTTET',
+    FATTER_VEDTAK = 'FATTER_VEDTAK',
+    IVERKSETTER_VEDTAK = 'IVERKSETTER_VEDTAK',
+    OPPRETTET = 'OPPRETTET',
+    UTREDES = 'UTREDES',
 }
+
+export const tilbakekrevingBehandlingsstatusTilTekst: Record<
+    TilbakekrevingBehandlingsstatus,
+    string
+> = {
+    AVSLUTTET: 'Avsluttet',
+    FATTER_VEDTAK: 'Fatter vedtak',
+    IVERKSETTER_VEDTAK: 'Iverksetter vedtak',
+    OPPRETTET: 'Opprettet',
+    UTREDES: 'Utredes',
+};
 
 export enum TilbakekrevingBehandlingstype {
-    TILBAKEKREVING = 'Tilbakekreving',
-    REVURDERING_TILBAKEKREVING = 'Tilbakekreving revurdering',
+    TILBAKEKREVING = 'TILBAKEKREVING',
+    REVURDERING_TILBAKEKREVING = 'REVURDERING_TILBAKEKREVING',
 }
 
+export const tilbakekrevingBehandlingstypeTilTekst: Record<TilbakekrevingBehandlingstype, string> =
+    {
+        TILBAKEKREVING: 'Tilbakekreving',
+        REVURDERING_TILBAKEKREVING: 'Tilbakekreving - revurdering',
+    };
+
 export enum TilbakekrevingBehandlingsresultatstype {
-    IKKE_FASTSATT,
-    INGEN_TILBAKEBETALING,
-    DELVIS_TILBAKEBETALING,
-    FULL_TILBAKEBETALING,
-    HENLAGT,
+    IKKE_FASTSATT = 'IKKE_FASTSATT',
+    INGEN_TILBAKEBETALING = 'INGEN_TILBAKEBETALING',
+    DELVIS_TILBAKEBETALING = 'DELVIS_TILBAKEBETALING',
+    FULL_TILBAKEBETALING = 'FULL_TILBAKEBETALING',
+    HENLAGT = 'HENLAGT',
 }
+
+export const tilbakekrevingBehandlingsresultattypeTilTekst: Record<
+    TilbakekrevingBehandlingsresultatstype,
+    string
+> = {
+    IKKE_FASTSATT: 'Ikke fastsatt',
+    INGEN_TILBAKEBETALING: 'Ingen tilbakebetaling',
+    DELVIS_TILBAKEBETALING: 'Delvis tilbakebetaling',
+    FULL_TILBAKEBETALING: 'Full tilbakebetaling',
+    HENLAGT: 'Henlagt',
+};
