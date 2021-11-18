6608923f84c0b3f5d5935295f51fb93fc4ccf9de
Viktor <viktor.grondalen.solberg@nav.no>
2021-11-18 09:46:27 +0100

    rekaftorering

--- src/frontend/Komponenter/Behandling/Brev/Brevmeny.tsx
+++ src/frontend/Komponenter/Behandling/Brev/Brevmeny.tsx
@@ -122,14 +122,14 @@ const Brevmeny: React.FC<BrevmenyProps> = (props) => {
                                 {navn.visningsnavn}
                             </option>
                         ))}
-                        <option value={'Fritekstbrev'} key={'Fritekstbrev'}>
+                        <option value={fritekstmal} key={fritekstmal}>
                             {' '}
                             Fritekstbrev
                         </option>
                     </Select>
                 )}
             </DataViewer>
-            {brevMal === 'Fritekstbrev' ? (
+            {brevMal === fritekstmal ? (
                 <DataViewer response={{ mellomlagretBrev }}>
                     {({ mellomlagretBrev }) => (
                         <FritekstBrev
