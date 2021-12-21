86a7fd566fbd7575fdf96800651d9dbe3d61d734
eirarset <eirik.arseth@nav.no>
2021-12-21 09:37:39 +0100

    Legger til funksjonsbryter for brevmottakere-knapp

--- src/frontend/Komponenter/Behandling/Brevmottakere/SettBrevmottakere.tsx
+++ src/frontend/Komponenter/Behandling/Brevmottakere/SettBrevmottakere.tsx
@@ -2,6 +2,8 @@ import React, { FC } from 'react';
 import styled from 'styled-components';
 import { Hovedknapp } from 'nav-frontend-knapper';
 import { useBehandling } from '../../../App/context/BehandlingContext';
+import { ToggleName } from '../../../App/context/toggles';
+import { useToggles } from '../../../App/context/TogglesContext';
 
 const PaddingTop = styled.div`
     padding-top: 1rem;
@@ -9,18 +11,25 @@ const PaddingTop = styled.div`
 
 export const SettBrevmottakere: FC = () => {
     const { settVisBrevmottakereModal } = useBehandling();
-    return (
-        <>
-            <PaddingTop>
-                <h4>Brevmottakere</h4>
-                <Hovedknapp
-                    onClick={() => {
-                        settVisBrevmottakereModal(true);
-                    }}
-                >
-                    Sett Verge/Fullmakt mottakere
-                </Hovedknapp>
-            </PaddingTop>
-        </>
-    );
+    const { toggles } = useToggles();
+    const skalViseSettBrevmottakereKnapp = toggles[ToggleName.visSettBrevmottakereKnapp] || false;
+
+    if (skalViseSettBrevmottakereKnapp) {
+        return (
+            <>
+                <PaddingTop>
+                    <h4>Brevmottakere</h4>
+                    <Hovedknapp
+                        onClick={() => {
+                            settVisBrevmottakereModal(true);
+                        }}
+                    >
+                        Sett Verge/Fullmakt mottakere
+                    </Hovedknapp>
+                </PaddingTop>
+            </>
+        );
+    } else {
+        return null;
+    }
 };
