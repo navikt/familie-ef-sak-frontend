89c1476cc11a574a01374c6eea975c89e6dc9a2c
Johan Blomgren <blommish@users.noreply.github.com>
2022-01-13 20:54:15 +0100

    Legger til inntektsgrunnlag, kode og sakstype som er ønsket

--- src/frontend/Komponenter/Personoversikt/Infotrygdperioderoversikt.tsx
+++ src/frontend/Komponenter/Personoversikt/Infotrygdperioderoversikt.tsx
@@ -12,6 +12,7 @@ import DataViewer from '../../Felles/DataViewer/DataViewer';
 import { Checkbox } from 'nav-frontend-skjema';
 
 const StyledTabell = styled.table`
+    max-width: 80%;
     margin-top: 2rem;
 `;
 
@@ -37,21 +38,69 @@ interface SummertPeriode {
 }
 
 interface InfotrygdPeriode {
-    stønadId: number;
     vedtakId: number;
+    vedtakstidspunkt: string;
 
     stønadFom: string;
     stønadTom: string;
     opphørsdato?: string;
 
     beløp: number;
-    inntektsreduksjon: number;
+    inntektsgrunnlag: number;
     samordningsfradrag: number;
 
-    startDato: string;
-    kode: string;
+    kode: Kode;
+    sakstype: Sakstype;
     brukerId: string;
 }
+
+enum Kode {
+    ANNULERT = 'ANNULERT',
+    ENDRING_BEREGNINGSGRUNNLAG = 'ENDRING_BEREGNINGSGRUNNLAG',
+    FØRSTEGANGSVEDTAK = 'FØRSTEGANGSVEDTAK',
+    G_REGULERING = 'G_REGULERING',
+    NY = 'NY',
+    OPPHØRT = 'OPPHØRT',
+    SATSENDRING = 'SATSENDRING',
+    UAKTUELL = 'UAKTUELL',
+    OVERTFØRT_NY_LØSNING = 'OVERTFØRT_NY_LØSNING',
+}
+
+const kodeTilTekst: Record<Kode, string> = {
+    ANNULERT: 'Annullert (AN)',
+    ENDRING_BEREGNINGSGRUNNLAG: 'Endring i beregningsgrunnlag (E)',
+    FØRSTEGANGSVEDTAK: 'Førstegangsvedtak (F)',
+    G_REGULERING: 'G-regulering (G)',
+    NY: 'Ny (NY)',
+    OPPHØRT: 'Opphørt (O)',
+    SATSENDRING: 'Satsendring (S)',
+    UAKTUELL: 'Uaktuell (UA)',
+    OVERTFØRT_NY_LØSNING: 'Overf ny løsning (OO)',
+};
+
+enum Sakstype {
+    KLAGE = 'KLAGE',
+    MASKINELL_G_OMREGNING = 'MASKINELL_G_OMREGNING',
+    REVURDERING = 'REVURDERING',
+    GRUNNBELØP_OMREGNING = 'GRUNNBELØP_OMREGNING',
+    KONVERTERING = 'KONVERTERING',
+    MASKINELL_SATSOMREGNING = 'MASKINELL_SATSOMREGNING',
+    ANKE = 'ANKE',
+    SØKNAD = 'SØKNAD',
+    SØKNAD_ØKNING_ENDRING = 'SØKNAD_ØKNING_ENDRING',
+}
+
+const sakstypeTilTekst: Record<Sakstype, string> = {
+    KLAGE: 'Klage (K)',
+    MASKINELL_G_OMREGNING: 'Maskinell G-omregning (MG)',
+    REVURDERING: 'Revurdering (R)',
+    GRUNNBELØP_OMREGNING: 'Grunnbeløp omregning (GO)',
+    KONVERTERING: 'Konvertering (KO)',
+    MASKINELL_SATSOMREGNING: 'Maskinell satsomregning (MS)',
+    ANKE: 'Anke (A)',
+    SØKNAD: 'Søknad (S)',
+    SØKNAD_ØKNING_ENDRING: 'Søknad om økning/endring (SØ)',
+};
 
 const SummertePerioder: React.FC<{ perioder: SummertPeriode[] }> = ({ perioder }) => {
     return (
@@ -87,22 +136,21 @@ const InfotrygdPerioder: React.FC<{ perioder: InfotrygdPeriode[] }> = ({ periode
         <StyledTabell className="tabell">
             <thead>
                 <tr>
-                    <th>StønadId</th>
                     <th>VedtakId</th>
                     <th>Periode (fom-tom)</th>
                     <th>Opphørsdato</th>
-                    <th>Inntektsreduskjon</th>
+                    <th>Inntektsgrunnlag</th>
                     <th>Samordningsfradrag</th>
                     <th>Stønadsbeløp</th>
-                    <th>Startdato</th>
+                    <th>Vedtakstidspunkt</th>
                     <th>Kode</th>
+                    <th>Sakstype</th>
                     <th>Saksbehandler</th>
                 </tr>
             </thead>
             <tbody>
                 {perioder.map((periode) => (
                     <Rad>
-                        <td>{periode.stønadId}</td>
                         <td>{periode.vedtakId}</td>
                         <td>
                             {formaterNullableMånedÅr(periode.stønadFom)}
@@ -110,11 +158,12 @@ const InfotrygdPerioder: React.FC<{ perioder: InfotrygdPeriode[] }> = ({ periode
                             {formaterNullableMånedÅr(periode.stønadTom)}
                         </td>
                         <td>{formaterNullableMånedÅr(periode.opphørsdato)}</td>
-                        <td>{formaterTallMedTusenSkille(periode.inntektsreduksjon)}</td>
+                        <td>{formaterTallMedTusenSkille(periode.inntektsgrunnlag)}</td>
                         <td>{formaterTallMedTusenSkille(periode.samordningsfradrag)}</td>
                         <td>{formaterTallMedTusenSkille(periode.beløp)}</td>
-                        <td>{formaterNullableIsoDato(periode.startDato)}</td>
-                        <td>{periode.kode}</td>
+                        <td>{formaterNullableIsoDato(periode.vedtakstidspunkt)}</td>
+                        <td>{kodeTilTekst[periode.kode]}</td>
+                        <td>{sakstypeTilTekst[periode.sakstype]}</td>
                         <td>{periode.brukerId}</td>
                     </Rad>
                 ))}
