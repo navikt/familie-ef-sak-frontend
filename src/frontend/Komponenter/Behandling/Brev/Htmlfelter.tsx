78eb9621838c1ae9db566727f18c64ba61a85d4a
Viktor <viktor.grondalen.solberg@nav.no>
2022-01-26 10:24:49 +0100

    Kolonne for samordningsfradag skal være med i brevmal

--- src/frontend/Komponenter/Behandling/Brev/Htmlfelter.tsx
+++ src/frontend/Komponenter/Behandling/Brev/Htmlfelter.tsx
@@ -5,12 +5,13 @@ export const delmalTilHtml = (tilkjentYtelse?: TilkjentYtelse) => {
     return { inntektsperioderHtml: lagInntektsperioder(tilkjentYtelse) };
 };
 
-const borderStyling = 'border: 1px solid black; padding:3px 8px 3px 8px';
+const borderStyling = 'border: 1px solid black; padding: 3px 8px 3px 8px;';
 const lagInntektsperioder = (tilkjentYtelse?: TilkjentYtelse): string => {
     return `<table style="margin-left: 2px; border-collapse: collapse; ${borderStyling}"><thead><tr>
-                <th style="${borderStyling}">Periode</th>
-                <th style="${borderStyling}">Årsinntekt</th>
-                <th style="${borderStyling}">Dette får du i overgangsstønad pr. måned</th></tr></thead>
+                <th style="width: 160px; ${borderStyling}">Periode</th>
+                <th style="width: 75px; ${borderStyling}">Årsinntekt</th>
+                <th style="width: 190px; word-wrap: break-word; ${borderStyling}">Dette får du i overgangsstønad pr. måned</th></tr></thead>
+                <th style="width: 85px; ${borderStyling}">Samordning</th></tr></thead>
                 <tbody>${lagRaderForVedtak(tilkjentYtelse)}</tbody>
             </table>`;
 };
@@ -23,11 +24,16 @@ const lagRaderForVedtak = (tilkjentYtelse?: TilkjentYtelse): string => {
         .map((andel: AndelTilkjentYtelse) => {
             const inntekt = formaterTallMedTusenSkille(andel.inntekt);
             const beløp = formaterTallMedTusenSkille(andel.beløp);
-            return `<tr><td style="${borderStyling}">${formaterNullableIsoDato(
-                andel.stønadFra
-            )} - ${formaterNullableIsoDato(
+            const samordningsfradag = formaterTallMedTusenSkille(andel.samordningsfradrag);
+            return `<tr>
+                    <td style="${borderStyling}">
+                        ${formaterNullableIsoDato(andel.stønadFra)} - ${formaterNullableIsoDato(
                 andel.stønadTil
-            )}</td><td style="${borderStyling}">${inntekt}</td><td style="${borderStyling}">${beløp}</td></tr>`;
+            )}
+                    </td>
+                    <td style="${borderStyling}">${inntekt}</td>
+                    <td style="${borderStyling}">${beløp}</td>
+                    <td style="${borderStyling}">${samordningsfradag}</td></tr>`;
         })
         .join('');
 };
