4278c7b6ac60a3c106cab2cefb39aebdef2472b1
Viktor <viktor.grondalen.solberg@nav.no>
2022-01-26 15:46:43 +0100

    Tabell for samordningsfradag skal kun inkluderes dersom det eksisterer et samordningsbeløp stÃørre enn 0.

--- src/frontend/Komponenter/Behandling/Brev/Htmlfelter.tsx
+++ src/frontend/Komponenter/Behandling/Brev/Htmlfelter.tsx
@@ -7,18 +7,30 @@ export const delmalTilHtml = (tilkjentYtelse?: TilkjentYtelse) => {
 
 const borderStyling = 'border: 1px solid black; padding: 3px 5px 3px 5px;';
 const lagInntektsperioder = (tilkjentYtelse?: TilkjentYtelse): string => {
-    return `<table style="margin-left: 2px; border-collapse: collapse; ${borderStyling}">
+    const fradagStørreEnnNullEksisterer = tilkjentYtelse?.andeler.some(
+        (andel) => andel.samordningsfradrag > 0
+    );
+    const tabellHeadereUtenSamordning = `<table style="margin-left: 2px; border-collapse: collapse; ${borderStyling}">
                 <thead><tr>
                     <th style="width: 160px; ${borderStyling}">Periode</th>
                     <th style="width: 75px; ${borderStyling}">Årsinntekt</th>
-                    <th style="width: 150px; word-wrap: break-word; ${borderStyling}">Dette får du i overgangsstønad pr. måned</th>
-                    <th style="width: 85px; ${borderStyling}">Samordning</th>
-                </tr></thead>
-                <tbody>${lagRaderForVedtak(tilkjentYtelse)}</tbody>
-            </table>`;
+                    <th style="width: 150px; word-wrap: break-word; ${borderStyling}">Dette får du i overgangsstønad pr. måned</th>`;
+    const samordningHeader = `<th style="width: 85px; ${borderStyling}">Samordning</th>`;
+    const tabellRader = `</tr></thead><tbody>${lagRaderForVedtak(
+        tilkjentYtelse,
+        fradagStørreEnnNullEksisterer
+    )}</tbody></table>`;
+    return (
+        tabellHeadereUtenSamordning +
+        (fradagStørreEnnNullEksisterer ? samordningHeader : ``) +
+        tabellRader
+    );
 };
 
-const lagRaderForVedtak = (tilkjentYtelse?: TilkjentYtelse): string => {
+const lagRaderForVedtak = (
+    tilkjentYtelse?: TilkjentYtelse,
+    inkluderSamordning?: boolean
+): string => {
     if (!tilkjentYtelse) {
         return '';
     }
@@ -27,15 +39,16 @@ const lagRaderForVedtak = (tilkjentYtelse?: TilkjentYtelse): string => {
             const inntekt = formaterTallMedTusenSkille(andel.inntekt);
             const beløp = formaterTallMedTusenSkille(andel.beløp);
             const samordningsfradag = formaterTallMedTusenSkille(andel.samordningsfradrag);
-            return `<tr>
+            const tabellRaderUtenSamordning = `<tr>
                     <td style="${borderStyling}">
                         ${formaterNullableIsoDato(andel.stønadFra)} - ${formaterNullableIsoDato(
                 andel.stønadTil
             )}
                     </td>
                     <td style="${borderStyling}">${inntekt}</td>
-                    <td style="${borderStyling}">${beløp}</td>
-                    <td style="${borderStyling}">${samordningsfradag}</td></tr>`;
+                    <td style="${borderStyling}">${beløp}</td>`;
+            const samordningRad = `<td style="${borderStyling}">${samordningsfradag}</td>`;
+            return tabellRaderUtenSamordning + (inkluderSamordning ? samordningRad : ``) + `</tr>`;
         })
         .join('');
 };
