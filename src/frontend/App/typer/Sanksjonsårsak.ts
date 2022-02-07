0812339d5e239fe2055932dc77be36ad58f9f764
Viktor <viktor.grondalen.solberg@nav.no>
2022-02-07 12:43:05 +0100

    Side for sanksjonsfastsettelse er implementert uten logikk.

---
+++ src/frontend/App/typer/Sanksjonsårsak.ts
@@ -0,0 +1,30 @@
+export enum Sanksjonsårsak {
+    NEKTET_DELTAGELSE_ARBEIDSMARKEDSTILTAK = 'NEKTET_DELTAGELSE_ARBEIDSMARKEDSTILTAK',
+    NEKTET_TILBUDT_ARBEID = 'NEKTET_TILBUDT_ARBEID',
+    SAGT_OPP_STILLING = 'SAGT_OPP_STILLING',
+    UNNLATT_GJENOPPTAGELSE_ARBEIDSFORHOLD = 'UNNLATT_GJENNOPPTAGELSE_ARBEIDSFORHOLD',
+    UNNLATT_MØTE_INNKALLING = 'UNNLATT_MØTE_INNKALLING',
+}
+
+export const sanksjonsårsakTilTekst: Record<Sanksjonsårsak, string> = {
+    NEKTET_DELTAGELSE_ARBEIDSMARKEDSTILTAK: 'Nektet å delta i arbeidsmarkedstiltak',
+    NEKTET_TILBUDT_ARBEID: 'Nektet å ta i mot tilbudt arbeid',
+    SAGT_OPP_STILLING: 'Sagt opp stilling',
+    UNNLATT_GJENNOPPTAGELSE_ARBEIDSFORHOLD:
+        'Unnlatt å gjenoppta sitt arbeidsforhold etter endt foreldrepermisjon',
+    UNNLATT_MØTE_INNKALLING: 'Unnlatt å møte ved innkalling til arbeids- og velferdsetaten',
+};
+
+export const sanksjonsårsaker: Sanksjonsårsak[] = [
+    Sanksjonsårsak.NEKTET_DELTAGELSE_ARBEIDSMARKEDSTILTAK,
+    Sanksjonsårsak.NEKTET_TILBUDT_ARBEID,
+    Sanksjonsårsak.SAGT_OPP_STILLING,
+    Sanksjonsårsak.UNNLATT_GJENOPPTAGELSE_ARBEIDSFORHOLD,
+    Sanksjonsårsak.UNNLATT_MØTE_INNKALLING,
+];
+
+export const sanksjonInfo =
+    'Sanksjonen gjelder for alle brukerens stønader etter kapittel 15, herunder stønader etter tilleggsstønadsforskriften. Følgende stønader i EF Sak vil bli stoppet automatisk i overforstående periode av dette vedtaket: - Overgangssstønad Har brukeren andre løpende stønader i Infotrygd eller Arena må disse stanses manuelt';
+
+export const sanksjonAdvarsel =
+    'Obs! Det er under 2 dager igjen av denne måneden. Det er viktig at du følger opp at beslutter godkjenner vedtaket før månedsslutt da perioden for sanksjon vil endre seg om vedtaket blir godkjent i neste måned.';
--- src/frontend/Komponenter/Behandling/Sanksjon/Sanksjonsfastsettelse.tsx
+++ src/frontend/Komponenter/Behandling/Sanksjon/Sanksjonsfastsettelse.tsx
@@ -1,11 +1,93 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import styled from 'styled-components';
+import { Select, Textarea } from 'nav-frontend-skjema';
+import {
+    sanksjonAdvarsel,
+    sanksjonInfo,
+    Sanksjonsårsak,
+    sanksjonsårsaker,
+    sanksjonsårsakTilTekst,
+} from '../../../App/typer/Sanksjonsårsak';
+import { Normaltekst, Undertittel } from 'nav-frontend-typografi';
+import { AlertStripeAdvarsel, AlertStripeInfo } from 'nav-frontend-alertstriper';
+import { Hovedknapp } from 'nav-frontend-knapper';
+
+const SanksjonVelger = styled(Select)`
+    margin-top: 1rem;
+`;
+
+const Container = styled.div`
+    margin: 2rem;
+`;
+
+const Seksjon = styled.div`
+    margin-top: 2rem;
+`;
+
+const NormaltekstMedMargin = styled(Normaltekst)`
+    margin-top: 1rem;
+`;
+
+const Advarsel = styled(AlertStripeAdvarsel)`
+    margin-top: 1.5rem;
+`;
 
 interface Props {
     behandlingId: string;
 }
 
 const Sanksjonsfastsettelse: FC<Props> = ({ behandlingId }) => {
-    return <p>{behandlingId}</p>;
+    const [valgtSanksjonsårsak, settValgtSanksjonsårsak] = useState<Sanksjonsårsak>();
+    const [internBegrunnelse, settInternBegrunnelse] = useState<string>('');
+
+    return (
+        <Container>
+            <section>
+                <Undertittel>Sanksjon</Undertittel>
+                <SanksjonVelger
+                    label="Brukeren har uten rimelig grunn:"
+                    value={valgtSanksjonsårsak || ''}
+                    bredde={'xxl'}
+                    onChange={(e) => {
+                        settValgtSanksjonsårsak(e.target.value as Sanksjonsårsak);
+                    }}
+                >
+                    <option value="">Velg</option>
+                    {sanksjonsårsaker.map((sanksjonsårsak: Sanksjonsårsak, index: number) => (
+                        <option key={index} value={sanksjonsårsak}>
+                            {sanksjonsårsakTilTekst[sanksjonsårsak]}
+                        </option>
+                    ))}
+                </SanksjonVelger>
+            </section>
+            {valgtSanksjonsårsak && (
+                <>
+                    <Seksjon>
+                        <Undertittel>Sanksjonsperiode</Undertittel>
+                        <NormaltekstMedMargin>
+                            Måneden for sanksjon er <b>Februar 2022</b> som er måneden etter dette
+                            vedtaket. Bruker vil ikke få utbetalt stønad i denne perioden.{' '}
+                            {behandlingId}
+                        </NormaltekstMedMargin>
+                    </Seksjon>
+                    <Seksjon>
+                        <AlertStripeInfo>{sanksjonInfo}</AlertStripeInfo>
+                        <Advarsel>{sanksjonAdvarsel}</Advarsel>
+                    </Seksjon>
+                    <Seksjon>
+                        <Textarea
+                            label={'Begrunnelse (intern)'}
+                            value={internBegrunnelse}
+                            onChange={(e) => settInternBegrunnelse(e.target.value)}
+                        />
+                    </Seksjon>
+                    <Seksjon>
+                        <Hovedknapp>Lagre vedtak</Hovedknapp>
+                    </Seksjon>
+                </>
+            )}
+        </Container>
+    );
 };
 
 export default Sanksjonsfastsettelse;
