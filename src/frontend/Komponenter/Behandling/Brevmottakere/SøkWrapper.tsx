4eabb120537ca5442b1f71e3af84ca1c454e7516
Asbjørn Steinskog <asbjorn@steinskog.me>
2022-02-10 11:59:30 +0100

    Style Manuelt søk

--- src/frontend/Komponenter/Behandling/Brevmottakere/SøkWrapper.tsx
+++ src/frontend/Komponenter/Behandling/Brevmottakere/SøkWrapper.tsx
@@ -4,6 +4,7 @@ import { Select } from 'nav-frontend-skjema';
 import { SøkPerson } from './SøkPerson';
 import { SøkOrganisasjon } from './SøkOrganisasjon';
 import { IBrevmottaker, IOrganisasjonMottaker } from './typer';
+import styled from 'styled-components';
 
 interface Props {
     valgtePersonMottakere: IBrevmottaker[];
@@ -17,6 +18,15 @@ enum ESøktype {
     PERSON = 'PERSON',
 }
 
+const StyledIngress = styled(Ingress)`
+    margin-bottom: 1rem;
+`;
+
+const SøkTypeSelect = styled(Select)`
+    width: 200px;
+    margin-bottom: 1rem;
+`;
+
 export const SøkWrapper: FC<Props> = ({
     valgtePersonMottakere,
     settValgtePersonMottakere,
@@ -27,12 +37,15 @@ export const SøkWrapper: FC<Props> = ({
 
     return (
         <>
-            <Ingress>Manuelt søk</Ingress>
-            <Select value={søktype} onChange={(e) => settSøktype(e.target.value as ESøktype)}>
+            <StyledIngress>Manuelt søk</StyledIngress>
+            <SøkTypeSelect
+                value={søktype}
+                onChange={(e) => settSøktype(e.target.value as ESøktype)}
+            >
                 <option>Velg</option>
                 <option value={ESøktype.ORGANISASJON}>Organisasjon</option>
                 <option value={ESøktype.PERSON}>Person</option>
-            </Select>
+            </SøkTypeSelect>
             {søktype === ESøktype.ORGANISASJON && (
                 <SøkOrganisasjon
                     valgteMottakere={valgteOrganisasjonMottakere}
