6a5f3964844fcf84273e68026564853faa2f3213
Viktor <viktor.grondalen.solberg@nav.no>
2022-02-16 15:45:44 +0100

    Fjernet ubrukt 'tilTekst' kode

--- src/frontend/App/typer/tilbakekreving.ts
+++ src/frontend/App/typer/tilbakekreving.ts
@@ -17,27 +17,10 @@ export enum TilbakekrevingBehandlingsstatus {
     UTREDES = 'UTREDES',
 }
 
-export const tilbakekrevingBehandlingsstatusTilTekst: Record<
-    TilbakekrevingBehandlingsstatus,
-    string
-> = {
-    AVSLUTTET: 'Avsluttet',
-    FATTER_VEDTAK: 'Fatter vedtak',
-    IVERKSETTER_VEDTAK: 'Iverksetter vedtak',
-    OPPRETTET: 'Opprettet',
-    UTREDES: 'Utredes',
-};
-
 export enum TilbakekrevingBehandlingstype {
     TILBAKEKREVING = 'TILBAKEKREVING',
     REVURDERING_TILBAKEKREVING = 'REVURDERING_TILBAKEKREVING',
 }
-
-export const tilbakekrevingBehandlingstypeTilTekst: Record<TilbakekrevingBehandlingstype, string> =
-    {
-        TILBAKEKREVING: 'Tilbakekreving',
-        REVURDERING_TILBAKEKREVING: 'Tilbakekreving - revurdering',
-    };
 
 export enum TilbakekrevingBehandlingsresultatstype {
     IKKE_FASTSATT = 'IKKE_FASTSATT',
@@ -47,17 +30,6 @@ export enum TilbakekrevingBehandlingsresultatstype {
     HENLAGT = 'HENLAGT',
 }
 
-export const tilbakekrevingBehandlingsresultattypeTilTekst: Record<
-    TilbakekrevingBehandlingsresultatstype,
-    string
-> = {
-    IKKE_FASTSATT: 'Ikke fastsatt',
-    INGEN_TILBAKEBETALING: 'Ingen tilbakebetaling',
-    DELVIS_TILBAKEBETALING: 'Delvis tilbakebetaling',
-    FULL_TILBAKEBETALING: 'Full tilbakebetaling',
-    HENLAGT: 'Henlagt',
-};
-
 export enum TilbakekrevingsbehandlingÅrsak {
     REVURDERING_KLAGE_NFP = 'REVURDERING_KLAGE_NFP',
     REVURDERING_KLAGE_KA = 'REVURDERING_KLAGE_KA',
@@ -65,12 +37,3 @@ export enum TilbakekrevingsbehandlingÅrsak {
     REVURDERING_OPPLYSNINGER_OM_FORELDELSE = 'REVURDERING_OPPLYSNINGER_OM_FORELDELSE',
     REVURDERING_FEILUTBETALT_BELØP_HELT_ELLER_DELVIS_BORTFALT = 'REVURDERING_FEILUTBETALT_BELØP_HELT_ELLER_DELVIS_BORTFALT',
 }
-
-export const tilbakekrevingsårsakTilTekst: Record<TilbakekrevingsbehandlingÅrsak, string> = {
-    REVURDERING_KLAGE_NFP: 'Klage tilbakekreving',
-    REVURDERING_KLAGE_KA: 'Klage omgjort av KA',
-    REVURDERING_OPPLYSNINGER_OM_VILKÅR: 'Nye opplysninger',
-    REVURDERING_OPPLYSNINGER_OM_FORELDELSE: 'Nye opplysninger',
-    REVURDERING_FEILUTBETALT_BELØP_HELT_ELLER_DELVIS_BORTFALT:
-        'Feilutbetalt beløp helt eller delvis bortfalt',
-};
