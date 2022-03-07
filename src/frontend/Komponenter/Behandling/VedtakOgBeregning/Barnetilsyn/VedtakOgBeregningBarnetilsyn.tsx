5be8d4885464919d0b44f65c0c09799d3fbded67
Viktor <viktor.grondalen.solberg@nav.no>
2022-03-07 14:37:02 +0100

    vedtak og beregningsiden skal kunne innvilge, avslå eller opphøre vedtaket.

--- src/frontend/Komponenter/Behandling/VedtakOgBeregning/Barnetilsyn/VedtakOgBeregningBarnetilsyn.tsx
+++ src/frontend/Komponenter/Behandling/VedtakOgBeregning/Barnetilsyn/VedtakOgBeregningBarnetilsyn.tsx
@@ -1,14 +1,50 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { Behandling } from '../../../../App/typer/fagsak';
 import { IVilkår } from '../../Inngangsvilkår/vilkår';
+import styled from 'styled-components';
+import { EBehandlingResultat } from '../../../../App/typer/vedtak';
+import { useHentVedtak } from '../../../../App/hooks/useHentVedtak';
+import { erAlleVilkårOppfylt } from '../../Vilkårresultat/utils';
+import { RessursStatus } from '../../../../App/typer/ressurs';
+import SelectVedtaksresultat from '../SelectVedtaksresultat';
 
 interface Props {
-    behandling?: Behandling;
-    vilkår?: IVilkår;
+    behandling: Behandling;
+    vilkår: IVilkår;
 }
 
-const VedtakOgBeregningBarnetilsyn: FC<Props> = () => {
-    return <p>TODO</p>;
+const Wrapper = styled.div`
+    padding: 1rem 2rem;
+`;
+
+const VedtakOgBeregningBarnetilsyn: FC<Props> = ({ behandling, vilkår }) => {
+    const behandlingId = behandling.id;
+    const [resultatType, settResultatType] = useState<EBehandlingResultat>();
+    const { vedtak, hentVedtak } = useHentVedtak(behandlingId);
+
+    const alleVilkårOppfylt = erAlleVilkårOppfylt(vilkår);
+    //const ikkeOppfyltVilkårEksisterer = eksistererIkkeOppfyltVilkår(vilkår);
+
+    useEffect(() => {
+        hentVedtak();
+    }, [hentVedtak]);
+
+    useEffect(() => {
+        if (vedtak.status === RessursStatus.SUKSESS && vedtak.data) {
+            settResultatType(vedtak.data.resultatType);
+        }
+    }, [vedtak]);
+
+    return (
+        <Wrapper>
+            <SelectVedtaksresultat
+                behandling={behandling}
+                resultatType={resultatType}
+                settResultatType={settResultatType}
+                alleVilkårOppfylt={alleVilkårOppfylt}
+            />
+        </Wrapper>
+    );
 };
 
 export default VedtakOgBeregningBarnetilsyn;
