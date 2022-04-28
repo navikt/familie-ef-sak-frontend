89530b9dbf06bd63fd913096f52f302ba8e41907
Viktor <viktor.grondalen.solberg@nav.no>
2022-04-28 09:36:35 +0200

    Ãtekst for årsak til barnepass var i utgangspunktet riktig

--- src/frontend/Komponenter/Behandling/Aktivitet/AlderPåBarn/AlderPåBarnTyper.ts
+++ src/frontend/Komponenter/Behandling/Aktivitet/AlderPåBarn/AlderPåBarnTyper.ts
@@ -6,7 +6,7 @@ export enum EÅrsakBarnepass {
 
 export const ÅrsakBarnepassTilTekst: Record<EÅrsakBarnepass, string> = {
     trengerMerPassEnnJevnaldrede:
-        'Barnet har behov for vesentlig mer plass enn det som er vanlig for jevnaldrende',
+        'Barnet har behov for vesentlig mer pass enn det som er vanlig for jevnaldrende',
     myeBortePgaJobb: 'Jeg må være borte fra hjemmet i lengre perioder på grunn av jobb',
     utenomVanligArbeidstid:
         'Jeg jobber turnus eller skift, og jobber på tider utenom vanlig arbeidstid',
