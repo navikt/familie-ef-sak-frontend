34d4c20e218d351cc229173417f224adee84c12a
Mattis Janitz <mattis.janitz@nav.no>
2022-06-23 09:21:50 +0200

    Infotrygdperioder barnetilsyn (#1310)
    * Barnetilsyn-infotrygdperioder: Fjern inntekt og sammordningsfradrag. Legg til antall barn og utgift.
    
    * Vis stønadId/vedtakId i Tooltip
    
    * Fjernet "store tabeller".
    * Splittet OS og BT i separate tabeller for å slippe bruke erBT, erOS i loop. Litt lettere å lese.
    
    * Brekk tabell-headers med &shy;

--- src/frontend/Komponenter/Migrering/InfotrygdPerioder.tsx
+++ src/frontend/Komponenter/Migrering/InfotrygdPerioder.tsx
@@ -4,33 +4,25 @@ import { slåSammenOgSorterPerioder } from '../Infotrygd/grupperInfotrygdperiode
 import { Stønadstype } from '../../App/typer/behandlingstema';
 import {
     InfotrygdPerioderSkolepenger,
-    TabellInfotrygdPerioder,
-    TabellInfotrygdPerioderKompakt,
+    TabellInfotrygdBarnetilsynperioderKompakt,
+    TabellInfotrygdOvergangsstønadperioderKompakt,
 } from './TabellInfotrygdPerioder';
 
 const InfotrygdPerioder: React.FC<{
     stønadstype: Stønadstype;
-    visStørreTabell: boolean;
     perioder: InfotrygdPeriode[];
-}> = ({ stønadstype, visStørreTabell, perioder }) => {
+}> = ({ stønadstype, perioder }) => {
     if (perioder.length === 0) {
         return <>Ingen vedtaksperioder i Infotrygd</>;
     }
     const sammenslåttePerioder = slåSammenOgSorterPerioder(perioder);
     switch (stønadstype) {
         case Stønadstype.OVERGANGSSTØNAD:
-        case Stønadstype.BARNETILSYN:
-            return visStørreTabell ? (
-                <TabellInfotrygdPerioder
-                    perioder={sammenslåttePerioder}
-                    stønadstype={stønadstype}
-                />
-            ) : (
-                <TabellInfotrygdPerioderKompakt
-                    perioder={sammenslåttePerioder}
-                    stønadstype={stønadstype}
-                />
+            return (
+                <TabellInfotrygdOvergangsstønadperioderKompakt perioder={sammenslåttePerioder} />
             );
+        case Stønadstype.BARNETILSYN:
+            return <TabellInfotrygdBarnetilsynperioderKompakt perioder={sammenslåttePerioder} />;
         case Stønadstype.SKOLEPENGER:
             return <InfotrygdPerioderSkolepenger perioder={sammenslåttePerioder} />;
     }
