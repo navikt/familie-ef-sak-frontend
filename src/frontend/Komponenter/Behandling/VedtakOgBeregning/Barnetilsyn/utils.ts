289147267b2f4bb53440cd66b04a475481a97029
eirarset <eirik.arseth@nav.no>
2022-06-23 13:24:44 +0200

    Refaktor. Trenger bare å sjekke barn som ikke oppfyller hvilket som helst vilkår

--- src/frontend/Komponenter/Behandling/VedtakOgBeregning/Barnetilsyn/utils.ts
+++ src/frontend/Komponenter/Behandling/VedtakOgBeregning/Barnetilsyn/utils.ts
@@ -1,24 +1,11 @@
-import {
-    AktivitetsvilkårType,
-    InngangsvilkårType,
-    IVilkår,
-    Vilkårsresultat,
-} from '../../Inngangsvilkår/vilkår';
+import { IVilkår, Vilkårsresultat } from '../../Inngangsvilkår/vilkår';
 
 export const barnSomOppfyllerAlleVilkår = (vilkår: IVilkår) => {
-    const barnMedOppfyltAleneomsorg = vilkår.vurderinger
-        .filter((vurdering) => vurdering.vilkårType === InngangsvilkårType.ALENEOMSORG)
-        .filter((vurdering) => vurdering.resultat === Vilkårsresultat.OPPFYLT)
-        .map((vurdering) => vurdering.barnId);
-
-    const barnMedOppfyltAldersvilkår = vilkår.vurderinger
-        .filter((vurdering) => vurdering.vilkårType === AktivitetsvilkårType.ALDER_PÅ_BARN)
-        .filter((vurdering) => vurdering.resultat === Vilkårsresultat.OPPFYLT)
+    const barnSomIkkeOppfyllerVilkår = vilkår.vurderinger
+        .filter((vurdering) => vurdering.resultat !== Vilkårsresultat.OPPFYLT)
         .map((vurdering) => vurdering.barnId);
 
     return vilkår.grunnlag.barnMedSamvær.filter(
-        (barn) =>
-            barnMedOppfyltAleneomsorg.includes(barn.barnId) &&
-            barnMedOppfyltAldersvilkår.includes(barn.barnId)
+        (barn) => !barnSomIkkeOppfyllerVilkår.includes(barn.barnId)
     );
 };
