84ce3f23b3cefad7f5abb21392266c56d661d416
Johan Blomgren <blommish@users.noreply.github.com>
2022-06-28 10:59:11 +0200

    Setter dokumentasjonstype automatisk hvis behandlesAvApplikasjon er satt for å unngå at saksbehandler må sette denne for hver ettersending (#1353)

--- src/frontend/Komponenter/Journalforing/VelgUstrukturertDokumentasjonType.tsx
+++ src/frontend/Komponenter/Journalforing/VelgUstrukturertDokumentasjonType.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Select } from 'nav-frontend-skjema';
+import { IOppgave } from '../Oppgavebenk/typer/oppgave';
+import { byggTomRessurs, Ressurs, RessursStatus } from '../../App/typer/ressurs';
+import { useApp } from '../../App/context/AppContext';
+import DataViewer from '../../Felles/DataViewer/DataViewer';
 
 const VelgUstrukturertDokumentasjonTypeSelect = styled(Select)`
     width: 10rem;
@@ -18,40 +22,67 @@ const ustrukturertTypeTilTekst: Record<UstrukturertDokumentasjonType, string> = 
 };
 
 const VelgUstrukturertDokumentasjonType: React.FC<{
+    oppgaveId: string;
     ustrukturertDokumentasjonType: UstrukturertDokumentasjonType | undefined;
     settUstrukturertDokumentasjonType: React.Dispatch<
         React.SetStateAction<UstrukturertDokumentasjonType | undefined>
     >;
-}> = ({ ustrukturertDokumentasjonType, settUstrukturertDokumentasjonType }) => {
+}> = ({ oppgaveId, ustrukturertDokumentasjonType, settUstrukturertDokumentasjonType }) => {
+    const { axiosRequest } = useApp();
+    const [oppgave, settOppgave] = useState<Ressurs<IOppgave>>(byggTomRessurs());
+
+    useEffect(() => {
+        axiosRequest<IOppgave, undefined>({
+            method: 'GET',
+            url: `/familie-ef-sak/api/oppgave/gosys/${oppgaveId}`,
+        }).then((res: Ressurs<IOppgave>) => settOppgave(res));
+    }, [axiosRequest, oppgaveId]);
+
+    useEffect(() => {
+        if (
+            oppgave.status === RessursStatus.SUKSESS &&
+            oppgave.data.behandlesAvApplikasjon === 'familie-ef-sak'
+        ) {
+            settUstrukturertDokumentasjonType(UstrukturertDokumentasjonType.ETTERSENDING);
+        }
+    }, [settUstrukturertDokumentasjonType, oppgave]);
+
     return (
-        <>
-            <VelgUstrukturertDokumentasjonTypeSelect
-                label="Type dokumentasjon"
-                onChange={(e) => {
-                    if (e.target.value.trim() !== '') {
-                        settUstrukturertDokumentasjonType(
-                            e.target.value as UstrukturertDokumentasjonType
-                        );
-                    } else {
-                        settUstrukturertDokumentasjonType(undefined);
-                    }
-                }}
-                value={ustrukturertDokumentasjonType}
-            >
-                <option value={''}>Ikke valgt</option>
-                {[
-                    UstrukturertDokumentasjonType.PAPIRSØKNAD,
-                    UstrukturertDokumentasjonType.ETTERSENDING,
-                ].map((type) => (
-                    <option key={type} value={type}>
-                        {ustrukturertTypeTilTekst[type]}
-                    </option>
-                ))}
-            </VelgUstrukturertDokumentasjonTypeSelect>
-            {ustrukturertDokumentasjonType === UstrukturertDokumentasjonType.PAPIRSØKNAD && (
-                <div>Årsak til behandling kommer settes til papirsøknad</div>
-            )}
-        </>
+        <DataViewer response={{ oppgave }}>
+            {() => {
+                return (
+                    <>
+                        <VelgUstrukturertDokumentasjonTypeSelect
+                            label="Type dokumentasjon"
+                            onChange={(e) => {
+                                if (e.target.value.trim() !== '') {
+                                    settUstrukturertDokumentasjonType(
+                                        e.target.value as UstrukturertDokumentasjonType
+                                    );
+                                } else {
+                                    settUstrukturertDokumentasjonType(undefined);
+                                }
+                            }}
+                            value={ustrukturertDokumentasjonType}
+                        >
+                            <option value={''}>Ikke valgt</option>
+                            {[
+                                UstrukturertDokumentasjonType.PAPIRSØKNAD,
+                                UstrukturertDokumentasjonType.ETTERSENDING,
+                            ].map((type) => (
+                                <option key={type} value={type}>
+                                    {ustrukturertTypeTilTekst[type]}
+                                </option>
+                            ))}
+                        </VelgUstrukturertDokumentasjonTypeSelect>
+                        {ustrukturertDokumentasjonType ===
+                            UstrukturertDokumentasjonType.PAPIRSØKNAD && (
+                            <div>Årsak til behandling kommer settes til papirsøknad</div>
+                        )}
+                    </>
+                );
+            }}
+        </DataViewer>
     );
 };
 
