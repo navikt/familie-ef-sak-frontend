af2b89acb2a250d5fac7e31d07d750549d739e72
charliemidtlyng <charlie.midtlyng@bekk.no>
2022-06-28 14:41:24 +0200

    Veiledere skal ikke bruke og se oppgavebenken

--- src/frontend/Komponenter/Oppgavebenk/OppgavebenkApp.tsx
+++ src/frontend/Komponenter/Oppgavebenk/OppgavebenkApp.tsx
@@ -8,11 +8,21 @@ import { Side } from '../../Felles/Visningskomponenter/Side';
 import { IMappe, tomMappeListe } from './typer/mappe';
 import OppgaveFiltrering from './OppgaveFiltrering';
 import { erProd } from '../../App/utils/miljø';
+import { harTilgangTilRolle } from '../../App/utils/roller';
+import { AlertStripeInfo } from 'nav-frontend-alertstriper';
+import styled from 'styled-components';
 
 export type OppgaveRessurs = Ressurs<IOppgaverResponse>;
+const InfoVisning = styled(AlertStripeInfo)`
+    margin-top: 2rem;
+    max-width: 60rem;
+    .alertstripe__tekst {
+        max-width: 60rem;
+    }
+`;
 
 export const OppgavebenkApp: React.FC = () => {
-    const { axiosRequest } = useApp();
+    const { axiosRequest, appEnv, innloggetSaksbehandler } = useApp();
     const [oppgaveRessurs, settOppgaveRessurs] = useState<OppgaveRessurs>(byggTomRessurs());
     const [mapper, settMapper] = useState<IMappe[]>(tomMappeListe);
     const [feilmelding, settFeilmelding] = useState<string>('');
@@ -40,21 +50,37 @@ export const OppgavebenkApp: React.FC = () => {
     useEffect(() => {
         document.title = 'Oppgavebenk';
     }, []);
-
-    return (
-        <Side className={'container'}>
-            {!erProd() && <OpprettDummyBehandling />}
-            <OppgaveFiltrering
-                hentOppgaver={hentOppgaver}
-                mapper={mapper}
-                feilmelding={feilmelding}
-                settFeilmelding={settFeilmelding}
-            />
-            <OppgaveTabell
-                oppgaveRessurs={oppgaveRessurs}
-                mapper={mapper}
-                settFeilmelding={settFeilmelding}
-            />
-        </Side>
+    const harTilgangTilOppgavebenk = harTilgangTilRolle(
+        appEnv,
+        innloggetSaksbehandler,
+        'saksbehandler'
     );
+
+    if (!harTilgangTilOppgavebenk) {
+        return (
+            <Side className={'container'}>
+                <InfoVisning>
+                    Oppgavebenken er ikke tilgjengelig for veiledere. Benytt fødselsnummer i
+                    søkefelt for å finne informasjon om en person
+                </InfoVisning>
+            </Side>
+        );
+    } else {
+        return (
+            <Side className={'container'}>
+                {!erProd() && <OpprettDummyBehandling />}
+                <OppgaveFiltrering
+                    hentOppgaver={hentOppgaver}
+                    mapper={mapper}
+                    feilmelding={feilmelding}
+                    settFeilmelding={settFeilmelding}
+                />
+                <OppgaveTabell
+                    oppgaveRessurs={oppgaveRessurs}
+                    mapper={mapper}
+                    settFeilmelding={settFeilmelding}
+                />
+            </Side>
+        );
+    }
 };
