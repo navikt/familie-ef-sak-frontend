818260557b9f917e69f433f7afce9fd7445478ba
Johan Blomgren <blommish@users.noreply.github.com>
2022-09-09 11:33:59 +0200

    Legger til KopierbartNullableFødselsnummer

--- src/frontend/Komponenter/Journalforing/Brukerinfo.tsx
+++ src/frontend/Komponenter/Journalforing/Brukerinfo.tsx
@@ -1,19 +1,28 @@
 import { Normaltekst, Systemtittel } from 'nav-frontend-typografi';
 import React from 'react';
-import { formaterFødselsnummer } from '../../App/utils/formatter';
+import { KopierbartNullableFødselsnummer } from '../../Felles/Fødselsnummer/KopierbartNullableFødselsnummer';
+import styled from 'styled-components';
 
 interface BrukerinfoProps {
     navn: string;
     personIdent: string;
 }
 
+const FlexDiv = styled.div`
+    display: flex;
+    > :last-child {
+        padding-left: 0.25rem;
+    }
+`;
+
 const Brukerinfo: React.FC<BrukerinfoProps> = ({ navn, personIdent }) => {
     return (
         <>
             <Systemtittel>Navn og fødselsnummer</Systemtittel>
-            <Normaltekst>
-                {navn} - {formaterFødselsnummer(personIdent)}
-            </Normaltekst>
+            <FlexDiv>
+                <Normaltekst>{navn} -</Normaltekst>
+                <KopierbartNullableFødselsnummer fødselsnummer={personIdent} />
+            </FlexDiv>
         </>
     );
 };
