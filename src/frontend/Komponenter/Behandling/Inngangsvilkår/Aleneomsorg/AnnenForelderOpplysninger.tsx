d6b7cc6722058b75a2ac41b90e74787d0cba1932
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2022-10-13 12:37:27 +0200

    Lagt til hjelpetekst

--- src/frontend/Komponenter/Behandling/Inngangsvilkår/Aleneomsorg/AnnenForelderOpplysninger.tsx
+++ src/frontend/Komponenter/Behandling/Inngangsvilkår/Aleneomsorg/AnnenForelderOpplysninger.tsx
@@ -12,6 +12,14 @@ import {
 import { AnnenForelderNavnOgFnr } from '../NyttBarnSammePartner/AnnenForelderNavnOgFnr';
 import { harVerdi } from '../../../../App/utils/utils';
 import { formaterNullableIsoDato } from '../../../../App/utils/formatter';
+import { HelpText } from '@navikt/ds-react';
+import styled from 'styled-components';
+
+const StyledHelpText = styled(HelpText)`
+    & + .navds-popover {
+        max-width: 40rem;
+    }
+`;
 
 interface Props {
     forelderRegister?: IAnnenForelder;
@@ -126,6 +134,30 @@ const AnnenForelderOpplysninger: FC<Props> = ({ forelderRegister, søknadsgrunnl
                         {forelderRegister &&
                             utledAvstandTilSøkerTekst(forelderRegister?.avstandTilSøker)}
                     </Normaltekst>
+                    <StyledHelpText placement={'right'}>
+                        Automatisk beregning av avstand mellom bostedsadressene til bruker og annen
+                        forelder. Dette betyr de ulike resultatene:
+                        <ul>
+                            <li>
+                                <b>xx km</b> - Det er automatisk beregnet nøyaktig avstand mellom
+                                boligene i luftlinje.
+                            </li>
+                            <li>
+                                <b>Kan ikke beregnes automatisk</b> - Vilkåret må vurderes manuelt
+                                fordi avstanden mellom boligene er mindre enn 1 km i luftlinje,
+                                eller en av forelder er registrert med bostedsadresse utenfor Norge.
+                            </li>
+                            <li>
+                                <b>Mer enn 1 km</b> - Det er automatisk beregnet at det er mer enn 1
+                                km mellom boligene i luftlinje. Det kan ikke vises helt nøyaktig
+                                avstand da avstandsberegningen er upresis i retning øst og vest i
+                                Nord-Norge, fordi Nord-Norge ligger i et annet koordinatsystem. For
+                                å unngå feilmarginen det gir, vil det kun avstandsberegnes i retning
+                                nord og sør for brukere som bor i Nord-Norge, da dette ikke avhenger
+                                av koordinatsystem.
+                            </li>
+                        </ul>
+                    </StyledHelpText>
                 </>
             )}
         </GridTabell>
