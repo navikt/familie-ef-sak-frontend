0d6cb38bcae4e41de252edcb476466ff34e7d742
Johan Blomgren <blommish@users.noreply.github.com>
2022-11-03 13:56:27 +0100

    Link til journalføring av klage (#1710)

--- src/frontend/Komponenter/Journalføring/journalføringUtil.ts
+++ src/frontend/Komponenter/Journalføring/journalføringUtil.ts
@@ -4,14 +4,17 @@ export const JOURNALPOST_QUERY_STRING = 'journalpostId';
 export const OPPGAVEID_QUERY_STRING = 'oppgaveId';
 export const FRA_VANLIG_QUERY_STRING = 'fraVanlig';
 
-export const lagJournalføringKlageUrl = (journalpostId: string, oppgaveId: string): string => {
+export const lagJournalføringKlageUrl = (
+    journalpostId: string,
+    oppgaveId: string | number
+): string => {
     return (
         `/journalfor-klage?${JOURNALPOST_QUERY_STRING}=${journalpostId}` +
         `&${OPPGAVEID_QUERY_STRING}=${oppgaveId}&${FRA_VANLIG_QUERY_STRING}=true`
     );
 };
 
-export const lagJournalføringUrl = (journalpostId: string, oppgaveId: string): string => {
+export const lagJournalføringUrl = (journalpostId: string, oppgaveId: string | number): string => {
     return `/journalfor?${JOURNALPOST_QUERY_STRING}=${journalpostId}&${OPPGAVEID_QUERY_STRING}=${oppgaveId}`;
 };
 
