cd8fe881d03ce783a2a67bd4c24be87473e99e63
eirarset <eirik.arseth@nav.no>
2022-11-14 16:00:09 +0100

    Legger til enums for årsdak revurdering

--- src/frontend/Komponenter/Behandling/ÅrsakRevurdering/ÅrsakRevurdering.tsx
+++ src/frontend/Komponenter/Behandling/ÅrsakRevurdering/ÅrsakRevurdering.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import ToKolonnerLayout from '../../../Felles/Visningskomponenter/ToKolonnerLayout';
+import IkkeVurdert from '../../../Felles/Ikoner/IkkeVurdert';
+import Oppfylt from '../../../Felles/Ikoner/Oppfylt';
+import { Undertittel } from 'nav-frontend-typografi';
+import styled from 'styled-components';
 
 interface Props {
     behandlingId: string;
 }
 
-export const ÅrsakRevurdering: React.FC<Props> = () => {
-    // const { behandling } = useBehandling();
+const FlexDiv = styled.div`
+    display: flex;
+    gap: 0.5rem;
+`;
 
-    return <div>Årsak Revurdering</div>;
+export const ÅrsakRevurdering: React.FC<Props> = () => {
+    return (
+        <ToKolonnerLayout skillelinje={false}>
+            {{
+                venstre: (
+                    <FlexDiv>
+                        <ÅrsakRevurderingIkon oppfylt={true} />
+                        <Undertittel>Årsak til revurdering</Undertittel>
+                    </FlexDiv>
+                ),
+                høyre: <></>,
+            }}
+        </ToKolonnerLayout>
+    );
 };
+
+const ÅrsakRevurderingIkon: React.FC<{ oppfylt: boolean }> = ({ oppfylt }) =>
+    oppfylt ? <Oppfylt heigth={23} width={21} /> : <IkkeVurdert heigth={23} width={21} />;
