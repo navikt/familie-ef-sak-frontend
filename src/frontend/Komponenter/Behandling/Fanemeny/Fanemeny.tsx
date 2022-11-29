63b9fb029346e0f2b46a730ca4f05c50b96e6810
Johan Blomgren <blommish@users.noreply.github.com>
2022-11-29 17:49:24 +0100

    Fjerner feature toggle for årsak revurdering (#1785)

--- src/frontend/Komponenter/Behandling/Fanemeny/Fanemeny.tsx
+++ src/frontend/Komponenter/Behandling/Fanemeny/Fanemeny.tsx
@@ -9,8 +9,6 @@ import navFarger from 'nav-frontend-core';
 import { Steg } from '../Høyremeny/Steg';
 import Fane from './Fane';
 import { useApp } from '../../../App/context/AppContext';
-import { useToggles } from '../../../App/context/TogglesContext';
-import { ToggleName } from '../../../App/context/toggles';
 
 const StickyMedBoxShadow = styled(Sticky)`
     box-shadow: 0 2px 5px rgba(0, 0, 0, 0.4);
@@ -33,7 +31,6 @@ interface Props {
 const Fanemeny: FC<Props> = ({ behandlingId }) => {
     const { behandling, behandlingErRedigerbar } = useBehandling();
     const { erSaksbehandler } = useApp();
-    const { toggles } = useToggles();
     const låsendeSteg = [Steg.VILKÅR, Steg.BEREGNE_YTELSE];
     const fanerSomKanLåses = [SideNavn.SIMULERING, SideNavn.BREV, SideNavn.KORRIGERING_UTEN_BREV];
     const fanerSomErLåstForVeilederUnderArbeid = [
@@ -49,18 +46,13 @@ const Fanemeny: FC<Props> = ({ behandlingId }) => {
         return fanerSomKanLåses.includes(side.navn as SideNavn) && låsendeSteg.includes(steg);
     };
 
-    const skalViseÅrsakRevurdering = toggles[ToggleName.årsakRevurdering];
-
     return (
         <DataViewer response={{ behandling }}>
             {({ behandling }) => (
                 <>
                     <StickyMedBoxShadow>
                         <StyledFanemeny>
-                            {filtrerSiderEtterBehandlingstype(
-                                behandling,
-                                skalViseÅrsakRevurdering
-                            ).map((side, index) => (
+                            {filtrerSiderEtterBehandlingstype(behandling).map((side, index) => (
                                 <Fane
                                     side={side}
                                     behandlingId={behandlingId}
