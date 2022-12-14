ab4dc046f4a84e9b3a05a9822693bac36bef274b
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2022-12-14 09:17:14 +0100

    Styling fagsakId slik at den ikke blir i bold

--- src/frontend/Komponenter/Personoversikt/FagsakTittelLinje.tsx
+++ src/frontend/Komponenter/Personoversikt/FagsakTittelLinje.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Fagsak } from '../../App/typer/fagsak';
 import { formatterEnumVerdi } from '../../App/utils/utils';
 import { Heading, Tag } from '@navikt/ds-react';
+import { BodyShortSmall } from '../../Felles/Visningskomponenter/Tekster';
 
 const StyledTag = styled(Tag)`
     margin-left: 1rem;
@@ -12,13 +13,20 @@ const TittelLinje = styled.div`
     display: flex;
     align-items: flex-start;
 `;
+
+const TekstMedMargin = styled(BodyShortSmall)`
+    margin-top: 0.3rem;
+    margin-left: 0.3rem;
+`;
+
 export const FagsakTittelLinje: React.FC<{
     fagsak: Fagsak;
 }> = ({ fagsak }) => (
     <TittelLinje>
         <Heading size={'small'} level="3">
-            Fagsak: {formatterEnumVerdi(fagsak.stønadstype)} ({fagsak.eksternId})
+            Fagsak: {formatterEnumVerdi(fagsak.stønadstype)}
         </Heading>
+        <TekstMedMargin>({fagsak.eksternId})</TekstMedMargin>
         {fagsak.erLøpende && (
             <StyledTag variant={'info'} size={'small'}>
                 Løpende
