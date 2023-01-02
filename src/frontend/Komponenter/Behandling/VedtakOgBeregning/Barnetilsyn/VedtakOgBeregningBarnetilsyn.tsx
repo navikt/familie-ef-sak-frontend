c1e35c5f90bda3fbd6e6c4f3ac6f7d5d70c2eccf
Johan Blomgren <blommish@users.noreply.github.com>
2023-01-02 11:12:32 +0100

    Barnetilsyn revurder fra (#1863)
    * Revurder fra for barnetilsyn
    
    * Gjør RevurderesFraOgMed til felles
    
    * Skal resette stønadsreduksjonState når man endrer revurderFra
    
    * lar revurderFraInitPeriode legge til tomPeriode

--- src/frontend/Komponenter/Behandling/VedtakOgBeregning/Barnetilsyn/VedtakOgBeregningBarnetilsyn.tsx
+++ src/frontend/Komponenter/Behandling/VedtakOgBeregning/Barnetilsyn/VedtakOgBeregningBarnetilsyn.tsx
@@ -8,11 +8,10 @@ import { erAlleVilkårOppfylt, skalViseNullstillVedtakKnapp } from '../Felles/ut
 import { RessursStatus } from '../../../../App/typer/ressurs';
 import SelectVedtaksresultat from '../Felles/SelectVedtaksresultat';
 import DataViewer from '../../../../Felles/DataViewer/DataViewer';
-import { Vedtaksform } from './Vedtaksform';
 import { AvslåVedtak } from '../Overgangsstønad/AvslåVedtak/AvslåVedtak';
 import { Opphør } from '../Overgangsstønad/Opphør/Opphør';
 import { barnSomOppfyllerAlleVilkår } from './utils';
-import { v4 as uuidv4 } from 'uuid';
+import { InnvilgeBarnetilsyn } from './InnvilgeBarnetilsyn';
 
 interface Props {
     behandling: Behandling;
@@ -26,29 +25,6 @@ const Wrapper = styled.div`
 const WrapperMarginTop = styled.div`
     margin-top: 1rem;
 `;
-
-const oppdaterVedtakMedEndretKey = (
-    vedtak: IInnvilgeVedtakForBarnetilsyn | undefined
-): IInnvilgeVedtakForBarnetilsyn | undefined => {
-    if (!vedtak) {
-        return vedtak;
-    }
-    return {
-        ...vedtak,
-        perioder: vedtak.perioder.map((periode) => ({ ...periode, endretKey: uuidv4() })),
-        perioderKontantstøtte: vedtak.perioderKontantstøtte.map((periode) => ({
-            ...periode,
-            endretKey: uuidv4(),
-        })),
-        tilleggsstønad: {
-            ...vedtak.tilleggsstønad,
-            perioder: vedtak.tilleggsstønad.perioder.map((periode) => ({
-                ...periode,
-                endretKey: uuidv4(),
-            })),
-        },
-    };
-};
 
 const VedtakOgBeregningBarnetilsyn: FC<Props> = ({ behandling, vilkår }) => {
     const behandlingId = behandling.id;
@@ -83,11 +59,11 @@ const VedtakOgBeregningBarnetilsyn: FC<Props> = ({ behandling, vilkår }) => {
                             case EBehandlingResultat.INNVILGE:
                             case EBehandlingResultat.INNVILGE_UTEN_UTBETALING:
                                 return (
-                                    <Vedtaksform
+                                    <InnvilgeBarnetilsyn
                                         behandling={behandling}
-                                        lagretVedtak={oppdaterVedtakMedEndretKey(
-                                            vedtak as IInnvilgeVedtakForBarnetilsyn | undefined // TODO: Fjern "as" når vi får på plass vedtakDto-håndtering(egen oppgave)
-                                        )}
+                                        lagretVedtak={
+                                            vedtak as IInnvilgeVedtakForBarnetilsyn | undefined
+                                        }
                                         barn={barnSomOppfyllerAlleVilkår(vilkår)}
                                         settResultatType={settResultatType}
                                     />
--- src/frontend/Komponenter/Behandling/VedtakOgBeregning/Felles/RevurderesFraOgMed.tsx
+++ src/frontend/Komponenter/Behandling/VedtakOgBeregning/Felles/RevurderesFraOgMed.tsx
@@ -1,10 +1,8 @@
 import React, { Dispatch, SetStateAction } from 'react';
-import MånedÅrVelger from '../../../../../Felles/Input/MånedÅr/MånedÅrVelger';
+import MånedÅrVelger from '../../../../Felles/Input/MånedÅr/MånedÅrVelger';
 import styled from 'styled-components';
-import { IVedtakshistorikk } from '../../../../../App/typer/vedtak';
-import { revurdererFørFørstePeriode } from './revurderFraUtils';
-import { AlertWarning } from '../../../../../Felles/Visningskomponenter/Alerts';
-import { EnsligErrorMessage } from '../../../../../Felles/ErrorMessage/EnsligErrorMessage';
+import { AlertWarning } from '../../../../Felles/Visningskomponenter/Alerts';
+import { EnsligErrorMessage } from '../../../../Felles/ErrorMessage/EnsligErrorMessage';
 
 const WrapperMarginBottom = styled.div`
     margin-bottom: 2rem;
@@ -22,8 +20,8 @@ export const RevurderesFraOgMed: React.FC<{
     settRevurderesFra: Dispatch<SetStateAction<string | undefined>>;
     revurderesFra: string | undefined;
     feilmelding: string | null;
-    vedtakshistorikk: IVedtakshistorikk | undefined;
-}> = ({ settRevurderesFra, revurderesFra, feilmelding, vedtakshistorikk }) => {
+    revurdererFørFørstePeriode: boolean;
+}> = ({ settRevurderesFra, revurderesFra, feilmelding, revurdererFørFørstePeriode }) => {
     return (
         <WrapperMarginBottom>
             <MånedÅrVelger
@@ -38,7 +36,7 @@ export const RevurderesFraOgMed: React.FC<{
                 årMånedInitiell={revurderesFra}
             />
             <EnsligErrorMessage>{feilmelding}</EnsligErrorMessage>
-            {revurdererFørFørstePeriode(vedtakshistorikk, revurderesFra) && (
+            {revurdererFørFørstePeriode && (
                 <Advarsel>
                     Fom-datoen for denne revurderingen er før fom-datoen for tidligere vedtak. Husk
                     å fylle ut vedtaksperiode og inntekt for den nye perioden.
