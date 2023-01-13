60cace12f6657041fb5b576fb604c4006cb05594
sarahjelle <sara.hjelle@bekk.no>
2023-01-13 10:02:14 +0100

    Legg til toggle på sett på vent knapp

--- src/frontend/Felles/PersonHeader/PersonHeaderHamburgermeny.tsx
+++ src/frontend/Felles/PersonHeader/PersonHeaderHamburgermeny.tsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import { useBehandling } from '../../App/context/BehandlingContext';
+import { ToggleName } from '../../App/context/toggles';
+import { useToggles } from '../../App/context/TogglesContext';
 import { Hamburgermeny } from '../Hamburgermeny/Hamburgermeny';
 
 export const PersonHeaderHamburgermeny = () => {
     const { settVisHenleggModal, settVisSettpåVentModal } = useBehandling();
+    const { toggles } = useToggles();
 
-    return (
-        <Hamburgermeny
-            items={[
-                {
-                    tekst: 'Henlegg',
-                    onClick: () => settVisHenleggModal(true),
-                },
-                {
-                    tekst: 'Sett på vent',
-                    onClick: () => settVisSettpåVentModal(true),
-                },
-            ]}
-        />
-    );
+    const skalViseSettPåVentKnapp = toggles[ToggleName.visSettPåVentKnapp];
+
+    const menyvalg = [
+        {
+            tekst: 'Henlegg',
+            onClick: () => settVisHenleggModal(true),
+        },
+    ];
+
+    if (skalViseSettPåVentKnapp) {
+        menyvalg.push({
+            tekst: 'Sett på vent',
+            onClick: () => settVisSettpåVentModal(true),
+        });
+    }
+
+    return <Hamburgermeny items={menyvalg} />;
 };
