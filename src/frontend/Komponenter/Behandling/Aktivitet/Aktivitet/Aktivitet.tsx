4e935367ac3c60638e9cbfe93e39716cc1433c7b
sarahjelle <sara.hjelle@bekk.no>
2023-01-25 09:51:07 +0100

    Fjern vilkårtittel fra aktivitet

--- src/frontend/Komponenter/Behandling/Aktivitet/Aktivitet/Aktivitet.tsx
+++ src/frontend/Komponenter/Behandling/Aktivitet/Aktivitet/Aktivitet.tsx
@@ -3,7 +3,6 @@ import { VilkårProps } from '../../Inngangsvilkår/vilkårprops';
 import { AktivitetsvilkårType } from '../../Inngangsvilkår/vilkår';
 import VisEllerEndreVurdering from '../../Vurdering/VisEllerEndreVurdering';
 import AktivitetInfo from './AktivitetInfo';
-import { Vilkårstittel } from '../../Inngangsvilkår/Vilkårstittel';
 import { useBehandling } from '../../../../App/context/BehandlingContext';
 import DataViewer from '../../../../Felles/DataViewer/DataViewer';
 import { Behandlingsårsak } from '../../../../App/typer/Behandlingsårsak';
@@ -45,10 +44,6 @@ export const Aktivitet: React.FC<VilkårProps> = ({
                                 {{
                                     venstre: (
                                         <>
-                                            <Vilkårstittel
-                                                tittel="Aktivitet"
-                                                vilkårsresultat={vurdering.resultat}
-                                            />
                                             {grunnlag.aktivitet && (
                                                 <AktivitetInfo
                                                     aktivitet={grunnlag.aktivitet}
