4f1dd3a80b35543a5587846a56413e155c3d44aa
sarahjelle <sara.hjelle@bekk.no>
2023-02-01 15:19:25 +0100

    Ny forenklet tabellvisning med nytt design

---
+++ src/frontend/Komponenter/Behandling/Vilkårpanel/TabellVisning.tsx
@@ -0,0 +1,76 @@
+import * as React from 'react';
+import { Registergrunnlag, Søknadsgrunnlag } from '../../../Felles/Ikoner/DataGrunnlagIkoner';
+import { Calculator } from '@navikt/ds-icons';
+import { Detail, HelpText, Label } from '@navikt/ds-react';
+import { BodyShortSmall } from '../../../Felles/Visningskomponenter/Tekster';
+import styled from 'styled-components';
+import { GridTabell } from './GridTabell';
+
+export enum TabellIkon {
+    REGISTER = 'REGISTER',
+    SØKNAD = 'SØKNAD',
+    KALKULATOR = 'KALKULATOR',
+}
+
+export interface Kolonndata<T> {
+    ikon?: TabellIkon;
+    tittel?: string;
+    undertittel?: string;
+    tittelType?: 'element' | 'undertittel';
+    verdier: T[];
+    kolonner: Kolonner<T>[];
+    underTabellMargin?: number;
+}
+
+export interface Kolonner<T> {
+    overskrift: string;
+    helperText?: string;
+    tekstVerdi: (data: T) => React.ReactNode;
+}
+
+export const mapIkon = (ikon: TabellIkon) => {
+    switch (ikon) {
+        case TabellIkon.REGISTER:
+            return <Registergrunnlag />;
+        case TabellIkon.SØKNAD:
+            return <Søknadsgrunnlag />;
+        case TabellIkon.KALKULATOR:
+            return <Calculator />;
+    }
+};
+
+const FlexDiv = styled.div`
+    display: flex;
+    gap: 1rem;
+`;
+
+function TabellVisning<T>(props: Kolonndata<T>): React.ReactElement<Kolonndata<T>> {
+    const { ikon, tittel, verdier, kolonner } = props;
+    return (
+        <GridTabell kolonner={kolonner.length + 1}>
+            {ikon && mapIkon(ikon)}
+            {tittel && (
+                <Label size="small" className="tittel" as="h3">
+                    {tittel}
+                </Label>
+            )}
+            {kolonner.map((kolonne, index) => (
+                <FlexDiv className={index === 0 ? 'førsteDataKolonne' : ''} key={index}>
+                    <Detail>
+                        <strong>{kolonne.overskrift}</strong>
+                    </Detail>
+                    {kolonne.helperText && <HelpText>{kolonne.helperText}</HelpText>}
+                </FlexDiv>
+            ))}
+            {verdier.map((item) =>
+                kolonner.map((kolonne, index) => (
+                    <BodyShortSmall className={index === 0 ? 'førsteDataKolonne' : ''} key={index}>
+                        {kolonne.tekstVerdi(item) || ''}
+                    </BodyShortSmall>
+                ))
+            )}
+        </GridTabell>
+    );
+}
+
+export default TabellVisning;
