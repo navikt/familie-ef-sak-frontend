1565e56d20c0b8d0b995c52afcc251bc6e49765b
charliemidtlyng <charlie.midtlyng@bekk.no>
2023-02-01 14:55:33 +0100

    Skal bruke tertiærknapp med eksternlenke-ikon for å lenke til historisk pensjon fra tidligere vedtaksperioder

--- src/frontend/Komponenter/Personoversikt/Historiskpensjon/Historiskpensjon.tsx
+++ src/frontend/Komponenter/Personoversikt/Historiskpensjon/Historiskpensjon.tsx
@@ -6,12 +6,13 @@ import { useDataHenter } from '../../../App/hooks/felles/useDataHenter';
 import { IHistoriskPensjon } from '../../../App/typer/historiskpensjon';
 import DataViewer from '../../../Felles/DataViewer/DataViewer';
 import { BodyShort, Button, Link } from '@navikt/ds-react';
+import { ExternalLink } from '@navikt/ds-icons';
 
 const FlexBox = styled.div`
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
-    gap: 2rem;
+    gap: 1rem;
     align-items: center;
 `;
 
@@ -43,14 +44,23 @@ const Historiskpensjon: React.FC<{ fagsakPersonId: string }> = ({ fagsakPersonId
                         <FlexBox>
                             <BodyShort>Bruker har fått stønad før desember 2008</BodyShort>
                             <Link href={historiskPensjon.webAppUrl} target={'_blank'}>
-                                <Button type={'button'} as={'p'} size={'small'}>
+                                <Button
+                                    type={'button'}
+                                    as={'p'}
+                                    size={'small'}
+                                    variant={'tertiary'}
+                                    icon={<ExternalLink />}
+                                    iconPosition={'right'}
+                                >
                                     Se vedtaksperioder
                                 </Button>
                             </Link>
                         </FlexBox>
                     </StyledWarningStripe>
                 ) : (
-                    <StyledInfoStripe>Bruker har ikke stønad før desember 2008</StyledInfoStripe>
+                    <StyledInfoStripe>
+                        Bruker har ikke fått stønad før desember 2008
+                    </StyledInfoStripe>
                 );
             }}
         </DataViewer>
