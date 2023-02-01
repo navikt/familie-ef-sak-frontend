8c554c615d66989c65d4146b307f4773969f4541
sarahjelle <sara.hjelle@bekk.no>
2023-02-01 15:21:56 +0100

    Lage egen fil for generelle styled komponents brukt i vilkår

---
+++ src/frontend/Komponenter/Behandling/Vilkårpanel/StyledVilkårInnhold.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const InformasjonContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+`;
+
+export const FlexColumnContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1.5rem;
+`;
