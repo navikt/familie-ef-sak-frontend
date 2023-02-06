8b795477f20745afdd0af4fbffeec43473d37e92
sarahjelle <sara.hjelle@bekk.no>
2023-02-06 14:14:04 +0100

    Oppdater tabellvisning for å matche ny styling

--- src/frontend/Komponenter/Behandling/Tabell/TabellVisning.tsx
+++ src/frontend/Komponenter/Behandling/Tabell/TabellVisning.tsx
@@ -1,49 +1,53 @@
 import * as React from 'react';
+import { BodyShortSmall } from '../../../Felles/Visningskomponenter/Tekster';
+import styled from 'styled-components';
+import { Detail, HelpText, Label } from '@navikt/ds-react';
 import { Registergrunnlag, Søknadsgrunnlag } from '../../../Felles/Ikoner/DataGrunnlagIkoner';
 import { Calculator } from '@navikt/ds-icons';
-import { Heading } from '@navikt/ds-react';
-import { BodyShortSmall, SmallTextLabel } from '../../../Felles/Visningskomponenter/Tekster';
-import styled from 'styled-components';
 
 const GridTabell = styled.div<{
     kolonner?: number;
-    utenIkon: boolean;
+    ikonVisning: boolean;
 }>`
     display: grid;
-    grid-template-columns: ${(props) => props.utenIkon && '21px'} min(200px, 250px) repeat(
+    grid-template-columns: ${(props) => props.ikonVisning && '21px'} min(200px, 250px) repeat(
             ${(props) => (props.kolonner ? props.kolonner - 2 : 2)},
             ${(props) => (props.kolonner && props.kolonner > 3 ? '150px' : '325px')}
         );
     grid-gap: 0.5rem;
     .tittel {
         padding-bottom: 0.25rem;
-        grid-column: ${(props) => (props.utenIkon ? 2 : 1)} /
+        grid-column: ${(props) => (props.ikonVisning ? 2 : 1)} /
             ${(props) => (props.kolonner || 3) + 1};
         display: flex;
         align-items: center;
     }
     .førsteDataKolonne {
-        grid-column: ${(props) => (props.utenIkon ? '2/3' : '1/2')};
+        grid-column: ${(props) => (props.ikonVisning ? '2/3' : '1/2')};
     }
 `;
 
-export enum TabellIkon {
-    REGISTER = 'REGISTER',
-    SØKNAD = 'SØKNAD',
-    KALKULATOR = 'KALKULATOR',
-}
-
-export interface Kolonndata<T> {
+const FlexDiv = styled.div`
+    display: flex;
+    gap: 1rem;
+`;
+export interface Kolonnedata<T> {
     ikon?: TabellIkon;
-    tittel: string;
-    tittelType?: 'element' | 'undertittel';
+    tittel?: string;
     verdier: T[];
     kolonner: Kolonner<T>[];
 }
 
 export interface Kolonner<T> {
     overskrift: string;
+    hjelpetekst?: string;
     tekstVerdi: (data: T) => React.ReactNode;
+}
+
+export enum TabellIkon {
+    REGISTER = 'REGISTER',
+    SØKNAD = 'SØKNAD',
+    KALKULATOR = 'KALKULATOR',
 }
 
 const mapIkon = (ikon: TabellIkon) => {
@@ -57,33 +61,31 @@ const mapIkon = (ikon: TabellIkon) => {
     }
 };
 
-function TabellVisning<T>(props: Kolonndata<T>): React.ReactElement<Kolonndata<T>> {
+function TabellVisning<T>(props: Kolonnedata<T>): React.ReactElement<Kolonnedata<T>> {
     const { ikon, tittel, verdier, kolonner } = props;
     return (
-        <GridTabell kolonner={kolonner.length + 1} utenIkon={!ikon}>
+        <GridTabell kolonner={kolonner.length + 1} ikonVisning={!!ikon}>
             {ikon && mapIkon(ikon)}
-
-            <Heading size="small" className="tittel" level={'3'}>
-                {tittel}
-            </Heading>
-
-            <>
-                {kolonner.map((kolonne, index) => (
-                    <SmallTextLabel className={index === 0 ? 'førsteDataKolonne' : ''} key={index}>
-                        {kolonne.overskrift}
-                    </SmallTextLabel>
-                ))}
-                {verdier.map((item) =>
-                    kolonner.map((kolonne, index) => (
-                        <BodyShortSmall
-                            className={index === 0 ? 'førsteDataKolonne' : 'kolonne'}
-                            key={index}
-                        >
-                            {kolonne.tekstVerdi(item) || ''}
-                        </BodyShortSmall>
-                    ))
-                )}
-            </>
+            {tittel && (
+                <Label size="small" className="tittel" as="h3">
+                    {tittel}
+                </Label>
+            )}
+            {kolonner.map((kolonne, index) => (
+                <FlexDiv className={index === 0 ? 'førsteDataKolonne' : ''} key={index}>
+                    <Detail>
+                        <strong>{kolonne.overskrift}</strong>
+                    </Detail>
+                    {kolonne.hjelpetekst && <HelpText>{kolonne.hjelpetekst}</HelpText>}
+                </FlexDiv>
+            ))}
+            {verdier.map((item) =>
+                kolonner.map((kolonne, index) => (
+                    <BodyShortSmall className={index === 0 ? 'førsteDataKolonne' : ''} key={index}>
+                        {kolonne.tekstVerdi(item) || ''}
+                    </BodyShortSmall>
+                ))
+            )}
         </GridTabell>
     );
 }
