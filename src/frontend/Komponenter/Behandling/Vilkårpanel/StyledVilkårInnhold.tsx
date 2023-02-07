ae9a3a32ad91b2de228198dc1069d0cfc2463af4
sarahjelle <sara.hjelle@bekk.no>
2023-02-07 13:33:02 +0100

    Styling. Legg inn margin-right for å ikke kræsje med høyreside

--- src/frontend/Komponenter/Behandling/Vilkårpanel/StyledVilkårInnhold.tsx
+++ src/frontend/Komponenter/Behandling/Vilkårpanel/StyledVilkårInnhold.tsx
@@ -5,6 +5,7 @@ export const InformasjonContainer = styled.div`
     display: flex;
     flex-direction: column;
     gap: 1.5rem;
+    margin-right: 1rem;
 `;
 
 export const FlexColumnContainer = styled.div<{
