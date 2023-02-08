62b57b78467dcd1992fa977cb8c70dfafa3e8704
sarahjelle <sara.hjelle@bekk.no>
2023-02-08 14:44:47 +0100

    Styling. Mink spacing mellom elementene

--- src/frontend/Komponenter/Behandling/Vilkårpanel/VilkårInformasjonKomponenter.tsx
+++ src/frontend/Komponenter/Behandling/Vilkårpanel/VilkårInformasjonKomponenter.tsx
@@ -43,7 +43,7 @@ export const InfoSeksjonWrapper: FC<InfoSeksjonWrapperProps> = ({
     children,
 }) => {
     return (
-        <FlexColumnContainer>
+        <FlexColumnContainer gap={1}>
             <UnderoverskriftWrapper>
                 {ikon}
                 <HeadingMedUnderlinje size="xsmall">{undertittel}</HeadingMedUnderlinje>
