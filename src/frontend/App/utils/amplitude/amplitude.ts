610da5a069d1bf3474b8d248e60b42655ceae529
sarahjelle <sara.hjelle@bekk.no>
2023-03-29 10:19:57 +0200

    Legg til api-keys i kildekode fordi de er public

--- src/frontend/App/utils/amplitude/amplitude.ts
+++ src/frontend/App/utils/amplitude/amplitude.ts
@@ -1,8 +1,16 @@
 import amplitude from 'amplitude-js';
+import { erProd } from '../miljø';
+
+const getApiKey = () => {
+    if (erProd()) {
+        return '950365543c4356bd38a6c8b1de8e166e'; // prod
+    }
+    return '84389e45c7a84638cbd753b471db5fb7'; // dev
+};
 
 const amplitudeInstance = amplitude.getInstance();
 
-amplitudeInstance.init('84389e45c7a84638cbd753b471db5fb7', '', {
+amplitudeInstance.init(getApiKey(), '', {
     apiEndpoint: 'amplitude.nav.no/collect',
     saveEvents: false,
     includeUtm: true,
