3db24c9138b637ec2ce8a51f80716efa8a7540de
eirarset <eirik.arseth@nav.no>
2023-06-06 13:26:15 +0200

    Tekstendring i brev for innhenting av karakterutskrift - utvidet, etter bestilling fra Mirja

--- src/frontend/Komponenter/Behandling/Brev/BrevTyperTekst.ts
+++ src/frontend/Komponenter/Behandling/Brev/BrevTyperTekst.ts
@@ -177,7 +177,7 @@ export const initielleAvsnittInnhentingAvKarakterutskriftUtvidetPeriode: Avsnitt
     {
         deloverskrift: '',
         innhold:
-            'Du får utvidet tid med overgangsstønad fordi du er i utdanning.' +
+            'Du får overgangsstønad og er i utdanning.' +
             '\n\nVi følger opp saken din, og ber deg om å sende oss:' +
             '\n\n•\tKarakterutskrift for skoleåret 2021/2022' +
             '\n\nDu må sende oss dokumentasjonen innen 21.07.22.',
