451c01026853f5cecedd0017d41c9ae9eef60e13
Viktor <viktor.grondalen.solberg@nav.no>
2023-06-13 13:41:35 +0200

    Legg til knapp skal ta inn variant slik at man kan velge tertiare selv om knapper har tekst. Dette kommer til å bli brukt i nytt gui for skolepenger

--- src/frontend/Felles/Knapper/LeggTilKnapp.tsx
+++ src/frontend/Felles/Knapper/LeggTilKnapp.tsx
@@ -1,20 +1,22 @@
 import { AddCircle } from '@navikt/ds-icons';
 import React from 'react';
 import { Knapp } from './HovedKnapp';
+import { ButtonProps } from '@navikt/ds-react';
 
 const LeggTilKnapp: React.FC<{
     className?: string;
     ikontekst?: string;
     knappetekst?: string;
     onClick: () => void;
-}> = ({ className, ikontekst, knappetekst, onClick }) => {
+    variant?: ButtonProps;
+}> = ({ className, ikontekst, knappetekst, onClick, variant }) => {
     return (
         <Knapp
             className={className}
             icon={<AddCircle title={knappetekst ? knappetekst : ikontekst} />}
             onClick={onClick}
             type="button"
-            variant={knappetekst ? 'secondary' : 'tertiary'}
+            variant={variant ? variant : 'secondary'}
         >
             {knappetekst && <span>{knappetekst}</span>}
         </Knapp>
