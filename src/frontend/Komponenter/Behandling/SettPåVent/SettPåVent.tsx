68863e509eb25b8bbc36d83c928de71548693601
throndi <56258085+throndi@users.noreply.github.com>
2023-06-29 23:20:34 +0200

    ...

--- src/frontend/Komponenter/Behandling/SettPåVent/SettPåVent.tsx
+++ src/frontend/Komponenter/Behandling/SettPåVent/SettPåVent.tsx
@@ -188,6 +188,13 @@ export const SettPåVent: FC<{ behandling: Behandling }> = ({ behandling }) => {
 
         settLåsKnapp(true);
 
+        if (oppgave.status !== RessursStatus.SUKSESS || !oppgave.data.versjon || !oppgave.data.id) {
+            settFeilmelding(
+                'Teknisk feil. Mangler versjonsnumer for oppgave. Kontakt brukerstøtte'
+            );
+            return;
+        }
+
         axiosRequest<string, SettPåVentRequest>({
             method: 'POST',
             url: `/familie-ef-sak/api/behandling/${behandling.id}/vent`,
