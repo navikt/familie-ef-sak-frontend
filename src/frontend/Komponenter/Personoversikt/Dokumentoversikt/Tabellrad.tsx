6c72eae33c793f95b662eb4a7182162b5c2a16cb
eirarset <eirik.arseth@nav.no>
2023-07-06 09:51:30 +0200

    Skal vise låsikon foran dokumenttittel som bruker ikke har tilgang til

--- src/frontend/Komponenter/Personoversikt/Dokumentoversikt/Tabellrad.tsx
+++ src/frontend/Komponenter/Personoversikt/Dokumentoversikt/Tabellrad.tsx
@@ -6,6 +6,8 @@ import { BodyShortSmall } from '../../../Felles/Visningskomponenter/Tekster';
 import styled from 'styled-components';
 import { useToggles } from '../../../App/context/TogglesContext';
 import { skalViseLenke } from '../utils';
+import { IkkeTilgang } from './Hovedtabellrad';
+import { PadlockLockedIcon } from '@navikt/aksel-icons';
 
 const LenkeVenstreMargin = styled.a`
     margin-left: 2rem;
@@ -36,7 +38,10 @@ export const Tabellrad: React.FC<{ dokument: Dokumentinfo; erKlikketId: string }
                         <LogiskeVedlegg logiskeVedlegg={dokument.logiskeVedlegg} />
                     </>
                 ) : (
-                    <BodyShortSmall>{dokument.tittel}</BodyShortSmall>
+                    <IkkeTilgang>
+                        <PadlockLockedIcon title="Mangler tilgang til dokument" />
+                        <BodyShortSmall>{dokument.tittel}</BodyShortSmall>
+                    </IkkeTilgang>
                 )}
             </Td>
             <Td></Td>
