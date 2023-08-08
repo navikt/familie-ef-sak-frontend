e80bdef21198d22d9a343160fa59f08e595a2ba9
Johan Blomgren <blommish@users.noreply.github.com>
2023-08-08 09:42:09 +0200

    Fjerner toggle for visning av endringer av personopplysninger

--- src/frontend/App/context/toggles.ts
+++ src/frontend/App/context/toggles.ts
@@ -11,9 +11,7 @@ export enum ToggleName {
     opprettBehandlingForFerdigstiltJournalpost = 'familie.ef.sak.opprett-behandling-for-ferdigstilt-journalpost', // Permission-toggle
     visAutomatiskUtfylleVilkår = 'familie.ef.sak.frontend-automatisk-utfylle-vilkar', // Miljø-toggle
     visSatsendring = 'familie.ef.sak.frontend-vis-satsendring',
-    visEndringerPersonopplysninger = 'familie.ef.sak.frontend.personopplysninger-endringer',
     visInntektPersonoversikt = 'familie.ef.sak.frontend.vis-inntekt-personoversikt',
-    angreSendTilBeslutter = 'familie.ef.sak.frontend-angre-send-til-beslutter',
     ulikeInntekter = 'familie.ef.sak-ulike-inntekter',
     visVurderHenvendelseOppgaver = 'familie.ef.sak.automatiske-oppgaver-lokalkontor',
     fremleggsoppgave = 'familie.ef.sak.automatiske-oppgaver.fremleggsoppgave',
