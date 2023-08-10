c677f1b3079014f9cef48248829c525c67a798c0
sarahjelle <sara.hjelle@bekk.no>
2023-08-10 12:49:56 +0200

    Skal presentere hver barnepassordning separat

---
+++ src/frontend/Komponenter/Behandling/Aktivitet/DokumentasjonTilsynsutgifter/Barnepassordning.tsx
@@ -0,0 +1,54 @@
+import React, { FC } from 'react';
+import {
+    BarnepassordningDto,
+    typeBarnepassordningTilTekst,
+} from '../../Inngangsvilkår/Aleneomsorg/typer';
+import { formaterNullableIsoDato } from '../../../../App/utils/formatter';
+import { VilkårInfoIkon } from '../../Vilkårpanel/VilkårInformasjonKomponenter';
+import Informasjonsrad from '../../Vilkårpanel/Informasjonsrad';
+import styled from 'styled-components';
+import { Label } from '@navikt/ds-react';
+
+const PassordningWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+`;
+
+const Barnepassordning: FC<{
+    barnepassordning: BarnepassordningDto;
+}> = ({ barnepassordning }) => {
+    return (
+        <PassordningWrapper>
+            <Informasjonsrad
+                ikon={VilkårInfoIkon.SØKNAD}
+                label="Barnepassordning"
+                verdi={
+                    <Label size="small">
+                        {typeBarnepassordningTilTekst[barnepassordning.type]}
+                    </Label>
+                }
+                verdiSomString={false}
+            />
+            <Informasjonsrad
+                label="Navn passordning"
+                verdi={barnepassordning.navn}
+                boldLabel={false}
+            />
+            <Informasjonsrad
+                label="Periode passordning"
+                verdi={`${formaterNullableIsoDato(
+                    barnepassordning.fra
+                )} - ${formaterNullableIsoDato(barnepassordning.til)}`}
+                boldLabel={false}
+            />
+            <Informasjonsrad
+                label="Utgifter"
+                verdi={barnepassordning.beløp + ',-'}
+                boldLabel={false}
+            />
+        </PassordningWrapper>
+    );
+};
+
+export default Barnepassordning;
--- src/frontend/Komponenter/Behandling/Aktivitet/DokumentasjonTilsynsutgifter/TilsynsutgifterBarnInfo.tsx
+++ src/frontend/Komponenter/Behandling/Aktivitet/DokumentasjonTilsynsutgifter/TilsynsutgifterBarnInfo.tsx
@@ -1,37 +1,15 @@
 import React, { FC } from 'react';
-import {
-    IBarnMedSamvær,
-    typeBarnepassordningTilTekst,
-} from '../../Inngangsvilkår/Aleneomsorg/typer';
-import { formaterNullableIsoDato } from '../../../../App/utils/formatter';
+import { IBarnMedSamvær } from '../../Inngangsvilkår/Aleneomsorg/typer';
 import { utledNavnOgAlder } from '../../Inngangsvilkår/utils';
-import { BarneInfoWrapper, VilkårInfoIkon } from '../../Vilkårpanel/VilkårInformasjonKomponenter';
+import { BarneInfoWrapper } from '../../Vilkårpanel/VilkårInformasjonKomponenter';
 import Informasjonsrad from '../../Vilkårpanel/Informasjonsrad';
+import Barnepassordning from './Barnepassordning';
 
 const TilsynsutgifterBarnInfo: FC<{
     gjeldendeBarn: IBarnMedSamvær;
 }> = ({ gjeldendeBarn }) => {
     const { registergrunnlag, barnepass } = gjeldendeBarn;
     const harPassordning = barnepass && barnepass.barnepassordninger;
-    const passordningTittel =
-        harPassordning && barnepass?.barnepassordninger.length > 1
-            ? 'Barnepassordninger'
-            : 'Barnepassordning';
-
-    if (!gjeldendeBarn.barnepass?.skalHaBarnepass) {
-        return (
-            <BarneInfoWrapper
-                navnOgAlderPåBarn={utledNavnOgAlder(
-                    registergrunnlag.navn,
-                    registergrunnlag.fødselsdato,
-                    registergrunnlag.dødsdato
-                )}
-                dødsdato={registergrunnlag.dødsdato}
-            >
-                <Informasjonsrad label="Ingen søknadsopplysninger" />
-            </BarneInfoWrapper>
-        );
-    }
 
     return (
         <BarneInfoWrapper
@@ -42,39 +20,13 @@ const TilsynsutgifterBarnInfo: FC<{
             )}
             dødsdato={registergrunnlag.dødsdato}
         >
-            {harPassordning && (
-                <>
-                    <Informasjonsrad
-                        ikon={VilkårInfoIkon.SØKNAD}
-                        label={passordningTittel}
-                        verdi={barnepass?.barnepassordninger.map((ordning) => {
-                            return typeBarnepassordningTilTekst[ordning.type]; //TODO: Ta hensyn til at barn kan ha flere passordninger
-                        })}
-                    />
-                    <Informasjonsrad
-                        ikon={VilkårInfoIkon.SØKNAD}
-                        label="Navn passordning"
-                        verdi={barnepass?.barnepassordninger.map((ordning) => {
-                            return ordning.navn; //TODO: Ta hensyn til at barn kan ha flere passordninger
-                        })}
-                    />
-                    <Informasjonsrad
-                        ikon={VilkårInfoIkon.SØKNAD}
-                        label="Periode passordning"
-                        verdi={barnepass?.barnepassordninger.map((ordning) => {
-                            return `${formaterNullableIsoDato(
-                                ordning.fra
-                            )} - ${formaterNullableIsoDato(ordning.til)}`; //TODO: Ta hensyn til at barn kan ha flere passordninger
-                        })}
-                    />
-                    <Informasjonsrad
-                        ikon={VilkårInfoIkon.SØKNAD}
-                        label="Utgifter"
-                        verdi={barnepass?.barnepassordninger.map((ordning) => {
-                            return ordning.beløp + ',-'; //TODO: Ta hensyn til at barn kan ha flere passordninger
-                        })}
-                    />
-                </>
+            {!gjeldendeBarn.barnepass?.skalHaBarnepass ? (
+                <Informasjonsrad label="Ingen søknadsopplysninger" />
+            ) : (
+                harPassordning &&
+                barnepass.barnepassordninger.map((ordning) => (
+                    <Barnepassordning barnepassordning={ordning} />
+                ))
             )}
         </BarneInfoWrapper>
     );
