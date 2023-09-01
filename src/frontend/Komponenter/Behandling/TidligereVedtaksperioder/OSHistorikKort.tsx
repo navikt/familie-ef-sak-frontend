c650d83b97303b8a7f5f950c5c0bafde27a5a16b
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2023-09-01 14:35:57 +0200

    ...

--- src/frontend/Komponenter/Behandling/TidligereVedtaksperioder/OSHistorikKort.tsx
+++ src/frontend/Komponenter/Behandling/TidligereVedtaksperioder/OSHistorikKort.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { ITidligereVedtaksperioder } from './typer';
 import styled from 'styled-components';
-import { BodyShort, Heading, Label } from '@navikt/ds-react';
+import { BodyShort, Heading, Label, Tag } from '@navikt/ds-react';
+import { etikettTypeOvergangsstønad } from '../../Personoversikt/HistorikkVedtaksperioder/vedtakshistorikkUtil';
+import { periodetypeTilTekst } from '../../../App/typer/vedtak';
 
 const Container = styled.div`
     display: flex;
@@ -32,30 +34,44 @@ export const OSHistorikKort: React.FC<ITidligereVedtaksperioder> = ({ infotrygd,
     if (!infotrygd && !sak) {
         return null;
     }
-
+    // TODO: Legg til dager i periode
     return (
         <>
-            {sak && ( // TODO: Gjøre endring slik at tabellen ikke vises hvis det mangler data
+            {sak && ( // TODO: Endre tekst som vises hvis det ikke finnes noe data
                 <Container>
                     <Tittel level="3" size="small">
                         Historikk i EF Sak
                     </Tittel>
-                    <Grid>
-                        <Row>
-                            <Label>Periode</Label>
-                            <Label>Periodetype</Label>
-                            <Label>Måneder innvilget</Label>
-                        </Row>
-                        {sak?.periodeHistorikkOvergangsstønad.map((rad, i) => (
-                            <Row key={i}>
-                                <BodyShort>
-                                    {rad.periode.fom} - {rad.periode.tom}
-                                </BodyShort>
-                                <BodyShort>{rad.periodeType}</BodyShort>
-                                <BodyShort>{rad.antMnd}</BodyShort>
+                    {sak?.periodeHistorikkOvergangsstønad.length > 0 ? (
+                        <Grid>
+                            <Row>
+                                <Label>Periode</Label>
+                                <Label>Periodetype</Label>
+                                <Label>Måneder innvilget</Label>
                             </Row>
-                        ))}
-                    </Grid>
+                            {sak?.periodeHistorikkOvergangsstønad.map((rad, i) => (
+                                <Row key={i}>
+                                    <BodyShort size="small">
+                                        {rad.periode.fom === rad.periode.tom
+                                            ? `${rad.periode.fom}`
+                                            : `${rad.periode.fom} - ${rad.periode.tom}`}
+                                    </BodyShort>
+
+                                    <div>
+                                        <Tag
+                                            variant={etikettTypeOvergangsstønad(rad.periodeType)}
+                                            size={'small'}
+                                        >
+                                            {periodetypeTilTekst[rad.periodeType || '']}
+                                        </Tag>
+                                    </div>
+                                    <BodyShort size="small">{rad.antMnd}</BodyShort>
+                                </Row>
+                            ))}
+                        </Grid>
+                    ) : (
+                        <BodyShort size="small">Kan ikke vise tidligere hostorikk.</BodyShort>
+                    )}
                 </Container>
             )}
 
@@ -64,9 +80,7 @@ export const OSHistorikKort: React.FC<ITidligereVedtaksperioder> = ({ infotrygd,
                     <Tittel level="3" size="small">
                         Historikk i Infotrygd
                     </Tittel>
-                    <BodyShort>
-                        Bruker har historikk i Infotrygd som må sjekkes manuelt bla bla
-                    </BodyShort>
+                    <BodyShort>Bruker har historikk i Infotrygd som må sjekkes manuelt.</BodyShort>
                 </Container>
             )}
         </>
