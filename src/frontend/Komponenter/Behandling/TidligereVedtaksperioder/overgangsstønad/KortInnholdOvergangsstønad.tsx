e3c483d7e680e4745789605423502b48cca54db5
Trondl <56258085+throndi@users.noreply.github.com>
2024-01-25 11:58:55 +0100

    Historikk, responsiv fiks (#2692)
    * Fancy template columns minmax max-content hack for å stacke historiske vedtaksperioder

--- src/frontend/Komponenter/Behandling/TidligereVedtaksperioder/overgangsstønad/KortInnholdOvergangsstønad.tsx
+++ src/frontend/Komponenter/Behandling/TidligereVedtaksperioder/overgangsstønad/KortInnholdOvergangsstønad.tsx
@@ -6,10 +6,11 @@ import HistorikkRadIOvergangsstønad from './HistorikkRadIOvergangsstønad';
 
 const Grid = styled.div`
     display: grid;
-    grid-template-columns: repeat(4, max-content);
+    grid-template-columns: repeat(4, minmax(auto, max-content));
     row-gap: 0.2rem;
     column-gap: 1rem;
 `;
+
 const Row = styled.div`
     display: contents;
 `;
