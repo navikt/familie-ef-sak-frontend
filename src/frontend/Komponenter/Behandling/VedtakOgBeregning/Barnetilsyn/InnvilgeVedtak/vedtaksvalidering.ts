200147762235695686d5ad44e20a950d5bd4a74b
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2024-03-12 13:26:09 +0100

    Laget en variabel med feilmelding som skal vises når fra og med dato er før til og med dato, da feilmeldingen brukes flere steder. Endret teksten slik at den blir mer forklarende.

--- src/frontend/Komponenter/Behandling/VedtakOgBeregning/Barnetilsyn/InnvilgeVedtak/vedtaksvalidering.ts
+++ src/frontend/Komponenter/Behandling/VedtakOgBeregning/Barnetilsyn/InnvilgeVedtak/vedtaksvalidering.ts
@@ -12,7 +12,11 @@ import {
     erPåfølgendeÅrMåned,
 } from '../../../../../App/utils/dato';
 import { erOpphørEllerSanksjon } from '../Felles/utils';
-import { ugyldigEtterfølgendePeriodeFeilmelding, validerGyldigTallverdi } from '../../Felles/utils';
+import {
+    fraPeriodeErEtterTilPeriode,
+    ugyldigEtterfølgendePeriodeFeilmelding,
+    validerGyldigTallverdi,
+} from '../../Felles/utils';
 
 export const validerInnvilgetVedtakForm = ({
     utgiftsperioder,
@@ -131,7 +135,7 @@ export const validerUtgiftsperioder = ({
         if (!erMånedÅrEtterEllerLik(årMånedFra, årMånedTil)) {
             return {
                 ...utgiftsperiodeFeil,
-                årMånedFra: `Ugyldig periode - fra (${årMånedFra}) må være før til (${årMånedTil})`,
+                årMånedFra: fraPeriodeErEtterTilPeriode,
             };
         }
 
@@ -211,7 +215,7 @@ const validerKontantstøttePerioder = (
         if (!erMånedÅrEtterEllerLik(årMånedFra, årMånedTil)) {
             return {
                 ...kontantstøtteperiodeFeil,
-                årMånedFra: `Ugyldig periode - fra (${årMånedFra}) må være før til (${årMånedTil})`,
+                årMånedFra: fraPeriodeErEtterTilPeriode,
             };
         }
         const forrige = index > 0 && kontantstøtteperioder[index - 1];
@@ -272,7 +276,7 @@ const validerTilleggsstønadPerioder = (
         if (!erMånedÅrEtterEllerLik(årMånedFra, årMånedTil)) {
             return {
                 ...tilleggsstønadPeriodeFeil,
-                årMånedFra: `Ugyldig periode - fra (${årMånedFra}) må være før til (${årMånedTil})`,
+                årMånedFra: fraPeriodeErEtterTilPeriode,
             };
         }
         const forrige = index > 0 && tilleggsstønadsperioder[index - 1];
