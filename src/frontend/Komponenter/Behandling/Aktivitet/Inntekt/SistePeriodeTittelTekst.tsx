6ca3de7f0593635af6ef955229215854b1141c28
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2024-05-28 15:57:17 +0200

    Styled SistePeriodeTittelTekst slik at det er mer likt Figma-skissen.

--- src/frontend/Komponenter/Behandling/Aktivitet/Inntekt/SistePeriodeTittelTekst.tsx
+++ src/frontend/Komponenter/Behandling/Aktivitet/Inntekt/SistePeriodeTittelTekst.tsx
@@ -1,22 +1,29 @@
-import { BodyShort } from '@navikt/ds-react';
+import { BodyShort, Label } from '@navikt/ds-react';
 import React from 'react';
+import { styled } from 'styled-components';
 
 interface SistePeriodeTittelTekstProps {
     tekst?: string;
     tittel?: string;
 }
 
+const Container = styled.div`
+    & > *:not(:last-child) {
+        margin-bottom: 0.75rem;
+    }
+`;
+
 const SistePeriodeTittelTekst: React.FC<SistePeriodeTittelTekstProps> = ({
     tekst = 'Bruker har ingen stønadshistorikk i EF Sak',
     tittel = 'Siste periode med overgangsstønad',
 }) => {
     return (
-        <>
-            <BodyShort size="medium" weight="semibold">
+        <Container>
+            <Label size="small" className="tittel" as="h3">
                 {tittel}
-            </BodyShort>
-            <BodyShort>{tekst}</BodyShort>
-        </>
+            </Label>
+            <BodyShort size="small">{tekst}</BodyShort>
+        </Container>
     );
 };
 
