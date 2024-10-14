54462da902b3667cc51f9f2a631a6999192ba2f1
Viktor Solberg <viktorgsolberg@gmail.com>
2024-10-14 14:53:07 +0200

    wip

--- src/frontend/App/hooks/useHentAnsvarligSaksbehandler.ts
+++ src/frontend/App/hooks/useHentAnsvarligSaksbehandler.ts
@@ -1,20 +1,46 @@
 import { useApp } from '../context/AppContext';
 import { useCallback, useState } from 'react';
-import { byggHenterRessurs, byggTomRessurs, Ressurs } from '../typer/ressurs';
-import { AnsvarligSaksbehandler } from '../typer/saksbehandler';
+import {
+    byggHenterRessurs,
+    byggSuksessRessurs,
+    byggTomRessurs,
+    Ressurs,
+    RessursStatus,
+} from '../typer/ressurs';
+import { AnsvarligSaksbehandler, AnsvarligSaksbehandlerRolle } from '../typer/saksbehandler';
 
 export const useHentAnsvarligSaksbehandler = (behandlingId: string) => {
-    const { axiosRequest } = useApp();
+    const { axiosRequest, innloggetSaksbehandler } = useApp();
     const [ansvarligSaksbehandler, settAnsvarligSaksbehandler] =
         useState<Ressurs<AnsvarligSaksbehandler>>(byggTomRessurs());
+
+    const utledAnsvarligSaksbehandler = useCallback(
+        (response: Ressurs<AnsvarligSaksbehandler>) => {
+            if (
+                response.status === RessursStatus.SUKSESS &&
+                response.data.rolle === AnsvarligSaksbehandlerRolle.OPPGAVE_FINNES_IKKE
+            ) {
+                settAnsvarligSaksbehandler(
+                    byggSuksessRessurs<AnsvarligSaksbehandler>({
+                        etternavn: innloggetSaksbehandler.firstName,
+                        fornavn: innloggetSaksbehandler.lastName,
+                        rolle: AnsvarligSaksbehandlerRolle.INNLOGGET_SAKSBEHANDLER,
+                    })
+                );
+            } else {
+                settAnsvarligSaksbehandler(response);
+            }
+        },
+        [innloggetSaksbehandler.firstName, innloggetSaksbehandler.lastName]
+    );
 
     const hentAnsvarligSaksbehandlerCallback = useCallback(() => {
         settAnsvarligSaksbehandler(byggHenterRessurs());
         axiosRequest<AnsvarligSaksbehandler, string>({
             method: 'GET',
             url: `/familie-ef-sak/api/oppgave/${behandlingId}/ansvarlig-saksbehandler`,
-        }).then((res: Ressurs<AnsvarligSaksbehandler>) => settAnsvarligSaksbehandler(res));
-    }, [axiosRequest, behandlingId]);
+        }).then((res: Ressurs<AnsvarligSaksbehandler>) => utledAnsvarligSaksbehandler(res));
+    }, [axiosRequest, behandlingId, utledAnsvarligSaksbehandler]);
 
     return {
         ansvarligSaksbehandler,
