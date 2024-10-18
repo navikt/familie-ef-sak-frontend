bc21587b8b524313bd7f1ec6c3d5e4a321bd6477
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2024-10-18 10:16:35 +0200

    Alternativ versjon av kortNedBeskrivelse som bruker bruker input som konstant.

--- src/frontend/Komponenter/Oppgavebenk/kortNedBeskrivelse.ts
+++ src/frontend/Komponenter/Oppgavebenk/kortNedBeskrivelse.ts
@@ -3,17 +3,16 @@ export const kortNedBeskrivelse = (beskrivelse?: string) => {
         return '';
     }
 
-    if (beskrivelse.startsWith('---')) {
-        const sluttIndeks = beskrivelse.indexOf('---', 3) + 3;
-        beskrivelse = beskrivelse.slice(sluttIndeks).trim();
-    }
+    const beskrivelseUtenStartMetadata = beskrivelse.startsWith('---')
+        ? beskrivelse.slice(beskrivelse.indexOf('---', 3) + 3).trim()
+        : beskrivelse;
 
-    const klippBortIndeks = beskrivelse.indexOf('---');
+    const klippBortIndeks = beskrivelseUtenStartMetadata.indexOf('---');
     if (klippBortIndeks !== -1 && klippBortIndeks <= 75) {
-        return beskrivelse.slice(0, klippBortIndeks).trim();
-    } else if (beskrivelse.length > 75) {
-        return beskrivelse.slice(0, 75).trim() + '...';
+        return beskrivelseUtenStartMetadata.slice(0, klippBortIndeks).trim();
+    } else if (beskrivelseUtenStartMetadata.length > 75) {
+        return beskrivelseUtenStartMetadata.slice(0, 75).trim() + '...';
     }
 
-    return beskrivelse;
+    return beskrivelseUtenStartMetadata;
 };
