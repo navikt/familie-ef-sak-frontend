68547e173a44a61e4be35c4e09b983bf12d339e6
EndreHM <endre.heien.mo@nav.no>
2024-12-09 17:33:21 +0100

    Lagt til funskjon for å sjekke om henleggelsesbrev skal sendes. Endret fra checkbox til radio for avhukning om brev skal sendes. Lagt til divider og stacks for å bedre skille mellom elementer

--- src/frontend/Komponenter/Behandling/Modal/HenleggModal.tsx
+++ src/frontend/Komponenter/Behandling/Modal/HenleggModal.tsx
@@ -6,7 +6,7 @@ import { useApp } from '../../../App/context/AppContext';
 import { EToast } from '../../../App/typer/toast';
 import { EHenlagtårsak } from '../../../App/typer/behandlingsårsak';
 import { ModalWrapper } from '../../../Felles/Modal/ModalWrapper';
-import { Alert, Radio, RadioGroup } from '@navikt/ds-react';
+import { Alert, Link, Radio, RadioGroup } from '@navikt/ds-react';
 import styled from 'styled-components';
 import { useRedirectEtterLagring } from '../../../App/hooks/felles/useRedirectEtterLagring';
 import { v4 as uuidv4 } from 'uuid';
@@ -14,17 +14,16 @@ import { useToggles } from '../../../App/context/TogglesContext';
 import DataViewer from '../../../Felles/DataViewer/DataViewer';
 import { AnsvarligSaksbehandlerRolle } from '../../../App/typer/saksbehandler';
 import { ToggleName } from '../../../App/context/toggles';
-import { Checkbox, CheckboxGroup } from '@navikt/ds-react';
-import { Link } from '@navikt/ds-react';
 import { base64toBlob, åpnePdfIEgenTab } from '../../../App/utils/utils';
+import { ABorderSubtle } from '@navikt/ds-tokens/dist/tokens';
+import { VStack, Stack } from '@navikt/ds-react';
 
 const AlertStripe = styled(Alert)`
     margin-top: 1rem;
 `;
 
-const TillegsValg = styled.div`
-    display: flex;
-    flex-direction: column;
+const HorizontalDivider = styled.div`
+    border-bottom: 2px solid ${ABorderSubtle};
 `;
 
 export const HenleggModal: FC<{ behandling: Behandling }> = ({ behandling }) => {
@@ -43,7 +42,7 @@ export const HenleggModal: FC<{ behandling: Behandling }> = ({ behandling }) => 
         settIkkePersistertKomponent,
     } = useApp();
     const [henlagtårsak, settHenlagtårsak] = useState<EHenlagtårsak>();
-    const [sendHenleggelsesbrev, settSendHenleggelsesbrev] = useState(['Send henleggelsesbrev']);
+    const [huketAvSendBrev, settHuketAvSendBrev] = useState<boolean>(true);
     const [låsKnapp, settLåsKnapp] = useState<boolean>(false);
     const [feilmelding, settFeilmelding] = useState<string>();
     const [forhåndsvisningsFeil, settForhåndsvisningsFeil] = useState<string>();
@@ -84,11 +83,12 @@ export const HenleggModal: FC<{ behandling: Behandling }> = ({ behandling }) => 
 
         const endepunkt = utledEndepunktForHenleggelse(ansvarligSaksbehandlerRolle);
 
-        axiosRequest<string, { årsak: EHenlagtårsak }>({
+        axiosRequest<string, { årsak: EHenlagtårsak; skalSendeHenleggelsesbrev: boolean }>({
             method: 'POST',
             url: endepunkt,
             data: {
                 årsak: henlagtårsak,
+                skalSendeHenleggelsesbrev: skalSendeHenleggelsesBrev,
             },
         })
             .then((respons: Ressurs<string>) => {
@@ -115,6 +115,9 @@ export const HenleggModal: FC<{ behandling: Behandling }> = ({ behandling }) => 
         settHenlagtårsak(undefined);
     };
 
+    const skalSendeHenleggelsesBrev =
+        huketAvSendBrev && henlagtårsak === EHenlagtårsak.TRUKKET_TILBAKE;
+
     return (
         <DataViewer response={{ ansvarligSaksbehandler }}>
             {({ ansvarligSaksbehandler }) => {
@@ -133,32 +136,52 @@ export const HenleggModal: FC<{ behandling: Behandling }> = ({ behandling }) => 
                         }}
                         ariaLabel={'Velg årsak til henleggelse av behandlingen'}
                     >
-                        <RadioGroup
-                            legend={''}
-                            onChange={(årsak: EHenlagtårsak) => settHenlagtårsak(årsak)}
-                        >
-                            <Radio value={EHenlagtårsak.TRUKKET_TILBAKE}>Trukket tilbake</Radio>
-                            <Radio value={EHenlagtårsak.FEILREGISTRERT}>Feilregistrert</Radio>
-                        </RadioGroup>
-                        {henlagtårsak === EHenlagtårsak.TRUKKET_TILBAKE && (
-                            <TillegsValg>
-                                <CheckboxGroup
-                                    legend="Send henleggelsesbrev"
-                                    hideLegend
-                                    onChange={settSendHenleggelsesbrev}
-                                    value={sendHenleggelsesbrev}
-                                >
-                                    <Checkbox value="Send henleggelsesbrev">
-                                        Send henleggelsesbrev
-                                    </Checkbox>
-                                </CheckboxGroup>
-                                <Link onClick={visBrevINyFane}>Forhåndsvis brev</Link>
-                            </TillegsValg>
-                        )}
-                        {feilmelding && <AlertStripe variant={'error'}>{feilmelding}</AlertStripe>}
-                        {forhåndsvisningsFeil && (
-                            <AlertStripe variant={'error'}>{forhåndsvisningsFeil}</AlertStripe>
-                        )}
+                        <VStack gap="4">
+                            <RadioGroup
+                                legend={''}
+                                onChange={(årsak: EHenlagtårsak) => settHenlagtårsak(årsak)}
+                            >
+                                <Radio value={EHenlagtårsak.TRUKKET_TILBAKE}>Trukket tilbake</Radio>
+                                <Radio value={EHenlagtårsak.FEILREGISTRERT}>Feilregistrert</Radio>
+                            </RadioGroup>
+                            {henlagtårsak === EHenlagtårsak.TRUKKET_TILBAKE && (
+                                <>
+                                    <HorizontalDivider />
+                                    <RadioGroup
+                                        legend="Send henleggelsesbrev"
+                                        value={huketAvSendBrev}
+                                    >
+                                        <Stack
+                                            gap="0 6"
+                                            direction={{ xs: 'column', sm: 'row' }}
+                                            wrap={false}
+                                        >
+                                            <Radio
+                                                value={true}
+                                                onChange={() => {
+                                                    settHuketAvSendBrev(true);
+                                                }}
+                                            >
+                                                Ja
+                                            </Radio>
+                                            <Radio
+                                                value={false}
+                                                onChange={() => settHuketAvSendBrev(false)}
+                                            >
+                                                Nei
+                                            </Radio>
+                                        </Stack>
+                                    </RadioGroup>
+                                    <Link onClick={visBrevINyFane}>Forhåndsvis brev</Link>
+                                </>
+                            )}
+                            {feilmelding && (
+                                <AlertStripe variant={'error'}>{feilmelding}</AlertStripe>
+                            )}
+                            {forhåndsvisningsFeil && (
+                                <AlertStripe variant={'error'}>{forhåndsvisningsFeil}</AlertStripe>
+                            )}
+                        </VStack>
                     </ModalWrapper>
                 );
             }}
