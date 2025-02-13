66058bad67ceba9ea4d5d74b91621fcd1407742c
Viktor Solberg <viktorgsolberg@gmail.com>
2025-02-13 14:10:25 +0100

    skal fjerne egen side for visning av samværskalkulator

--- src/frontend/Komponenter/Verktøy/SamværkalkulatorSide.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { BodyShort } from '@navikt/ds-react';
-
-export const SamværkalkulatorSide: React.FC = () => {
-    return <BodyShort>Under arbeid</BodyShort>;
-};
