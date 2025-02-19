4ae9889c407f285553f6485cc31a264aeae9d464
Kevin Sillerud <kevin.sillerud@gmail.com>
2025-02-19 13:44:18 +0100

    Bytt ut tilbakekreving URL for prod

--- src/frontend/App/utils/miljø.ts
+++ src/frontend/App/utils/miljø.ts
@@ -1,9 +1,7 @@
 export const erProd = (): boolean => window.location.host === 'ensligmorellerfar.intern.nav.no';
 
 export const tilbakekrevingBaseUrl = (): string =>
-    erProd()
-        ? 'https://familietilbakekreving.intern.nav.no'
-        : 'https://tilbakekreving.ansatt.dev.nav.no';
+    erProd() ? 'https://tilbakekreving.intern.nav.no' : 'https://tilbakekreving.ansatt.dev.nav.no';
 
 export const klageBaseUrl = (): string =>
     erProd() ? 'https://familie-klage.intern.nav.no' : 'https://familie-klage.ansatt.dev.nav.no';
