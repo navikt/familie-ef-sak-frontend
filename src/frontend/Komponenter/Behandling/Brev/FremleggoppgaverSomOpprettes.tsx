77439abf1637a8a1f0070fff03aa24144b5120ab
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2025-02-25 07:52:44 +0100

    Det holder å sjekke lengden på oppgavetyperSomSkalOpprettes.

--- src/frontend/Komponenter/Behandling/Brev/FremleggoppgaverSomOpprettes.tsx
+++ src/frontend/Komponenter/Behandling/Brev/FremleggoppgaverSomOpprettes.tsx
@@ -15,7 +15,7 @@ const Container = styled.div`
 export const FremleggoppgaverSomOpprettes: FC<{
     oppgavetyperSomSkalOpprettes: OppgaveTypeForOpprettelse[];
 }> = ({ oppgavetyperSomSkalOpprettes }) => {
-    if (!oppgavetyperSomSkalOpprettes || oppgavetyperSomSkalOpprettes.length === 0) {
+    if (oppgavetyperSomSkalOpprettes.length === 0) {
         return;
     }
 
