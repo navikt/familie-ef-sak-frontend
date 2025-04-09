50e360de0c218981948a78ff52e801f3f9de9be2
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2025-04-09 13:29:25 +0200

    Tittel skal starte med stor bokstav

--- src/frontend/Komponenter/Personoversikt/Vedtaksperioder/TittelOgValg.tsx
+++ src/frontend/Komponenter/Personoversikt/Vedtaksperioder/TittelOgValg.tsx
@@ -22,7 +22,9 @@ const TittelOgValg: FC<{
 }) => {
     return (
         <HStack gap="3" justify="space-between" align="center">
-            <Heading size="medium">{utledTittel(fagsak)}</Heading>
+            <Heading size="medium">
+                {utledTittel(fagsak).charAt(0).toUpperCase() + utledTittel(fagsak).slice(1)}
+            </Heading>
             <BehandlingVelger
                 valgtFagsak={valgtFagsak}
                 behandlinger={behandlinger}
