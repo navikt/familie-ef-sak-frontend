a01521423ee80610a0f4981dfd4495a5aa7fe047
Viktor Solberg <viktorgsolberg@gmail.com>
2025-04-22 15:08:26 +0200

    skal kunne kjøre opp og vise brev lokalt med denne konfigurasjonen

--- src/frontend/App/hooks/useHentBrevStruktur.ts
+++ src/frontend/App/hooks/useHentBrevStruktur.ts
@@ -1,25 +1,47 @@
 import { useEffect, useState } from 'react';
-import { BrevStruktur, datasett } from '../../Komponenter/Behandling/Brev/BrevTyper';
-import { byggTomRessurs, Ressurs } from '../typer/ressurs';
-import { useApp } from '../context/AppContext';
+import {
+    AlleFlettefelter,
+    BrevStruktur,
+    DokumentMal,
+} from '../../Komponenter/Behandling/Brev/BrevTyper';
+import { byggTomRessurs, Ressurs, RessursStatus, RessursSuksess } from '../typer/ressurs';
 
 export const useHentBrevStruktur = (
     brevMal: string | undefined
 ): { brevStruktur: Ressurs<BrevStruktur> } => {
     const [brevStruktur, settBrevStruktur] = useState<Ressurs<BrevStruktur>>(byggTomRessurs());
-    const { axiosRequest } = useApp();
+    // const { axiosRequest } = useApp();
 
-    const url = `/familie-brev/api/${datasett}/avansert-dokument/bokmaal/${brevMal}/felter/v3`;
+    // const url = `/familie-brev/api/${datasett}/avansert-dokument/bokmaal/${brevMal}/felter/v3`;
+
+    const dummyAlleFlettefelter: AlleFlettefelter = {
+        flettefeltReferanse: [],
+    };
+
+    const dummyDokumentMal: DokumentMal = {
+        brevmenyBlokker: [],
+    };
+
+    const dummyBrevstruktur: BrevStruktur = {
+        dokument: dummyDokumentMal,
+        flettefelter: dummyAlleFlettefelter,
+    };
+
+    const dummyRessursMedBrevstruktur: RessursSuksess<BrevStruktur> = {
+        data: dummyBrevstruktur,
+        status: RessursStatus.SUKSESS,
+    };
 
     useEffect(() => {
-        if (brevMal) {
-            axiosRequest<BrevStruktur, null>({
-                method: 'GET',
-                url: url,
-            }).then((respons: Ressurs<BrevStruktur>) => {
-                settBrevStruktur(respons);
-            });
-        }
+        // if (brevMal) {
+        //     axiosRequest<BrevStruktur, null>({
+        //         method: 'GET',
+        //         url: url,
+        //     }).then((respons: Ressurs<BrevStruktur>) => {
+        //         settBrevStruktur(respons);
+        //     });
+        // }
+        settBrevStruktur(dummyRessursMedBrevstruktur);
         // eslint-disable-next-line
     }, [brevMal]);
 
