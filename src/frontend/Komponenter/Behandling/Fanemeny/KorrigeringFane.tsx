286dc2a00b7dd223a3adf5460d33b170ab1929af
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2025-05-07 10:15:00 +0200

    Fjerner ubrukt kode fra KorrigeringFane - ikke nødvendig med vedtak

--- src/frontend/Komponenter/Behandling/Fanemeny/KorrigeringFane.tsx
+++ src/frontend/Komponenter/Behandling/Fanemeny/KorrigeringFane.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import SendTilBeslutter from '../Totrinnskontroll/SendTilBeslutter';
 import { useBehandling } from '../../../App/context/BehandlingContext';
 import { AlertInfo } from '../../../Felles/Visningskomponenter/Alerts';
-import { utledAvslagValg } from '../VedtakOgBeregning/Felles/utils';
-import DataViewer from '../../../Felles/DataViewer/DataViewer';
 import { Behandlingsårsak } from '../../../App/typer/behandlingsårsak';
 import { Behandling } from '../../../App/typer/fagsak';
 
@@ -12,25 +10,20 @@ interface Props {
 }
 
 export const KorrigeringFane: React.FC<Props> = ({ behandling }) => {
-    const { behandlingErRedigerbar, vedtak } = useBehandling();
+    const { behandlingErRedigerbar } = useBehandling();
 
     return (
-        <DataViewer response={{ vedtak }}>
-            {({ vedtak }) => (
-                <>
-                    <AlertInfo>
-                        {behandling.behandlingsårsak === Behandlingsårsak.IVERKSETTE_KA_VEDTAK
-                            ? 'Iverksette KA-vedtak (uten brev)'
-                            : 'Korrigering av vedtak uten brevutsendelse'}
-                    </AlertInfo>
-                    <SendTilBeslutter
-                        behandling={behandling}
-                        kanSendesTilBeslutter={behandlingErRedigerbar}
-                        behandlingErRedigerbar={behandlingErRedigerbar}
-                        avslagValg={utledAvslagValg(vedtak)}
-                    />
-                </>
-            )}
-        </DataViewer>
+        <>
+            <AlertInfo>
+                {behandling.behandlingsårsak === Behandlingsårsak.IVERKSETTE_KA_VEDTAK
+                    ? 'Iverksette KA-vedtak (uten brev)'
+                    : 'Korrigering av vedtak uten brevutsendelse'}
+            </AlertInfo>
+            <SendTilBeslutter
+                behandling={behandling}
+                kanSendesTilBeslutter={behandlingErRedigerbar}
+                behandlingErRedigerbar={behandlingErRedigerbar}
+            />
+        </>
     );
 };
