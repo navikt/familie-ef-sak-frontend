b87acdd22c66520672a0fe15777c1f8b1e297a60
EndreHM <endre.heien.mo@nav.no>
2025-05-12 12:44:19 +0200

    Fjernet utgått hjelpetekst fra rimgelig grunn hjelpetekst

--- src/frontend/Komponenter/Behandling/Aktivitet/SagtOppEllerRedusert/RimeligGrunnHjelpetekst.tsx
+++ src/frontend/Komponenter/Behandling/Aktivitet/SagtOppEllerRedusert/RimeligGrunnHjelpetekst.tsx
@@ -9,19 +9,6 @@ export const RimeligGrunnHjelpetekst: React.FC = () => {
                 første ledd. Påfølgende søknader om nye perioder for nye barn skal også regnes som
                 førstegangssøknader.
             </BodyLong>
-            <br />
-            <BodyLong>
-                Arbeid som er til hinder for omsorgen for barn, som for eksempel overveiende
-                nattarbeid, arbeid som forutsetter lange fraværsperioder eller lang reisevei, kan
-                anses som rimelig grunn til å avslutte et arbeidsforhold. Det samme gjelder dersom
-                arbeidsforholdet er lønnet under tariff eller sedvane, eller dersom omfanget av
-                arbeidet var så lite at det i svært liten grad bidro til selvforsørgelse.
-            </BodyLong>
-            <br />
-            <BodyLong>
-                Søker bør fortsatt ha rett til å avslutte et arbeidsforhold for å gå over i et nytt,
-                eller redusere arbeidstiden, jf. krav til omfanget av aktivitetsplikten.
-            </BodyLong>
         </>
     );
 };
