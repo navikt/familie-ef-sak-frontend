771cb727c584cf2568bdb02c725f376f8a9c4c0b
krikof-nav <kristian.kofoed@nav.no>
2025-05-12 09:45:40 +0200

    Lint

--- start-lokal.sh
+++ start-lokal.sh
@@ -3,7 +3,6 @@
 # Henter secrets og setter .env filen. Oasis og Wonderwall trenger disse for lokalkjøring.
 ./hent-og-lagre-miljøvariabler.sh
 
-
 # Starter Docker container med Wonderwall config - se wonderwall-docker-compose-lokal.yml
 docker-compose -f wonderwall-docker-compose-lokal.yml up -d
 
