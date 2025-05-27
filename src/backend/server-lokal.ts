a2a771003c1c7903057a82ce7480061772f2bc1b
gunnsteingarmo <141132903+gunnsteingarmo@users.noreply.github.com>
2025-05-27 08:31:34 +0200

    Legger til observability. Endrer struktur på nais filer. (#3103)

--- src/backend/server-lokal.ts
+++ src/backend/server-lokal.ts
@@ -9,7 +9,7 @@ import { buildPath, sessionConfig } from './config.js';
 import { prometheusTellere } from './metrikker.js';
 import { logInfo } from '@navikt/familie-logging';
 // @ts-expect-error Spesial-import
-import config from '../../build_n_deploy/webpack/webpack.dev.js';
+import config from '../../webpack/webpack.dev.js';
 import { setupServerFelles } from './server-felles';
 
 backend(sessionConfig, prometheusTellere).then((appConfig: IApp) => {
