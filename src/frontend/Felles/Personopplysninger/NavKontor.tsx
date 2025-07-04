17e4a3d09b75665a82b62e8fd261cd891b81f5c4
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2025-07-04 09:52:38 +0200

    Ønsker at nav-kontor skal være øverst til høyre slik at var tidligere.

--- src/frontend/Felles/Personopplysninger/NavKontor.tsx
+++ src/frontend/Felles/Personopplysninger/NavKontor.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { INavKontor } from '../../App/typer/personopplysninger';
-import { Detail } from '@navikt/ds-react';
+import { Detail, HStack } from '@navikt/ds-react';
 import { Buildings3Icon } from '@navikt/aksel-icons';
-import PersonopplysningerPanel from './PersonopplysningPanel';
-
-const OfficeIkon: React.FC = () => <Buildings3Icon width={24} height={24} />;
 
 const NavKontor: React.FC<{ navKontor?: INavKontor }> = ({ navKontor }) => {
     return (
-        <PersonopplysningerPanel Ikon={OfficeIkon} tittel="NAV-kontor">
-            <Detail>{navKontor ? `${navKontor.enhetNr} - ${navKontor.navn}` : 'Ingen data'}</Detail>
-        </PersonopplysningerPanel>
+        <HStack className="nav-kontor-container" gap="space-4" justify="end">
+            <HStack align="center">
+                <Buildings3Icon width={24} height={24} />
+                <Detail>
+                    {navKontor ? `${navKontor.enhetNr} - ${navKontor.navn}` : 'Ingen data'}
+                </Detail>
+            </HStack>
+        </HStack>
     );
 };
 
