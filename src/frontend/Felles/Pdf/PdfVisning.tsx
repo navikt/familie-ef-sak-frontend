e4cce6dea62cbbcf3e0d438182f6168578f1bd31
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2025-07-29 14:49:34 +0200

    Må endre på import av css-filer siden mappestruktur er endret i den nye versjonen.

--- src/frontend/Felles/Pdf/PdfVisning.tsx
+++ src/frontend/Felles/Pdf/PdfVisning.tsx
@@ -5,8 +5,8 @@ import { Document, Page, pdfjs } from 'react-pdf';
 import DataViewer from '../DataViewer/DataViewer';
 import { AlertError } from '../Visningskomponenter/Alerts';
 import { Loader, Pagination } from '@navikt/ds-react';
-import 'react-pdf/dist/esm/Page/TextLayer.css';
-import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/src/Page/TextLayer.css';
+import 'react-pdf/src/src/Page/AnnotationLayer.css';
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
     'pdfjs-dist/build/pdf.worker.min.mjs',
