33af652f002f63ff874139472bdeab263c3a3af9
RobinLundSadun <robin.lund.sadun@gmail.com>
2025-08-19 12:42:37 +0200

    Legger tilbake tom label

--- src/frontend/Komponenter/Behandling/VedtakOgBeregning/Barnetilsyn/InnvilgeVedtak/UtgiftsperiodeValg.tsx
+++ src/frontend/Komponenter/Behandling/VedtakOgBeregning/Barnetilsyn/InnvilgeVedtak/UtgiftsperiodeValg.tsx
@@ -197,6 +197,7 @@ const UtgiftsperiodeValg: React.FC<Props> = ({
                     <Label>Aktivitet</Label>
                     <Label>Periode fra og med</Label>
                     <Label>Periode til og med</Label>
+                    <Label> </Label>
                     <Label>Velg barn</Label>
                     <Label>Ant.</Label>
                     <Label>Utgifter</Label>
