e6a21f8cf57dba57da21f43f6b811ba94e963862
ma10s <mattis.janitz@nav.no>
2025-08-28 15:51:16 +0200

    Vil fjerne styled components (åpne og lukkeknapper)

--- src/frontend/Komponenter/Behandling/Aktivitet/AktivitetsvilkårHeader.tsx
+++ src/frontend/Komponenter/Behandling/Aktivitet/AktivitetsvilkårHeader.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import styled from 'styled-components';
 import {
     EVilkårstyper,
     useEkspanderbareVilkårpanelContext,
 } from '../../../App/context/EkspanderbareVilkårpanelContext';
 import { ÅpneOgLukkePanelKnapper } from '../Inngangsvilkår/ÅpneOgLukkePanelKnapper';
-
-const Container = styled.div`
-    margin: 2rem;
-    display: flex;
-    align-items: flex-end;
-`;
+import { HStack } from '@navikt/ds-react';
 
 export const AktivitetsvilkårHeader: React.FC = () => {
     const { lukkAlle, åpneAlle } = useEkspanderbareVilkårpanelContext();
 
     return (
-        <Container>
+        <HStack justify="end" style={{ margin: '2rem' }}>
             <ÅpneOgLukkePanelKnapper
                 lukkAlle={() => lukkAlle(EVilkårstyper.AKTIVITETSVILKÅR)}
                 åpneAlle={() => åpneAlle(EVilkårstyper.AKTIVITETSVILKÅR)}
             />
-        </Container>
+        </HStack>
     );
 };
