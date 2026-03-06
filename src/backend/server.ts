28874250194d7671bd12437ce48e37c4b0a8a6d8
gunnsteingarmo <141132903+gunnsteingarmo@users.noreply.github.com>
2026-03-06 13:41:14 +0100

    Flytter over deler av koden fra familie-logging - beholder det vi bruker. Fjerner unødvendig logging i server-lokal. Kan legges tilbake hvis nødvendig for feilsøking. Fjerner avhengigheten til familie-logging (#3333)

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -3,16 +3,12 @@ import './konfigurerApp.js';
 import backend, { IApp } from '@navikt/familie-backend';
 import path from 'path';
 
-import { buildPath, sessionConfig } from './config.js';
+import { sessionConfig } from './config.js';
 import { prometheusTellere } from './metrikker.js';
 import expressStaticGzip from 'express-static-gzip';
-import { logInfo } from '@navikt/familie-logging';
 import { setupServerFelles } from './server-felles';
 
 backend(sessionConfig, prometheusTellere).then((appConfig: IApp) => {
-    logInfo(`Starter opp med miljø: ${process.env.NODE_ENV}`);
-    logInfo(`Starter opp med buildpath: ${buildPath}`);
-
     if (process.env.NODE_ENV === 'development') {
         throw Error('Kan ikke starte produksjonsserver i development-miljø');
     } else {
