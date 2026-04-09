6a3690f03c908c1bd126e2eba610f1d62f6aad33
gunnsteingarmo <gunnstein.hoj.garmo@nav.no>
2026-04-09 12:08:14 +0200

    Fjerner miljøvariabler som ikke lenger er i bruk.

--- hent-og-lagre-miljøvariabler.sh
+++ hent-og-lagre-miljøvariabler.sh
@@ -10,15 +10,6 @@ EF_SAK_FRONTEND_LOKAL_SECRETS=$(get_secrets azuread-familie-ef-sak-frontend-loka
 EF_SAK_FRONTEND_CLIENT_ID=$(echo "$EF_SAK_FRONTEND_LOKAL_SECRETS" | jq -r '.AZURE_APP_CLIENT_ID')
 EF_SAK_FRONTEND_CLIENT_SECRET=$(echo "$EF_SAK_FRONTEND_LOKAL_SECRETS" | jq -r '.AZURE_APP_CLIENT_SECRET')
 
-# Generate random 32 character strings for the cookie and session keys
-COOKIE_KEY1=$(openssl rand -hex 16)
-COOKIE_KEY2=$(openssl rand -hex 16)
-PASSPORTCOOKIE_KEY1=$(openssl rand -hex 16)
-PASSPORTCOOKIE_KEY2=$(openssl rand -hex 16)
-PASSPORTCOOKIE_KEY3=$(openssl rand -hex 16)
-PASSPORTCOOKIE_KEY4=$(openssl rand -hex 16)
-SESSION_SECRET=$(openssl rand -hex 16)
-
 if [ -z "$EF_SAK_FRONTEND_CLIENT_ID" ]
 then
       echo "Klarte ikke å hente miljøvariabler. Er du pålogget Naisdevice og google?"
@@ -28,15 +19,6 @@ fi
 # Write the variables into the .env file
 cat << EOF > .env
 # Denne filen er generert automatisk ved å kjøre \`hent-og-lagre-miljøvariabler.sh\`
-
-COOKIE_KEY1='$COOKIE_KEY1'
-COOKIE_KEY2='$COOKIE_KEY2'
-PASSPORTCOOKIE_KEY1='$PASSPORTCOOKIE_KEY1'
-PASSPORTCOOKIE_KEY2='$PASSPORTCOOKIE_KEY2'
-PASSPORTCOOKIE_KEY3='$PASSPORTCOOKIE_KEY3'
-PASSPORTCOOKIE_KEY4='$PASSPORTCOOKIE_KEY4'
-SESSION_SECRET='$SESSION_SECRET'
-
 CLIENT_ID='$EF_SAK_FRONTEND_CLIENT_ID'
 CLIENT_SECRET='$EF_SAK_FRONTEND_CLIENT_SECRET'
 
