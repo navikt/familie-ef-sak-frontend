99a320c642bf03221728fc0a87db3edb9351826e
Ole Christian Kvernberg <42737566+olekvernberg@users.noreply.github.com>
2026-06-03 14:04:57 +0200

    Ikke huk av varsel for aktivitetsplikt automatisk dersom overgangsregel toggle er på. Dette brevet skal kun sendes på gammelt regelverk. (#3420)

--- src/frontend/App/context/toggles.ts
+++ src/frontend/App/context/toggles.ts
@@ -22,4 +22,5 @@ export enum ToggleName {
     brukErrorAlertMedKopierKnapp = 'familie.ef.sak.frontend-alert-error-med-copy-button',
     visAutomatiskInntektsendring = 'familie.ef.sak.frontend-vis-automatisk-inntektsendring',
     visBeregningsskjema = 'familie.ef.sak.frontend-vis-beregningsskjema',
+    overgangsregel = 'familie.ef.soknad.overgangsstonad-regelendringer-2026',
 }
